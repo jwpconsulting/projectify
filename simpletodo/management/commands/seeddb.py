a106a284a5c637485cedb1c7e006c66a924e839f
Justus Perlwitz <mail@justusperlwitz.com>
2021-10-02 19:38:31 +0900

    Seeddb: Amend

--- simpletodo/management/commands/seeddb.py
+++ simpletodo/management/commands/seeddb.py
@@ -1,15 +1,55 @@
+"""Seeddb command."""
+import tqdm
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.db import transaction
 
 from user.factory import SuperUserFactory, UserFactory
+from todo.factory import TodoItemFactory, TodoItemFolderFactory
 
 
 class Command(BaseCommand):
+    """Command."""
 
-    def handle(self, *args, **options):
-        SuperUserFactory(
+    USERS = 5
+
+    def create_users(self):
+        """Create users."""
+        super_user = SuperUserFactory(
             email='admin@localhost',
             password='password',
         )
+        guest_user = UserFactory(
+            email='guest@localhost',
+            password='password',
+        )
 
-        for _ in range(5):
-            UserFactory.create()
+        users = super_user, guest_user
+
+        return users
+
+    FOLDERS = 5
+    TODOS_PER_FOLDER = 5
+
+    def create_todos(self, users):
+        """Create todo item folders and todo items."""
+        for user in tqdm.tqdm(users, desc='Todo Item Folders'):
+            count = user.todoitemfolder_set.count()
+            TodoItemFolderFactory.create_batch(
+                self.FOLDERS - count,
+                user=user,
+            )
+            folders = user.todoitemfolder_set.all()
+            for folder in tqdm.tqdm(folders, desc='Todo Items'):
+                count = folder.todoitem_set.count()
+                TodoItemFactory.create_batch(
+                    self.TODOS_PER_FOLDER - count,
+                    user=user,
+                    folder=folder,
+                )
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        """Handle."""
+        users = self.create_users()
+        self.create_todos(users)
