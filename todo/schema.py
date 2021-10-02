1df447235ca3805e46f089255956224049fea39b
Justus Perlwitz <mail@justusperlwitz.com>
2021-10-02 19:40:11 +0900

    Todo/Schema: Fix ref

--- todo/schema.py
+++ todo/schema.py
@@ -4,22 +4,33 @@ from graphene_django import DjangoObjectType
 
 from todo import models
 
+
 class TodoItemFolder(DjangoObjectType):
+    """TodoItemFolder."""
+
     class Meta:
+        """Meta."""
+
         model = models.TodoItemFolder
 
+
 class TodoItem(DjangoObjectType):
+    """TodoItem."""
 
     class Meta:
+        """Meta."""
+
         model = models.TodoItem
 
 
 class Query():
+    """Query object."""
+
     todo_items = graphene.List(TodoItem)
     todo_item_folders = graphene.List(TodoItemFolder)
 
     def resolve_todo_item_folders(self, info):
-        return TodoItemFolder.objects.all()
+        return models.TodoItemFolder.objects.all()
 
     def resolve_todo_items(self, info):
-        return TodoItem.objects.all()
+        return models.TodoItem.objects.all()
