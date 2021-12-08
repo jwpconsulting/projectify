680a65cdfd741980eb350acc34d276d8027794e4
Justus Perlwitz <mail@justusperlwitz.com>
2021-12-08 17:42:11 +0900

    Projectify: Fix default settings module

--- projectify/wsgi.py
+++ projectify/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectify.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectify.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
