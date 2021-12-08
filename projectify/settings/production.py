7313a020343b8928fc8f5b77415895e9e6ea377a
Justus Perlwitz <mail@justusperlwitz.com>
2021-12-08 17:41:51 +0900

    Add whitenoise

--- projectify/settings/production.py
+++ projectify/settings/production.py
@@ -6,3 +6,6 @@ from .base import *
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
+
+# Want forever-cacheable files and compression support? Just add this to your settings.py:
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- projectify/wsgi.py
+++ projectify/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectify.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
