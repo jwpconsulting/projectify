eb9f2e749751c48b33749304ae87b1b93ac6d40b
Justus Perlwitz <mail@justusperlwitz.com>
2021-12-10 13:12:52 +0900

    User/Models: Generate confirmation token

--- user/test/test_models.py
+++ user/test/test_models.py
@@ -9,3 +9,12 @@ class TestUser:
     def test_factory(self, user):
         """Test user factory."""
         assert user.email
+
+    def test_get_email_confirmation_token(self, user):
+        """."""
+        user.email = 'test@example'
+        user.save()
+        assert (
+            user.get_email_confirmation_token() ==
+            'd4aa423d5ee52b8d51ca9bbc7fd9d3acb7067855'
+        )
