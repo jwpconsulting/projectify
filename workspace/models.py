5f23c3434b3b075ceb13cab62dae27f43d64fc4f
Justus Perlwitz <mail@justusperlwitz.com>
2021-12-15 11:54:26 +0900

    Workspace: M2M user - workspace

--- workspace/models.py
+++ workspace/models.py
@@ -1,4 +1,7 @@
 """Workspace models."""
+from django.conf import (
+    settings,
+)
 from django.db import (
     models,
 )
@@ -6,3 +9,22 @@ from django.db import (
 
 class Workspace(models.Model):
     """Workspace."""
+
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="WorkspaceUser",
+        through_fields=("workspace", "user"),
+    )
+
+
+class WorkspaceUser(models.Model):
+    """Workspace to user mapping."""
+
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.PROTECT,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
