f1cb66696c3bde826a461ef339b6f4dff7cb6984
Justus Perlwitz <mail@justusperlwitz.com>
2021-12-15 16:18:14 +0900

    Workspace: Continue extending

--- workspace/admin.py
+++ workspace/admin.py
@@ -2,9 +2,11 @@
 from django.contrib import (
     admin,
 )
+from django.utils.translation import gettext_lazy as _
 
 from .models import (
     Workspace,
+    WorkspaceBoard,
     WorkspaceUser,
 )
 
@@ -16,13 +18,65 @@ class WorkspaceUserInline(admin.TabularInline):
     extra = 0
 
 
+class WorkspaceBoardInline(admin.TabularInline):
+    """WorkspaceBoard Inline."""
+
+    model = WorkspaceBoard
+    extra = 0
+
+
 @admin.register(Workspace)
 class WorkspaceAdmin(admin.ModelAdmin):
     """Workspace Admin."""
 
-    inlines = (WorkspaceUserInline,)
+    inlines = (WorkspaceUserInline, WorkspaceBoardInline)
+    list_display = (
+        "title",
+        "description",
+        "created",
+        "modified",
+    )
 
 
 @admin.register(WorkspaceUser)
 class WorkspaceUserAdmin(admin.ModelAdmin):
     """WorkspaceUser Admin."""
+
+    list_display = (
+        "workspace_title",
+        "user_email",
+        "created",
+        "modified",
+    )
+    list_select_related = (
+        "workspace",
+        "user",
+    )
+
+    @admin.display(description=_("Workspace title"))
+    def workspace_title(self, instance):
+        """Return the workspace's title."""
+        return instance.workspace.title
+
+    @admin.display(description=_("User email"))
+    def user_email(self, instance):
+        """Return the workspace's title."""
+        return instance.user.email
+
+
+@admin.register(WorkspaceBoard)
+class WorkspaceBoardAdmin(admin.ModelAdmin):
+    """WorkspaceBoard Admin."""
+
+    list_display = (
+        "title",
+        "workspace_title",
+        "created",
+        "modified",
+    )
+    list_select_related = ("workspace",)
+
+    @admin.display(description=_("Workspace title"))
+    def workspace_title(self, instance):
+        """Return the workspace's title."""
+        return instance.workspace.title
