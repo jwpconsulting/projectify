c690321c8a5361bf0f9dab7532a466ae74dcf9d8
Giosué Russo <josh@hizen.io>
2022-01-04 12:41:43 +0900

    graphql first setup

---
+++ src/lib/env.ts
@@ -0,0 +1,3 @@
+export default {
+  GRAPHQL_ENDPOINT: import.meta.env['VITE_GRAPHQL_ENDPOINT'] as string,
+};
---
+++ src/lib/graphql-client.ts
@@ -0,0 +1,4 @@
+import { GraphQLClient } from 'graphql-request'
+import vars from '$lib/env'
+
+export const client = new GraphQLClient(vars.GRAPHQL_ENDPOINT)
