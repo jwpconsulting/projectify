868882b96f4d02f8c44d7092849a385744f7a3ca
Giosué Russo <josh@hizen.io>
2022-01-05 14:54:56 +0900

    Signin UI markup

--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -1,17 +1,56 @@
 <script lang="ts">
     import { client } from "$lib/graphql-client";
+
+    let usernameValue;
+    let passwordValue;
+
+    function submit() {}
 </script>
 
 <main class="page page-center">
-    <div class="panel">
-        <div class="field">
-            <div class="label">Email</div>
-            <input type="email" placeholder="email" />
+    <div class="card text-center shadow-card">
+        <div class="card-body items-center">
+            <div class="py-2">
+                <h1 class="card-title">Sign in</h1>
+                <div>
+                    Let’s set up your account, Don't have one yet?<wbr />
+                    <a class="link link-primary" href="/singup">
+                        Sign up here.
+                    </a>
+                </div>
+            </div>
+
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Username</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="username"
+                    class="input input-bordered"
+                />
+            </div>
+
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Password</span>
+                </label>
+                <input
+                    type="password"
+                    placeholder="password"
+                    class="input input-bordered"
+                />
+            </div>
+            <div class="p-2 text-error">Wrong username or password.</div>
+
+            <div class="pt-7">
+                <button class="btn btn-primary">Sign in</button>
+
+                <div class="p-2">
+                    Forget password? Password reset
+                    <a class="link link-primary" href="/reset">here.</a>
+                </div>
+            </div>
         </div>
-        <div class="field">
-            <div class="label">Password</div>
-            <input type="password" placeholder="password" />
-        </div>
-        <button class="btn-primary">Sign in</button>
     </div>
 </main>
