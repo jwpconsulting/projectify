74f2231e50c508d835064b1be43604c160329465
Giosué Russo <josh@hizen.io>
2022-01-05 17:53:29 +0900

    Header Signout

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import { user, logout } from "$lib/stores/user";
 
-    const items = [
+    $: userData = $user;
+    $: items = [
         { label: "Home", to: "/" },
-        { label: "Signin", to: "/signin" }
-    ];
+        { label: "Signin", to: "/signin", authRequired: false },
+        { label: "Signout", action: logout, authRequired: true }
+    ].filter((it) => {
+        if (!userData && it.authRequired === true) {
+            return false;
+        }
+        if (userData && it.authRequired === false) {
+            return false;
+        }
+        return true;
+    });
 </script>
 
 <header class="h-16 bg-gray-200 flex items-center p-4">
@@ -12,9 +23,14 @@
         <ul class="flex">
             {#each items as it}
                 <li class:active={$page.path === it.to}>
-                    <a class="p-2" sveltekit:prefetch href={it.to}
-                        >{it.label}</a
+                    <a
+                        on:click={it.action}
+                        class="p-2"
+                        sveltekit:prefetch
+                        href={it.to}
                     >
+                        {it.label}
+                    </a>
                 </li>
             {/each}
         </ul>
--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,5 +1,7 @@
 import { writable } from 'svelte/store';
 import { goto } from "$app/navigation";
+import { client } from "$lib/graphql-client";
+import { gql } from "graphql-request";
 
 export const user = writable(null);
 export const singinRedirect = { to: null };
@@ -14,3 +16,19 @@ user.subscribe(u => {
     }
   }
 })
+
+export const logout = async () => {
+  const res = await client.request(
+    gql`
+        mutation {
+            logout {
+                user {
+                    email
+                }
+            }
+        }
+    `
+  );
+
+  user.set(null)
+}
