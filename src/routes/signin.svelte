784b843e9a8c2fcade3d4dcdfd5c9ac7ec068dcf
Giosué Russo <josh@hizen.io>
2022-01-05 11:23:50 +0900

    Signin UI scaffolding

---
+++ src/routes/signin.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { client } from "$lib/graphql-client";
+</script>
+
+<main class="page page-center">
+    <div class="panel">
+        <div class="field">
+            <div class="label">Email</div>
+            <input type="email" placeholder="email" />
+        </div>
+        <div class="field">
+            <div class="label">Password</div>
+            <input type="password" placeholder="password" />
+        </div>
+        <button class="btn-primary">Sign in</button>
+    </div>
+</main>
