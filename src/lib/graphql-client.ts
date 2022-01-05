da54457fb85abfc36d22bcd98bb03f07bff4d199
Giosué Russo <josh@hizen.io>
2022-01-05 18:43:07 +0900

    add credentials and cors to Graphql client

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,4 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 import vars from "$lib/env";
 
-export const client = new GraphQLClient(vars.GRAPHQL_ENDPOINT);
+export const client = new GraphQLClient(vars.GRAPHQL_ENDPOINT, {
+    credentials: "include",
+    mode: "cors"
+});
