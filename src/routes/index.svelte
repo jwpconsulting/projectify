cc579cb710727daa5055f6c643c0f8cbea5e8cd9
Giosué Russo <josh@hizen.io>
2022-01-07 14:00:38 +0900

    Redirect custom page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,3 @@
-<script lang="ts">
-    import { onMount } from "svelte";
-    import { goto } from "$app/navigation";
-    onMount(async () => {
-        if (location.pathname != "/") {
-            goto(location.pathname);
-        }
-    });
-</script>
-
 <main class="page page-center">
     <h1>HOME</h1>
 </main>
---
+++ src/routes/redirect.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    onMount(async () => {
+        if (location.pathname != "/") {
+            goto(location.pathname);
+        }
+    });
+</script>
