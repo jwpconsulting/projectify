95322d67a0bd11b84d4876cc73fc9b04a7cc1d24
Giosué Russo <josh@hizen.io>
2022-01-10 18:29:43 +0900

    Task scaffolding with AuthGuard

---
+++ src/routes/tasks.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import AuthGuard from "$lib/components/authGuard.svelte";
+    import WorkspacesSideNev from "$lib/components/tasks/workspaces-side-nev.svelte";
+</script>
+
+<AuthGuard>
+    <main class="page page-center">
+        <WorkspacesSideNev />
+    </main>
+</AuthGuard>
