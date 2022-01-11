126658acdc6970a04560e35736f9985df45c23cb
Giosué Russo <josh@hizen.io>
2022-01-11 12:30:36 +0900

    Define type for route item

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,11 @@
+type routeItem = {
+    label: string;
+    to?: string;
+    authRequired?: boolean;
+    forceNavigation?: boolean;
+    action?: (any) => void;
+};
+
 export default [
     { label: "Home", to: "/" },
     { label: "Signup", to: "/signup", authRequired: false },
@@ -7,4 +15,4 @@ export default [
         authRequired: true, // When is true user is fetched
         forceNavigation: true, // Force visibility in the nav bar even if auth is required
     },
-];
+] as routeItem[];
