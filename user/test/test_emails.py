81ed84bfe4d3a074cf52bcbe70c2469a702346f7
Justus Perlwitz <mail@justusperlwitz.com>
2022-01-12 18:54:20 +0100

    User/Emails: urlencode confirm data
    Fix #32

--- user/test/test_emails.py
+++ user/test/test_emails.py
@@ -12,8 +12,11 @@ class TestUserEmailConfirmationEmail:
 
     def test_send(self, user, mailoutbox):
         """Test send."""
+        user.email = "contains space@example.com"
+        user.save()
         mail = UserEmailConfirmationEmail(user)
         mail.send().get()
         assert len(mailoutbox) == 1
         m = mailoutbox[0]
+        assert "contains%20space%40example.com" in m.body
         assert user.get_email_confirmation_token() in m.body
