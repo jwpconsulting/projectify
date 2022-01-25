bd98bd53d252f51ee8f7c9c5fffb1d784c62dd1a
Justus Perlwitz <mail@justusperlwitz.com>
2022-01-25 11:41:44 +0100

    Workspace: Split up schema

---
+++ workspace/schema/subscription.py
@@ -0,0 +1,42 @@
+"""Workspace schema subscription."""
+import graphene
+
+import channels_graphql_ws
+
+from .. import (
+    models,
+)
+from . import (
+    types,
+)
+
+
+class OnWorkspaceBoardChange(channels_graphql_ws.Subscription):
+    """Notify ow workspace board changes."""
+
+    class Arguments:
+        """Arguments."""
+
+        uuid = graphene.ID(required=True)
+
+    workspace_board = graphene.Field(types.WorkspaceBoard)
+
+    @classmethod
+    def subscribe(cls, root, info, uuid):
+        """Return subscription information."""
+        workspace_board = models.WorkspaceBoard.objects.get_for_user_and_uuid(
+            info.context.user,
+            uuid,
+        )
+        return [str(workspace_board.uuid)]
+
+    @classmethod
+    def publish(cls, payload, info, uuid):
+        """Publish subscription information."""
+        return cls(payload)
+
+
+class Subscription:
+    """Subscription."""
+
+    on_workspace_board_change = OnWorkspaceBoardChange.Field()
---
+++ workspace/schema/types.py
@@ -0,0 +1,93 @@
+"""Workspace schema types."""
+import graphene
+import graphene_django
+
+from .. import (
+    models,
+)
+
+
+class Workspace(graphene_django.DjangoObjectType):
+    """Workspace."""
+
+    users = graphene.List("user.schema.User")
+    boards = graphene.List("workspace.schema.types.WorkspaceBoard")
+
+    def resolve_users(self, info):
+        """Resolve workspace users."""
+        return self.users.all()
+
+    def resolve_boards(self, info):
+        """Resolve workspace boards."""
+        return self.workspaceboard_set.all()
+
+    class Meta:
+        """Meta."""
+
+        fields = (
+            "users",
+            "created",
+            "modified",
+            "title",
+            "description",
+            "uuid",
+        )
+        model = models.Workspace
+
+
+class WorkspaceBoard(graphene_django.DjangoObjectType):
+    """WorkspaceBoard."""
+
+    sections = graphene.List("workspace.schema.types.WorkspaceBoardSection")
+
+    def resolve_sections(self, info):
+        """Resolve workspace board sections."""
+        return self.workspaceboardsection_set.all()
+
+    class Meta:
+        """Meta."""
+
+        fields = ("created", "modified", "title", "description", "uuid")
+        model = models.WorkspaceBoard
+
+
+class WorkspaceBoardSection(graphene_django.DjangoObjectType):
+    """WorkspaceBoardSection."""
+
+    tasks = graphene.List("workspace.schema.types.Task")
+
+    def resolve_tasks(self, info):
+        """Resolve tasks for this workspace board section."""
+        return self.task_set.all()
+
+    class Meta:
+        """Meta."""
+
+        fields = ("created", "modified", "title", "description", "uuid")
+        model = models.WorkspaceBoardSection
+
+
+class Task(graphene_django.DjangoObjectType):
+    """Task."""
+
+    sub_tasks = graphene.List("workspace.schema.types.SubTask")
+
+    def resolve_sub_tasks(self, info):
+        """Resolve sub tasks for this task."""
+        return self.subtask_set.all()
+
+    class Meta:
+        """Meta."""
+
+        fields = ("created", "modified", "title", "description", "uuid")
+        model = models.Task
+
+
+class SubTask(graphene_django.DjangoObjectType):
+    """SubTask."""
+
+    class Meta:
+        """Meta."""
+
+        fields = ("created", "modified", "title", "description", "uuid")
+        model = models.SubTask
