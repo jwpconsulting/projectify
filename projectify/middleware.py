bce79ec6c7b333dcd05db844d8f9e399ab1de00f
Justus Perlwitz <mail@justusperlwitz.com>
2022-02-08 11:03:52 +0900

    Projectify/Middleware: Run only once

--- projectify/middleware.py
+++ projectify/middleware.py
@@ -11,6 +11,10 @@ from .loader import (
 # Graphene middlewares
 def atomic_transaction_middleware(next, root, info, *args, **kwargs):
     """Wrap graphql query/mutation in transaction."""
+    # Ensure middleware is only run once
+    if hasattr(info.context, "atomic_transaction_middleware"):
+        return next(root, info, *args, **kwargs)
+    info.context.atomic_transaction_middleware = True
     with transaction.atomic():
         return_value = next(root, info, *args, **kwargs)
     return return_value
@@ -18,5 +22,9 @@ def atomic_transaction_middleware(next, root, info, *args, **kwargs):
 
 def loader_middleware(next, root, info, *args, **kwargs):
     """Add loaders to info.context."""
+    # Ensure middleware is only run once
+    if hasattr(info.context, "loader_middleware"):
+        return next(root, info, *args, **kwargs)
+    info.context.loader_middleware = True
     info.context.loader = Loader()
     return next(root, info, *args, **kwargs)
