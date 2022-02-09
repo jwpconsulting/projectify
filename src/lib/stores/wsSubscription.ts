03ba364cea2bcd891ff840a7ca522872f0776484
Giosué Russo <josh@hizen.io>
2022-02-09 10:55:39 +0900

    WSSubscitpion reconnection

--- src/lib/stores/wsSubscription.ts
+++ src/lib/stores/wsSubscription.ts
@@ -1,3 +1,4 @@
+import delay from "delay";
 import type { Subscriber } from "svelte/store";
 
 type WSStore = { message: string; at: number };
@@ -6,10 +7,35 @@ type WSSubscriber = Subscriber<WSStore>;
 export class WSSubscriptionStore {
     public store: WSStore;
     subscribers = [];
-    socket: WebSocket;
+    socket: WebSocket = null;
+    retryingConnection = false;
+
+    public retryTimeStart = 2000;
+    public retryTimeMult = 1.3;
+    public retryTimeJitter = 500;
+
+    retryTime: number;
 
     constructor(public url: string) {
-        this.socket = new WebSocket(url);
+        this.retryTime = this.retryTimeStart;
+        this.createNewConnection();
+    }
+
+    deleteConnection(): void {
+        if (this.socket !== null) {
+            this.socket.onmessage = null;
+            this.socket.onclose = null;
+            this.socket.onerror = null;
+            this.socket.close();
+            delete this.socket;
+            this.socket = null;
+        }
+    }
+
+    async createNewConnection(): Promise<void> {
+        this.deleteConnection();
+
+        this.socket = new WebSocket(this.url);
 
         this.socket.onmessage = ({ data, timeStamp }) => {
             this.store = {
@@ -18,6 +44,42 @@ export class WSSubscriptionStore {
             };
             this.dispatch();
         };
+
+        this.socket.onopen = () => {
+            this.retryTime = this.retryTimeStart;
+            this.dispatch();
+        };
+
+        this.socket.onclose = async () => {
+            this.retryConnection();
+        };
+
+        this.socket.onerror = async () => {
+            this.retryConnection();
+        };
+    }
+
+    async retryConnection(): Promise<void> {
+        if (this.retryingConnection) {
+            return;
+        }
+        this.retryingConnection = true;
+
+        const delayTime =
+            this.retryTime + Math.random() * this.retryTimeJitter;
+
+        await delay(delayTime);
+
+        this.retryTime *= this.retryTimeMult;
+
+        if (
+            this.retryingConnection &&
+            this.socket &&
+            this.socket.readyState == WebSocket.CLOSED
+        ) {
+            this.retryingConnection = false;
+            this.createNewConnection();
+        }
     }
 
     dispatch(): void {
@@ -37,7 +99,7 @@ export class WSSubscriptionStore {
                 this.subscribers.splice(index, 1);
             }
             if (!this.subscribers.length) {
-                this.socket.close();
+                this.deleteConnection();
                 stores[this.url] = null;
                 delete stores[this.url];
             }
