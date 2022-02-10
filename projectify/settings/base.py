648efbf2d9a9c89afee036139d18e46eeccac816
Justus Perlwitz <mail@justusperlwitz.com>
2022-02-10 15:11:44 +0900

    Projectify/Middleware: Remove duplicate

--- projectify/settings/base.py
+++ projectify/settings/base.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
