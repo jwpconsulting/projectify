4701868900a08d4c4e645ac81bae15eb247f147e
Giosué Russo <josh@hizen.io>
2022-02-14 11:43:17 +0900

    DrawerModal css animation to svelte transition

--- src/lib/components/drawerModal.svelte
+++ src/lib/components/drawerModal.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import { fly } from "svelte/transition";
+
     export let open = false;
+    let contentWidth;
 
     $: {
         if (open) {
@@ -18,9 +21,19 @@
         on:click={() => (open = !open)}
         class="d-modal-background fixed w-full h-full bg-[#002332] open:bg-opacity-30"
     />
-    <div class="d-modal-content bg-base-100 grow open:opacity-40">
-        <slot />
-    </div>
+    {#if open}
+        <div
+            bind:clientWidth={contentWidth}
+            transition:fly={{
+                duration: 300,
+                x: contentWidth,
+                opacity: 1,
+            }}
+            class="d-modal-content bg-base-100 grow open:opacity-40"
+        >
+            <slot />
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
@@ -39,10 +52,6 @@
     }
 
     .d-modal > .d-modal-content {
-        transform: translateX(100%);
-        transition: all 300ms ease-in-out;
-    }
-    .d-modal.open > .d-modal-content {
-        transform: translateX(0);
+        z-index: 100;
     }
 </style>
