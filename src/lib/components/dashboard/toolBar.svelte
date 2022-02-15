51512b29e714862dc08271c19edd769ca075efcd
Giosué Russo <josh@hizen.io>
2022-02-15 16:41:09 +0900

    toolBar

---
+++ src/lib/components/dashboard/toolBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+    export type ToolBarItem = {
+        label: string;
+        icon: any;
+        onClick: (...any) => void;
+    };
+</script>
+
+<script lang="ts">
+    export let items: ToolBarItem[];
+</script>
+
+{#each items as it}
+    <button
+        class="btn btn-ghost btn-xs h-10 px-3 flex justify-center items-center shrink-0"
+        on:click|stopPropagation={it.onClick}
+    >
+        <svelte:component this={it.icon} />
+        <span>{it.label}</span>
+    </button>
+{/each}
