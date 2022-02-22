9d80c0095c7c650c7f31a4e4490a3d9619cf5308
Giosué Russo <josh@hizen.io>
2022-02-22 15:36:52 +0900

    Add Icons

---
+++ src/lib/components/icons/icon-archive.svelte
@@ -0,0 +1,30 @@
+<svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M9 7H2V21H22V7H15"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M22 7L18 3H15M2 7L6 3H9"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M7.31235 13.6096C7.09672 13.4371 6.78207 13.472 6.60957 13.6877C6.43706 13.9033 6.47202 14.2179 6.68765 14.3904L7.31235 13.6096ZM12 18L11.6877 18.3904C11.8703 18.5365 12.1297 18.5365 12.3123 18.3904L12 18ZM17.3123 14.3904C17.528 14.2179 17.5629 13.9033 17.3904 13.6877C17.2179 13.472 16.9033 13.4371 16.6877 13.6096L17.3123 14.3904ZM6.68765 14.3904L11.6877 18.3904L12.3123 17.6096L7.31235 13.6096L6.68765 14.3904ZM12.3123 18.3904L17.3123 14.3904L16.6877 13.6096L11.6877 17.6096L12.3123 18.3904Z"
+        fill="currentColor"
+    />
+    <path
+        d="M12 18L12 3"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
