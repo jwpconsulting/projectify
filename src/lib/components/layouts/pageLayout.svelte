3c5fc7339e9bbff6e76aea4cdcb85af3ba5186f3
Giosué Russo <josh@hizen.io>
2022-02-22 10:26:52 +0900

    Page Layout with footer scaffolding

---
+++ src/lib/components/layouts/pageLayout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import HeaderLogo from "../assets/headerLogo.svelte";
+    import Header from "../Header.svelte";
+</script>
+
+<Header />
+<slot />
+<footer class="flex justify-center border-t border-base-300">
+    <div class="flex items-center justify-start w-full max-w-3xl py-8 px-4">
+        <HeaderLogo />
+    </div>
+</footer>
