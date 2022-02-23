94570a21ef09fe060acc22f3d9d487b3331bef5b
Giosué Russo <josh@hizen.io>
2022-02-23 10:48:07 +0900

    Setting page template

---
+++ src/lib/components/settingPage.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { gotoDashboard } from "$lib/stores/dashboard";
+
+    import IconArrowLeft from "./icons/icon-arrow-left.svelte";
+    export let title;
+    export let onBack = () => {
+        gotoDashboard();
+    };
+</script>
+
+<div class="page page-center">
+    <div class="w-full max-w-xl space-y-8">
+        <div class="flex space-x-5 justify-start items-center">
+            <button
+                class="btn btn-circle btn-primary"
+                on:click={() => onBack()}
+            >
+                <div class="translate-x-1">
+                    <IconArrowLeft />
+                </div>
+            </button>
+            <div class="font-bold text-2xl">{title}</div>
+        </div>
+        <div class="card min-h-8 overflow-visible">
+            <slot />
+        </div>
+    </div>
+</div>
--- src/routes/user/profile.svelte
+++ src/routes/user/profile.svelte
@@ -1,5 +1,23 @@
 <script>
-    import PageLayout from "$lib/components/layouts/pageLayout.svelte";
+    import IconPlus from "$lib/components/icons/icon-plus.svelte";
+    import SettingPage from "$lib/components/SettingPage.svelte";
 </script>
 
-<div class="page page-center">Profile</div>
+<SettingPage title="My Profile">
+    <header
+        class="flex flex-col justify-center items-center space-y-2 mt-[-40px]"
+    >
+        <div
+            class="m-1 flex -space-x-1 overflow-hidden w-20 h-20 rounded-full shrink-0 ring-4"
+        >
+            <img
+                draggable="false"
+                width="100%"
+                height="100%"
+                src="https://picsum.photos/200"
+                alt="user"
+            />
+        </div>
+        <div class="font-bold text-xl">Not set</div>
+    </header>
+</SettingPage>
