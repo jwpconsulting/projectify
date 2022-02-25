1f8d69332e7b0acccaa341948ebb92dbf2d6b851
Justus Perlwitz <mail@justusperlwitz.com>
2022-02-25 11:58:29 +0900

    Settings: Allow cross csrf requests

--- projectify/settings/production.py
+++ projectify/settings/production.py
@@ -24,6 +24,9 @@ EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 # Celery
 CELERY_BROKER_URL = os.environ["REDIS_TLS_URL"]
 
+# CSRF
+CSRF_COOKIE_DOMAIN = ".projectifyapp.com"
+
 CORS_ALLOWED_ORIGINS = ("https://www.projectifyapp.com",)
 
 CORS_ALLOWED_ORIGIN_REGEXES = (
