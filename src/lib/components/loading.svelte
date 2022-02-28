2ef23dc6800797308295040b46ac6548023752b7
Giosué Russo <josh@hizen.io>
2022-02-28 19:18:24 +0900

    Loading component

---
+++ src/lib/components/loading.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { circIn } from "svelte/easing";
+</script>
+
+<div class="w-20 h-20 text-primary">
+    <svg viewBox="-50 -50 100 100" fill="none">
+        <circle cx="0" cy="0" />
+        <circle cx="0" cy="0" />
+    </svg>
+</div>
+
+<style lang="scss">
+    svg {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        --stroke-size: 5px;
+    }
+    svg > circle {
+        stroke: currentColor;
+        stroke-width: var(--stroke-size);
+        opacity: 1;
+        border-radius: 50%;
+        animation: ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+        animation-delay: 0.5s;
+    }
+    svg circle:nth-child(2) {
+        animation-delay: 1s;
+    }
+    @keyframes ripple {
+        0% {
+            opacity: 1;
+            r: 0px;
+        }
+        100% {
+            opacity: 0;
+            r: calc(50% - var(--stroke-size));
+        }
+    }
+</style>
