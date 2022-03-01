9ae851f21e2bf9006d2f061f8f6630f304c21641
Giosué Russo <josh@hizen.io>
2022-03-01 13:59:37 +0900

    User picture handle error image loading

--- src/lib/components/userProfilePicture.svelte
+++ src/lib/components/userProfilePicture.svelte
@@ -2,6 +2,7 @@
     import IconUserProfile from "./icons/icon-user-profile.svelte";
     import vars from "$lib/env";
     import IconPlus from "./icons/icon-plus.svelte";
+    import IconClose from "./icons/icon-close.svelte";
     export let size = 32;
     export let url: string = null;
     export let prefix = vars.API_ENDPOINT;
@@ -9,11 +10,14 @@
 
     let src: string = null;
 
+    let loadingError = false;
+
     $: {
         if (url) {
             let usePrefix =
                 url.indexOf("data:") !== 0 && url.indexOf("http") !== 0;
             src = usePrefix ? prefix + url : url;
+            loadingError = false;
         } else {
             src = null;
         }
@@ -33,17 +37,22 @@
         class:ring-2={size <= 32}
         class:ring-4={size > 32}
         style={`width: ${sizePx}; height: ${sizePx};`}
-        class="flex overflow-hidden rounded-full shrink-0 bg-secondary text-base-100"
+        class="flex justify-center items-center overflow-hidden rounded-full shrink-0 bg-secondary text-base-100"
     >
         {#if src}
-            <img
-                class="object-cover"
-                draggable="false"
-                width="100%"
-                height="100%"
-                {src}
-                alt="user"
-            />
+            {#if loadingError}
+                <IconClose />
+            {:else}
+                <img
+                    class="object-cover"
+                    draggable="false"
+                    width="100%"
+                    height="100%"
+                    {src}
+                    alt="user"
+                    on:error={() => (loadingError = true)}
+                />
+            {/if}
         {:else}
             <IconUserProfile />
         {/if}
