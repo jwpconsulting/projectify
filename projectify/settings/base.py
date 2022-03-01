493f95e5cf49b2964f621869f77ef6f4671f6945
Justus Perlwitz <mail@justusperlwitz.com>
2022-03-01 12:54:50 +0900

    Settings: Reorganize trusted origins

--- projectify/settings/base.py
+++ projectify/settings/base.py
@@ -41,10 +41,6 @@ SESSION_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = (
-    "https://*.netlify.app/",
-    "https://www.projectifyapp.com",
-)
 
 # Cors
 
