4f941c939a38e2efbae8cfbcb095de9b86ce43de
Justus Perlwitz <mail@justusperlwitz.com>
2022-03-10 17:59:12 +0900

    User: Add UserInvite admin

--- user/admin.py
+++ user/admin.py
@@ -3,11 +3,16 @@ from django.contrib import (
     admin,
 )
 
-from .models import (
-    User,
+from . import (
+    models,
 )
 
 
-@admin.register(User)
+@admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     """User admin."""
+
+
+@admin.register(models.UserInvite)
+class UserInviteAdmin(admin.ModelAdmin):
+    """User invite admin."""
