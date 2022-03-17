7496e09e88a30783d451820c4eede0710e8b4f3b
Giosué Russo <josh@hizen.io>
2022-03-17 12:48:36 +0900

    LabelPill

---
+++ src/lib/components/dashboard/labelPill.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { getColorFromInx } from "$lib/utils/colors";
+    export let label;
+    export let active = true;
+</script>
+
+<div
+    style="{`--color:${getColorFromInx(label.color).style}; --opacity:${
+        active ? '1.0' : '0.2'
+    }`};"
+    class="max-w-min label whitespace-nowrap font-bold text-xs px-3 py-1 m-1 rounded-full border"
+>
+    {label.name}
+</div>
