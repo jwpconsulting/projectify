9ea474581b00d065d36cee01838e705590eb4392
Giosué Russo <josh@hizen.io>
2022-03-17 12:51:52 +0900

    LabelPill size fix

--- src/lib/components/dashboard/labelPill.svelte
+++ src/lib/components/dashboard/labelPill.svelte
@@ -8,7 +8,7 @@
     style="{`--color:${getColorFromInx(label.color).style}; --opacity:${
         active ? '1.0' : '0.2'
     }`};"
-    class="max-w-min label whitespace-nowrap font-bold text-xs px-3 py-1 m-1 rounded-full border"
+    class="label whitespace-nowrap font-bold text-xs px-3 py-1 m-1 rounded-full border"
 >
     {label.name}
 </div>
