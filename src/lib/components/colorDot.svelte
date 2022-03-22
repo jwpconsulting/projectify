5635cc24a4a906cc00c8c5f9e58d4aee73e91db0
Giosué Russo <josh@hizen.io>
2022-03-22 13:00:21 +0900

    separate ColorDot component

---
+++ src/lib/components/colorDot.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    export let color;
+    export let active = false;
+</script>
+
+<div
+    style={`--color:${color.h} ${color.s}% ${color.l}%;--color:${color.style};`}
+    class:text-base-100={color.br}
+    class:active
+    class="color flex shrink-0 justify-center items-center rounded-full p-2 w-full h-full"
+/>
+
+<style lang="scss">
+    .color {
+        --opacity: 1;
+        background-color: hsla(var(--color) / var(--opacity));
+        background-color: rgba(var(--color) / var(--opacity));
+        border: 1px solid var(--color);
+        transition: all ease-out 100ms;
+        position: relative;
+    }
+
+    .color::after {
+        --margin: 20%;
+        content: "";
+        position: absolute;
+        top: var(--margin);
+        left: var(--margin);
+        bottom: var(--margin);
+        right: var(--margin);
+        background-color: #fff;
+        opacity: 0.6;
+        border-radius: 50%;
+        transition: all ease-out 300ms;
+        transform: scale(0);
+    }
+
+    .color:hover {
+        --opacity: 0.5;
+        transform: scale(1.1);
+    }
+    .color:active:not(.active) {
+        --opacity: 0.3;
+        transition-duration: 50ms;
+        transform: scale(0.95);
+    }
+    .color.active {
+        --opacity: 1;
+        transform: scale(1);
+    }
+    .color.active::after {
+        transform: scale(1);
+    }
+</style>
--- src/lib/components/colorPicker.svelte
+++ src/lib/components/colorPicker.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { getColorFromInx, paletteSize } from "$lib/utils/colors";
     import { createEventDispatcher } from "svelte";
+    import ColorDot from "./colorDot.svelte";
 
     export let selectedColorInx = -1;
 
@@ -21,56 +22,15 @@
 <div class="flex flex-wrap">
     {#each colors as color, inx}
         <div
-            style={`--color:${color.h} ${color.s}% ${color.l}%;--color:${color.style};`}
-            class:text-base-100={color.br}
-            class:active={selectedColorInx % paletteSize === inx}
-            class="cursor-pointer color flex shrink-0 justify-center items-center rounded-full p-2 m-2 bg-debug h-10 w-10"
+            class="cursor-pointer m-2 h-10 w-10"
             on:click={() => {
                 onColorClick(inx);
             }}
-        />
+        >
+            <ColorDot
+                {color}
+                active={selectedColorInx % paletteSize === inx}
+            />
+        </div>
     {/each}
 </div>
-
-<style lang="scss">
-    .color {
-        --opacity: 1;
-        background-color: hsla(var(--color) / var(--opacity));
-        background-color: rgba(var(--color) / var(--opacity));
-        border: 1px solid var(--color);
-        transition: all ease-out 100ms;
-        position: relative;
-    }
-
-    .color::after {
-        --margin: 8px;
-        content: "";
-        position: absolute;
-        top: var(--margin);
-        left: var(--margin);
-        bottom: var(--margin);
-        right: var(--margin);
-        background-color: #fff;
-        opacity: 0.6;
-        border-radius: 50%;
-        transition: all ease-out 300ms;
-        transform: scale(0);
-    }
-
-    .color:hover {
-        --opacity: 0.5;
-        transform: scale(1.1);
-    }
-    .color:active:not(.active) {
-        --opacity: 0.3;
-        transition-duration: 50ms;
-        transform: scale(0.95);
-    }
-    .color.active {
-        --opacity: 1;
-        transform: scale(1);
-    }
-    .color.active::after {
-        transform: scale(1);
-    }
-</style>
