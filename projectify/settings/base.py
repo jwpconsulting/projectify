044228667403fd6c0ca862038c11f55c9a5b8112
apkallum <apkallum@protonmail.com>
2022-04-25 19:27:46 -0400

    Production: Try fix CSRF for profile photo

--- projectify/settings/base.py
+++ projectify/settings/base.py
@@ -204,7 +204,7 @@ EMAIL_EAGER = False
 # Rest framework
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
