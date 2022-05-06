5d1ff9e13189e560fe6fb96ecea4f8ff4e8d9bfb
Giosué Russo <josh@hizen.io>
2022-05-06 16:19:16 +0900

    404 page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+    import PageLayout from "$lib/components/layouts/pageLayout.svelte";
     import { _ } from "svelte-i18n";
 </script>
 
-<main class="page page-center">
-    <h1>{$_("404")}</h1>
-</main>
+<PageLayout>
+    <main
+        class="flex grow flex-col items-center justify-center bg-base-200 p-4"
+    >
+        <h1 class="pb-2 text-6xl">{$_("404")}</h1>
+        <div class="font-bold">{$_("page-not-found")}</div>
+        <div class="text-sm">
+            {$_("the-page-you-are-looking-for-could-not-be-found")}
+        </div>
+        <a href="/" class="btn-action btn btn-md m-3 rounded-full px-8"
+            >{$_("go-back")}</a
+        >
+    </main>
+</PageLayout>
