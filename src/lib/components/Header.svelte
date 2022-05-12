0c0f866d747f1c18a785a1c8f1a50a483224afc6
Giosué Russo <josh@hizen.io>
2022-05-12 14:48:20 +0900

    Langing header scroll

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -49,49 +49,50 @@
         }
     }
 
-    let scrollToTop = true;
+    let scrollY = 0;
+    $: scrollToTop = scrollY < 20;
 </script>
+
+<svelte:window bind:scrollY />
 
 {#if mode == "landing"}
     <header
-        class="sticky top-0 z-10 flex h-[80px] items-center p-4"
-        on:scroll={(e) => {
-            if (e.target["scrollTop"] > 0) {
-                scrollToTop = false;
-            } else {
-                scrollToTop = true;
-            }
-        }}
+        class="sticky top-0 z-10 flex h-[80px] items-center justify-center transition-all duration-300 ease-in-out"
+        class:bg-base-100={!scrollToTop}
+        class:shadow-lg={!scrollToTop}
     >
-        <a href="/" class="mr-8 flex">
-            <HeaderLogo />
-        </a>
-        <nav class="grow">
-            <ul class="flex">
-                {#each items as it}
-                    <li class:active={$page.url.pathname === it.to}>
-                        <a
-                            on:click={it.action}
-                            class="cursor-pointer p-2 font-bold"
-                            href={it.to}
-                        >
-                            {$_(it.label)}
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        </nav>
-        <nav class="flex gap-2">
-            <a
-                href="/signin"
-                class="btn btn-outline btn-primary rounded-full bg-base-100 px-8 capitalize"
-                >Signin</a
-            >
-            <a
-                href="/signup"
-                class="btn btn-primary rounded-full px-8 capitalize">Signup</a
-            >
-        </nav>
+        <div class="container flex items-center">
+            <a href="/" class="mr-8 flex">
+                <HeaderLogo />
+            </a>
+            <nav class="grow">
+                <ul class="flex">
+                    {#each items as it}
+                        <li class:active={$page.url.pathname === it.to}>
+                            <a
+                                on:click={it.action}
+                                class="cursor-pointer p-2 font-bold"
+                                href={it.to}
+                            >
+                                {$_(it.label)}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            </nav>
+            <nav class="flex gap-2">
+                <a
+                    href="/signin"
+                    class="btn btn-outline btn-primary rounded-full bg-base-100 px-8 capitalize"
+                    >Signin</a
+                >
+                <a
+                    href="/signup"
+                    class="btn btn-primary rounded-full px-8 capitalize"
+                    >Signup</a
+                >
+            </nav>
+        </div>
     </header>
 {:else}
     <header
