f0962ad0b6544a30885ed227446fb90af3424053
Giosué Russo <josh@hizen.io>
2022-05-13 13:12:38 +0900

    Header mobile drawer menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,6 +6,9 @@
     import HeaderLogo from "./assets/headerLogo.svelte";
     import HeaderUser from "./headerUser.svelte";
     import { onMount } from "svelte";
+    import IconHamburgerMenu from "./icons/icon-hamburger-menu.svelte";
+    import DrawerModal from "./drawerModal.svelte";
+    import IconClose from "./icons/icon-close.svelte";
 
     export let mode = "app";
     let items;
@@ -51,6 +54,8 @@
 
     let scrollY = 0;
     $: scrollToTop = scrollY < 20;
+
+    let drawerMenuOpen = false;
 </script>
 
 <svelte:window bind:scrollY />
@@ -80,6 +85,7 @@
                     {/each}
                 </ul>
             </nav>
+            <div class="grow" />
             <nav class="hidden gap-2 md:flex">
                 <a
                     href="/signin"
@@ -92,8 +98,63 @@
                     >Signup</a
                 >
             </nav>
+
+            <button
+                on:click={() => (drawerMenuOpen = true)}
+                class="flex  h-10 w-10 shrink-0 items-center justify-center self-end rounded-full bg-base-100 text-primary md:hidden"
+                ><IconHamburgerMenu /></button
+            >
         </div>
     </header>
+
+    <DrawerModal bind:open={drawerMenuOpen}>
+        <div class="flex min-h-[100%] min-w-[100vw] flex-col gap-6 p-6">
+            <div class="flex ">
+                <a href="/" class="mr-8 flex grow">
+                    <HeaderLogo />
+                </a>
+
+                <button
+                    on:click={() => (drawerMenuOpen = false)}
+                    class="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-base-100 text-primary"
+                    ><IconClose />
+                </button>
+            </div>
+            <nav class="flex grow flex-col">
+                <ul class="flex flex-col divide-y divide-base-300">
+                    {#each items as it}
+                        <li
+                            class="flex text-xl"
+                            class:active={$page.url.pathname === it.to}
+                        >
+                            <a
+                                on:click={() => {
+                                    drawerMenuOpen = false;
+                                }}
+                                class="cursor-pointer p-2 font-bold"
+                                href={it.to}
+                            >
+                                {$_(it.label)}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            </nav>
+            <div class="grow" />
+            <nav class="flex flex-col gap-2">
+                <a
+                    href="/signin"
+                    class="btn btn-outline btn-primary rounded-full bg-base-100 px-8 capitalize"
+                    >Signin</a
+                >
+                <a
+                    href="/signup"
+                    class="btn btn-primary rounded-full px-8 capitalize"
+                    >Signup</a
+                >
+            </nav>
+        </div>
+    </DrawerModal>
 {:else}
     <header
         class="sticky top-0 z-10 flex h-[80px] items-center border-b border-base-300 bg-base-100 p-4"
---
+++ src/lib/components/icons/icon-hamburger-menu.svelte
@@ -0,0 +1,10 @@
+<svg
+    width="24"
+    height="14"
+    viewBox="0 0 24 14"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <rect width="24" height="3" />
+    <rect y="11" width="24" height="3" />
+</svg>
