d0bed6654714dd6a75543fce2befd3032d9616f6
Justus Perlwitz <justuswilhelm@users.noreply.github.com>
2022-06-16 18:04:57 +0900

    Revert "WSSubscription: fix unused local variable"
    This reverts commit c387b3b82c021b035e792871f7e10f1c9a82ff93.

--- src/lib/stores/dashboardSubscription.ts
+++ src/lib/stores/dashboardSubscription.ts
@@ -12,6 +12,6 @@ export const getSubscriptionForCollection = (
     if (wsEndPoint.indexOf("ws://") == -1) {
         wsEndPoint = `ws://${location.host}${wsEndPoint}`;
     }
-    const wsURL = `${wsEndPoint}/${collection}/${uuid}/`;
+    const wsURL = `${vars.WS_ENDPOINT}/${collection}/${uuid}/`;
     return getSubscriptionFor(wsURL);
 };
