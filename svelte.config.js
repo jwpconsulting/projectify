f8bfc267d93c5179c4f6e642e3c1525d8ce59f71
Justus Perlwitz <justuswilhelm@users.noreply.github.com>
2022-06-16 18:04:57 +0900

    Revert "Configure proxy for local development"
    This reverts commit b96377164bc5ce614f42a2b4446611f3e48f048b.

--- svelte.config.js
+++ svelte.config.js
@@ -35,23 +35,6 @@ const config = {
             build: {
                 target: ["es2020"],
             },
-            server: {
-                proxy: {
-                    "/api": {
-                        target: "http://localhost:8000",
-                        changeOrigin: true,
-                        rewrite: (path) => path.replace(/^\/api/, ""),
-                    },
-                    "/graphql": {
-                        target: "http://localhost:8000",
-                        changeOrigin: true,
-                    },
-                    "/ws": {
-                        target: "ws://localhost:8000",
-                        ws: true,
-                    },
-                },
-            },
         },
     },
 };
