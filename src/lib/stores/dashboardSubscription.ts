d4564f90d61861e465533c3548b27cad9900de89
Giosué Russo <josh@hizen.io>
2022-06-16 11:15:00 +0900

    Proxy server: websocket connection fix

--- src/lib/stores/dashboardSubscription.ts
+++ src/lib/stores/dashboardSubscription.ts
@@ -8,6 +8,10 @@ export const getSubscriptionForCollection = (
     collection: "workspace" | "workspace-board" | "task",
     uuid: string
 ): WSSubscriptionStore => {
+    let wsEndPoint = vars.WS_ENDPOINT;
+    if (wsEndPoint.indexOf("ws://") == -1) {
+        wsEndPoint = `ws://${location.host}${wsEndPoint}`;
+    }
     const wsURL = `${vars.WS_ENDPOINT}/${collection}/${uuid}/`;
     return getSubscriptionFor(wsURL);
 };
