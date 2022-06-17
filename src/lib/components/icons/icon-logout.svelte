d7817a8d3751075edc2c903b14f8f8d3b49483d0
Giosué Russo <josh@hizen.io>
2022-06-17 11:44:42 +0900

    Profile menu icon replacement

--- src/lib/components/icons/icon-logout.svelte
+++ src/lib/components/icons/icon-logout.svelte
@@ -1,20 +1,14 @@
 <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
 >
     <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M9.00001 3.6677C5.05505 4.93486 2.2 8.63401 2.2 13C2.2 18.4124 6.58761 22.8 12 22.8C17.4124 22.8 21.8 18.4124 21.8 13C21.8 8.63402 18.9449 4.93487 15 3.66771V5.36613C18.0448 6.56364 20.2 9.53013 20.2 13C20.2 17.5287 16.5287 21.2 12 21.2C7.47126 21.2 3.8 17.5287 3.8 13C3.8 9.53012 5.95521 6.56363 9.00001 5.36612V3.6677Z"
-        fill="currentColor"
-    />
-    <path
-        d="M12 13.5V2"
-        stroke="currentColor"
-        stroke-width="1.6"
         stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
     />
 </svg>
--- src/lib/components/icons/icon-user.svelte
+++ src/lib/components/icons/icon-user.svelte
@@ -1,13 +1,14 @@
 <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
 >
-    <circle cx="12" cy="7" r="5" stroke="currentColor" />
     <path
-        d="M22 22C22 16.4772 17.5228 12 12 12C6.47715 12 2 16.4772 2 22"
-        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
     />
 </svg>
