c1396bf91e9256d2d4d9f780ec3d39c008c17ec0
Giosué Russo <josh@hizen.io>
2022-06-20 13:18:32 +0900

    Ritch text area component with link highlighting

---
+++ src/lib/components/rich-textarea.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    export let content = "";
+    export let placeholder = "";
+    export let modified = false;
+    let editMode = false;
+    import { tick } from "svelte";
+    let textAreaEl;
+
+    function urlify(text) {
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        return text.replace(urlRegex, function (url) {
+            return '<a href="' + url + '" target="_blank">' + url + "</a>";
+        });
+    }
+
+    function transform(text) {
+        text = urlify(text);
+        text = text.replace(/\n/g, "<br>");
+        return text;
+    }
+</script>
+
+<textarea
+    bind:this={textAreaEl}
+    class:hidden={!editMode}
+    rows="6"
+    class="textarea textarea-bordered resize-none p-4 leading-normal"
+    {placeholder}
+    on:input={() => (modified = true)}
+    on:blur={() => (editMode = false)}
+    bind:value={content}
+/>
+<div
+    class:hidden={editMode}
+    class="rich-textarea textarea textarea-bordered h-40 overflow-y-auto p-4 leading-normal"
+    on:click={async () => {
+        editMode = true;
+        await tick();
+        textAreaEl.focus();
+    }}
+>
+    {@html transform(content)}
+</div>
