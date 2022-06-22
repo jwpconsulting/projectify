7c43a926b980eace0e15a1efe26139cd610c0e81
Giosué Russo <josh@hizen.io>
2022-06-22 13:00:29 +0900

    GlobalDropDown: component support

--- src/lib/components/globalDropDown.svelte
+++ src/lib/components/globalDropDown.svelte
@@ -20,9 +20,11 @@
 </script>
 
 <script lang="ts">
-    import { tick } from "svelte";
+    import { SvelteComponent, tick } from "svelte";
     import IconChevronDown from "./icons/icon-chevron-down.svelte";
 
+    let component: SvelteComponent = null;
+    let componentProps: any = null;
     let items: DropDownMenuItem[] = null;
     let target: HTMLElement = null;
     let focusEl;
@@ -73,15 +75,7 @@
         window.requestAnimationFrame(checkTargetPosition);
     }
 
-    async function open(
-        its: DropDownMenuItem[],
-        trg: HTMLElement,
-        aId: any = null
-    ) {
-        items = its;
-        target = trg;
-        activeId = aId;
-
+    async function placeDialog() {
         await tick();
 
         focusEl.focus();
@@ -90,98 +84,151 @@
         startY = Math.round(rect.top);
 
         checkTargetPosition();
-
         window.requestAnimationFrame(checkTargetPosition);
+    }
+
+    async function open(
+        its: DropDownMenuItem[],
+        trg: HTMLElement,
+        aId: any = null
+    ) {
+        close();
+        items = its;
+        target = trg;
+        activeId = aId;
+        placeDialog();
+    }
+
+    async function openComponent(
+        comp: SvelteComponent,
+        trg: HTMLElement,
+        props: any
+    ) {
+        close();
+        target = trg;
+        component = comp;
+        componentProps = props;
+        placeDialog();
     }
 
     function close() {
         items = null;
         target = null;
+        component = null;
+        componentProps = null;
     }
 
-    dropDown = { open, close, items, target };
+    dropDown = { open, openComponent, close };
 </script>
 
-{#if items}
+{#if items || component}
     <div
         style={`top:${y}px; left:${x}px;`}
         bind:this={rootEl}
         class="absolute z-50 select-none"
     >
-        <ul
-            bind:this={focusEl}
-            on:blur={(e) => {
-                if (e.relatedTarget && !focusEl.contains(e.relatedTarget)) {
-                    close();
-                }
-            }}
-            tabindex="0"
-            class="dropdown-content menu min-w-[200px] rounded-lg
+        {#if component}
+            <div
+                bind:this={focusEl}
+                tabindex="0"
+                on:blur={(e) => {
+                    if (
+                        e.relatedTarget &&
+                        !focusEl.contains(e.relatedTarget)
+                    ) {
+                        close();
+                    }
+                }}
+            >
+                <svelte:component
+                    this={component}
+                    {...componentProps}
+                    on:{console.log}
+                />
+            </div>
+        {:else if items}
+            <ul
+                bind:this={focusEl}
+                on:blur={(e) => {
+                    if (
+                        e.relatedTarget &&
+                        !focusEl.contains(e.relatedTarget)
+                    ) {
+                        close();
+                    }
+                }}
+                tabindex="0"
+                class="dropdown-content menu min-w-[200px] rounded-lg
         border border-base-300 bg-base-100 py-2 shadow-xl"
-        >
-            {#each items as it}
-                {#if !it.hidden}
-                    <li>
-                        <a
-                            title={it.tooltip}
-                            disabled={it.disabled}
-                            class:active={activeId === it.id}
-                            class="h-9 space-x-2 truncate px-0 text-xs font-bold"
-                            href={it.href}
-                            on:click={() => {
-                                if (!it.items) {
-                                    if (it.onClick) {
-                                        it.onClick();
+            >
+                {#each items as it}
+                    {#if !it.hidden}
+                        <li>
+                            <a
+                                title={it.tooltip}
+                                disabled={it.disabled}
+                                class:active={activeId === it.id}
+                                class="h-9 space-x-2 truncate px-0 text-xs font-bold"
+                                href={it.href}
+                                on:click={() => {
+                                    if (!it.items) {
+                                        if (it.onClick) {
+                                            it.onClick();
+                                        }
+                                        focusEl.blur();
+                                        close();
+                                    } else {
+                                        it.open = !it.open;
                                     }
-                                    focusEl.blur();
-                                } else {
-                                    it.open = !it.open;
-                                }
-                            }}
-                        >
-                            <svelte:component this={it.icon} />
-                            <span class="grow">{it.label}</span>
+                                }}
+                            >
+                                <svelte:component this={it.icon} />
+                                <span class="grow">{it.label}</span>
+
+                                {#if it.items}
+                                    <div
+                                        class:rotate-180={it.open}
+                                        class="icon-sm transition-all"
+                                    >
+                                        <IconChevronDown />
+                                    </div>
+                                {/if}
+                            </a>
 
                             {#if it.items}
-                                <div
-                                    class:rotate-180={it.open}
-                                    class="icon-sm transition-all"
+                                <ul
+                                    class:h-0={!it.open}
+                                    class="menu overflow-hidden pl-0"
                                 >
-                                    <IconChevronDown />
-                                </div>
+                                    {#each it.items as it}
+                                        <li>
+                                            <a
+                                                title={it.tooltip}
+                                                disabled={it.disabled}
+                                                class:active={activeId ===
+                                                    it.id}
+                                                class="nowrap-ellipsis h-9 space-x-2 px-0 text-xs font-bold"
+                                                href={it.href}
+                                                on:click={() => {
+                                                    if (it.onClick) {
+                                                        it.onClick();
+                                                    }
+                                                    focusEl.blur();
+                                                }}
+                                            >
+                                                <svelte:component
+                                                    this={it.icon}
+                                                />
+                                                <span>{it.label}</span>
+                                            </a>
+                                        </li>
+                                    {/each}
+                                </ul>
                             {/if}
-                        </a>
-
-                        {#if it.items}
-                            <ul
-                                class:h-0={!it.open}
-                                class="menu overflow-hidden pl-0"
-                            >
-                                {#each it.items as it}
-                                    <li>
-                                        <a
-                                            title={it.tooltip}
-                                            disabled={it.disabled}
-                                            class:active={activeId === it.id}
-                                            class="nowrap-ellipsis h-9 space-x-2 px-0 text-xs font-bold"
-                                            href={it.href}
-                                            on:click={() => {
-                                                if (it.onClick) {
-                                                    it.onClick();
-                                                }
-                                                focusEl.blur();
-                                            }}
-                                        >
-                                            <svelte:component this={it.icon} />
-                                            <span>{it.label}</span>
-                                        </a>
-                                    </li>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </li>
-                {/if}
-            {/each}
-        </ul>
+                        </li>
+                    {/if}
+                {/each}
+            </ul>
+        {/if}
     </div>
 {/if}
