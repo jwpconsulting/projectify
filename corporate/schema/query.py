332830c288393a1302f794c5b4bcd20687bb2e0b
Justus Perlwitz <mail@justusperlwitz.com>
2022-06-22 18:10:05 +0900

    Corporate/Schema: Make customer queryable

--- corporate/schema/query.py
+++ corporate/schema/query.py
@@ -1,8 +1,29 @@
 """Corporate query schema."""
+import uuid
 
 import strawberry
+
+from .. import (
+    models,
+)
+from . import (
+    types,
+)
 
 
 @strawberry.type
 class Query:
     """Query."""
+
+    @strawberry.field
+    def customerByWorkspace(
+        self, info, workspace_uuid: uuid.UUID
+    ) -> types.Customer:
+        """Resolve customer by workspace UUID."""
+        customer = models.Customer.objects.get_by_workspace_uuid(
+            workspace_uuid
+        )
+        assert info.context.user.has_perm(
+            "corporate.can_read_customer", customer
+        )
+        return customer
