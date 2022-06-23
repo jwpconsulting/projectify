ea835873889dfc791d067099646a5608925e4757
Justus Perlwitz <mail@justusperlwitz.com>
2022-06-23 15:50:01 +0900

    Corporate/Admin: Add pizzazz

--- corporate/admin.py
+++ corporate/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import (
     admin,
 )
+from django.utils.translation import gettext_lazy as _
 
 from . import (
     models,
@@ -11,3 +12,15 @@ from . import (
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     """Customer Admin."""
+
+    list_display = ("workspace_title", "seats", "subscription_status")
+    list_select_related = ("workspace",)
+    readonly_fields = (
+        "uuid",
+        "stripe_customer_id",
+    )
+
+    @admin.display(description=_("Workspace title"))
+    def workspace_title(self, instance):
+        """Return the workspace's title."""
+        return instance.workspace.title
