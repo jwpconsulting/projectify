a793558aecb68a93c1562963557d09ea3ba79377
Giosué Russo <josh@hizen.io>
2022-06-23 16:58:56 +0900

    Task detail: breadcrumbs

---
+++ src/lib/components/dashboard/task-details-breadcrumbs.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+
+    export let task;
+
+    let pathLinks = [
+        {
+            label: task.workspaceBoardSection.workspaceBoard.title,
+        },
+        {
+            label: task.workspaceBoardSection.title,
+        },
+        {
+            label: `#${task.number}`,
+        },
+    ];
+</script>
+
+<div class="flex max-w-full items-center gap-2 overflow-hidden">
+    <div class="breadcrumbs max-w-full text-sm">
+        <ul>
+            {#each pathLinks as pl}
+                <li>
+                    <span>{pl.label}</span>
+                </li>
+            {/each}
+        </ul>
+    </div>
+</div>
