a3ec8ca791967241a774131348559117ae71f8c4
Justus Perlwitz <mail@justusperlwitz.com>
2022-06-30 13:52:38 +0900

    Workspace/Models: Populate chat message author ws user

--- workspace/test/test_migrations.py
+++ workspace/test/test_migrations.py
@@ -79,3 +79,84 @@ class Test0043:
         assert task.assignee_workspace_user is not None
         assert task.assignee_workspace_user.user == task.assignee
         assert other_task.assignee_workspace_user is None
+
+
+@pytest.mark.django_db
+class Test0047PopulateChatMessageAuthorWorkspaceUser:
+    """Test migration 0047."""
+
+    def test(self, migrator, settings):
+        """Test that author_workspace_user is populated correctly."""
+        old_state = migrator.apply_initial_migration(
+            ("workspace", "0046_chatmessage_author_workspace_user")
+        )
+        Workspace = old_state.apps.get_model("workspace", "Workspace")
+        WorkspaceBoard = old_state.apps.get_model(
+            "workspace", "WorkspaceBoard"
+        )
+        WorkspaceBoardSection = old_state.apps.get_model(
+            "workspace", "WorkspaceBoardSection"
+        )
+        Task = old_state.apps.get_model("workspace", "Task")
+        WorkspaceUser = old_state.apps.get_model("workspace", "WorkspaceUser")
+        User = old_state.apps.get_model(settings.AUTH_USER_MODEL)
+        ChatMessage = old_state.apps.get_model("workspace", "ChatMessage")
+        workspace = Workspace.objects.create(
+            title="",
+            description="",
+        )
+        workspace_board = WorkspaceBoard.objects.create(
+            title="",
+            description="",
+            workspace=workspace,
+        )
+        workspace_board_section = WorkspaceBoardSection.objects.create(
+            title="",
+            description="",
+            workspace_board=workspace_board,
+        )
+        user = User.objects.create(
+            email="hello@example.com",
+            is_staff=False,
+            is_superuser=False,
+            is_active=False,
+        )
+        WorkspaceUser.objects.create(
+            user=user,
+            workspace=workspace,
+        )
+        task = Task.objects.create(
+            title="",
+            description="",
+            workspace_board_section=workspace_board_section,
+            workspace=workspace,
+            number=1,
+        )
+        chat_message = ChatMessage.objects.create(
+            text="",
+            task=task,
+            author=user,
+        )
+
+        # Create a chat message with user, but no workspace user
+        other_user = User.objects.create(
+            email="goodbye@example.com",
+            is_staff=False,
+            is_superuser=False,
+            is_active=False,
+        )
+        other_chat_message = ChatMessage.objects.create(
+            text="",
+            task=task,
+            author=other_user,
+        )
+
+        new_state = migrator.apply_tested_migration(
+            ("workspace", "0047_populate_chatmessage_author_workspace_user")
+        )
+        ChatMessage = new_state.apps.get_model("workspace", "ChatMessage")
+        chat_message = ChatMessage.objects.get(pk=chat_message.pk)
+        other_chat_message = ChatMessage.objects.get(pk=other_chat_message.pk)
+        assert chat_message.author_workspace_user is not None
+        assert chat_message.author_workspace_user.user == chat_message.author
+        assert other_chat_message.author_workspace_user is None
