7f164f40ad9e06cc7f3d372f4a04339e7f7f946f
Justus Perlwitz <mail@justusperlwitz.com>
2022-08-03 18:58:51 +0900

    Lib/Figma: Add Avatar

---
+++ src/lib/figma/Avatar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import UserAvatar from "$lib/components/UserAvatar.svelte";
+    import type { User } from "$lib/types";
+
+    export let user: User | null;
+</script>
+
+<div
+    class="flex h-6 w-6 items-center justify-center rounded-full border border-primary"
+    class:bg-background={user === null}
+>
+    {#if user}
+        <UserAvatar {user} size={24} />
+    {/if}
+</div>
---
+++ src/routes/storybook/navigation.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Avatar from "$lib/figma/Avatar.svelte";
+    import { fr } from "$lib/storybook";
+
+    const users = [
+        {
+            email: "hello@example.com",
+            full_name: "Max Mustermann",
+        },
+        null,
+    ];
+</script>
+
+<div class={fr}>
+    {#each users as user}
+        <Avatar {user} />
+    {/each}
+</div>
