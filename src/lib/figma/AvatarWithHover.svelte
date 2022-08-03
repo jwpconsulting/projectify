0d37592a6eb91641e9dd7563aec92ba72e6bf3c2
Justus Perlwitz <mail@justusperlwitz.com>
2022-08-03 19:18:17 +0900

    Lib/Figma: Add AvatarWithHover

---
+++ src/lib/figma/AvatarWithHover.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Avatar from "$lib/figma/Avatar.svelte";
+    import { createEventDispatcher } from "svelte";
+    import type { User } from "$lib/types";
+
+    export let user: User | null;
+
+    const dispatch = createEventDispatcher();
+    function click() {
+        dispatch("click");
+    }
+</script>
+
+<button
+    on:click={click}
+    class="rounded-lg border border-transparent p-1.5 hover:bg-secondary-hover focus:border-base-content focus:outline-none"
+>
+    <Avatar {user} />
+</button>
--- src/routes/storybook/navigation.svelte
+++ src/routes/storybook/navigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Avatar from "$lib/figma/Avatar.svelte";
+    import AvatarWithHover from "$lib/figma/AvatarWithHover.svelte";
     import { fr } from "$lib/storybook";
 
     const users = [
@@ -11,8 +12,18 @@
     ];
 </script>
 
+Avatar
 <div class={fr}>
     {#each users as user}
         <Avatar {user} />
+    {/each}
+</div>
+Avatar with hover
+<div class={fr}>
+    {#each users as user}
+        <AvatarWithHover
+            {user}
+            on:click={() => console.log("AvatarWithHover", user)}
+        />
     {/each}
 </div>
