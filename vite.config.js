8215b562c2c2cb5caa067aa4ef4e8ea10ac20074
Justus Perlwitz <mail@justusperlwitz.com>
2022-08-05 13:01:24 +0900

    Vite: Remove server config

--- vite.config.js
+++ vite.config.js
@@ -16,22 +16,5 @@ const config = {
     build: {
         target: ["es2020"],
     },
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-            },
-            "/graphql": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-            },
-            "/ws": {
-                target: "ws://127.0.0.1:8000",
-                ws: true,
-            },
-        },
-    },
 };
 export default config;
