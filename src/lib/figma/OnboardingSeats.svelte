deb8c113aa64a6cc78ffc6e0c74d2f2d17c8a537
dopedsi <89956710+dopedsi@users.noreply.github.com>
2022-08-31 18:11:03 +0900

    Lib/Figma: Update OnboardingSeats

--- src/lib/figma/OnboardingSeats.svelte
+++ src/lib/figma/OnboardingSeats.svelte
@@ -12,7 +12,7 @@
     class="group rounded-llg border border-transparent p-0.5 text-xl font-bold focus:border-base-content focus:outline-none"
 >
     <div
-        class="rounded-lg border border-base-300 px-2 py-1.5 group-hover:bg-secondary group-active:bg-primary-hover group-active:text-base-100"
+        class="rounded-lg border border-base-300 px-2 py-1.5 group-hover:bg-secondary-hover group-active:bg-primary-hover group-active:text-base-100"
     >
         {seats}
     </div>
