7d23282d198c3d6dc665c9b2d303aecb5ea5b83d
Justus Perlwitz <mail@justusperlwitz.com>
2022-09-30 18:45:24 +0900

    Lib/Types: Remove unused types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,10 +11,6 @@ import type {
     Workspace,
 } from "$lib/types/workspace";
 
-export interface Locals {
-    userid: string;
-}
-
 export type Input = {
     name?: string;
     label?: string;
@@ -40,37 +36,6 @@ export type Input = {
         // maxLength?: number;
         // pattern?: string;
     };
-};
-
-export type ThemeColors = {
-    "primary": string;
-    "primary-focus": string;
-    "primary-content": string;
-    "secondary": string;
-    "secondary-focus": string;
-    "secondary-content": string;
-    "accent": string;
-    "accent-focus": string;
-    "accent-content": string;
-    "neutral": string;
-    "neutral-focus": string;
-    "neutral-content": string;
-    "base-100": string;
-    "base-200": string;
-    "base-300": string;
-    "base-content": string;
-    "info": string;
-    "success": string;
-    "warning": string;
-    "error": string;
-};
-
-export type Color = {
-    h: number;
-    s: number;
-    l: number;
-    br: boolean;
-    style: string;
 };
 
 export type LabelSelectionInput =
