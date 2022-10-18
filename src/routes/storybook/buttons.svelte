490fe1f6084a12ed70928d0b7f442ea08f39d0a4
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-18 10:31:51 +0900

    Lib/Figma: Move SeatInput

--- src/routes/storybook/buttons.svelte
+++ src/routes/storybook/buttons.svelte
@@ -16,7 +16,7 @@
     import OnboardingSeats from "$lib/figma/buttons/OnboardingSeats.svelte";
     import MinusSeats from "$lib/figma/MinusSeats.svelte";
     import AddSeats from "$lib/figma/buttons/AddSeats.svelte";
-    import SeatInput from "$lib/figma/SeatInput.svelte";
+    import SeatInput from "$lib/figma/buttons/SeatInput.svelte";
     import StepperComponent from "$lib/figma/buttons/StepperComponent.svelte";
     import Stepper from "$lib/figma/buttons/Stepper.svelte";
     import MobileMenu from "$lib/figma/buttons/MobileMenu.svelte";
