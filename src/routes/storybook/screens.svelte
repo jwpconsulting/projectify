d26cf7ca56fb05664d00142f3a2640801023ea16
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-18 18:25:42 +0900

    Lib/Figma: Move SignUp

--- src/routes/storybook/screens.svelte
+++ src/routes/storybook/screens.svelte
@@ -6,7 +6,7 @@
     import WorkspaceSettingsMembers from "$lib/figma/screens/WorkspaceSettingsMembers.svelte";
     import WorkspaceSettingsBilling from "$lib/figma/screens/WorkspaceSettingsBilling.svelte";
     import LogIn from "$lib/figma/screens/LogIn.svelte";
-    import SignUp from "$lib/figma/SignUp.svelte";
+    import SignUp from "$lib/figma/screens/SignUp.svelte";
     import RequestPasswordReset from "$lib/figma/RequestPasswordReset.svelte";
     import ConfirmPasswordReset from "$lib/figma/ConfirmPasswordReset.svelte";
     import { settingKinds } from "$lib/types/dashboard";
