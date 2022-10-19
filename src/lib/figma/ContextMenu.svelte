09b06cfe3aef22c2b13309779d6fc11f166d1429
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-19 13:51:16 +0900

    Lib/Figma: Move WorkspaceOverlay

--- src/lib/figma/ContextMenu.svelte
+++ src/lib/figma/ContextMenu.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import ProfileOverlay from "$lib/figma/overlays/ProfileOverlay.svelte";
-    import WorkspaceOverlay from "$lib/figma/WorkspaceOverlay.svelte";
+    import WorkspaceOverlay from "$lib/figma/overlays/WorkspaceOverlay.svelte";
     import SideNavOverlay from "$lib/figma/overlays/SideNavOverlay.svelte";
     import WorkspaceBoardOverlay from "$lib/figma/overlays/WorkspaceBoardOverlay.svelte";
     import WorkspaceBoardSectionOverlay from "$lib/figma/overlays/WorkspaceBoardSectionOverlay.svelte";
