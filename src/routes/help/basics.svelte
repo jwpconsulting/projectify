316586b6b837c68a54c1840f5d05f70ed25ea2c8
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-29 01:19:34 +0900

    Routes/Help: Update basics

--- src/routes/help/basics.svelte
+++ src/routes/help/basics.svelte
@@ -29,30 +29,32 @@
     ];
 </script>
 
-<div class="page bg base-200 space-y-4 p-8 sm:hidden">
-    <p class="text-xl font-bold">
-        {$_("help.skip")}
-    </p>
-    <ul class="space-y-1 text-primary">
-        {#each sections as section}
-            <li>
-                <a href={`#${section.id}`}>
-                    {section.title}
-                </a>
-            </li>
-        {/each}
-    </ul>
-</div>
+<div>
+    <div class="page bg base-200 space-y-4 p-8">
+        <p class="text-xl font-bold">
+            {$_("help.skip")}
+        </p>
+        <ul class="space-y-1 text-primary">
+            {#each sections as section}
+                <li>
+                    <a href={`#${section.id}`}>
+                        {section.title}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </div>
 
-<div class="m-8 space-y-16">
-    {#each sections as section}
-        <section id="workspace" class="space-y-4">
-            <h3 class="text-3xl font-bold">
-                {$_("help-basics.workspace")}
-            </h3>
-            <p>
-                {section.content}
-            </p>
-        </section>
-    {/each}
+    <div class="my-8 space-y-16">
+        {#each sections as section}
+            <section id="workspace" class="space-y-4">
+                <h3 class="text-3xl font-bold">
+                    {$_("help-basics.workspace")}
+                </h3>
+                <p>
+                    {section.content}
+                </p>
+            </section>
+        {/each}
+    </div>
 </div>
