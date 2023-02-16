b4efc0a873aefcc5ccc38f41acd673a150a383c2
Justus Perlwitz <hello@justus.pw>
2023-02-16 09:44:17 +0900

    Lib/Figma: Add MobileMenuOverlay

---
+++ src/lib/figma/overlays/MobileMenuOverlay.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import Button from "$lib/figma/buttons/Button.svelte";
+    import MobileMenu from "$lib/figma/overlays/MobileMenu.svelte";
+</script>
+
+<div class="py-4 px-2">
+    <MobileMenu />
+    <div class="px-4">
+        <a href="/dashboard">
+            <Button
+                style={{ kind: "primary" }}
+                color="blue"
+                disabled={false}
+                size="medium"
+                label={$_("header.continue-to-dashboard")}
+            />
+        </a>
+    </div>
+</div>
---
+++ src/stories/figma/overlays/MobileMenuOverlay.stories.ts
@@ -0,0 +1,11 @@
+import MobileMenuOverlay from "$lib/figma/overlays/MobileMenuOverlay.svelte";
+
+const component = MobileMenuOverlay;
+
+export default {
+    component,
+};
+
+export const Default = () => ({
+    Component: component,
+});
