8c88d6eb74db2f388ddfc9ae6a3b0012429be5d3
Justus Perlwitz <hello@justus.pw>
2023-02-16 09:44:11 +0900

    Lib/Figma: Add MobileMenu

---
+++ src/lib/figma/overlays/MobileMenu.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import Button from "$lib/figma/buttons/Button.svelte";
+</script>
+
+<div class="mb-8 flex flex-col divide-y divide-border p-4">
+    <div class="py-2">
+        <a href="/features">
+            <Button
+                style={{ kind: "tertiary", icon: null }}
+                color="blue"
+                disabled={false}
+                size="medium"
+                label={$_("header.features")}
+            />
+        </a>
+    </div>
+    <div class="py-2">
+        <Button
+            style={{ kind: "tertiary", icon: null }}
+            color="blue"
+            disabled={false}
+            size="medium"
+            label={$_("header.solutions")}
+        />
+    </div>
+    <div class="py-2">
+        <Button
+            style={{ kind: "tertiary", icon: null }}
+            color="blue"
+            disabled={false}
+            size="medium"
+            label={$_("header.resources")}
+        />
+    </div>
+    <div class="py-2">
+        <Button
+            style={{ kind: "tertiary", icon: null }}
+            color="blue"
+            disabled={false}
+            size="medium"
+            label={$_("header.company")}
+        />
+    </div>
+    <div class="py-2">
+        <a href="/pricing">
+            <Button
+                style={{ kind: "tertiary", icon: null }}
+                color="blue"
+                disabled={false}
+                size="medium"
+                label={$_("header.pricing")}
+            />
+        </a>
+    </div>
+</div>
---
+++ src/stories/figma/overlays/MobileMenu.stories.ts
@@ -0,0 +1,11 @@
+import MobileMenu from "$lib/figma/overlays/MobileMenu.svelte";
+
+const component = MobileMenu;
+
+export default {
+    component,
+};
+
+export const Default = () => ({
+    Component: component,
+});
