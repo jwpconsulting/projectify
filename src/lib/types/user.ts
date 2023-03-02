f23813f96ba951a02e28941ba868f5101bea33bd
Justus Perlwitz <hello@justus.pw>
2023-03-02 17:01:26 +0900

    Lib/Types: Add user name getter

--- src/lib/types/user.ts
+++ src/lib/types/user.ts
@@ -3,3 +3,10 @@ export type User = {
     profile_picture?: string;
     full_name?: string;
 };
+
+export function getDisplayName(user: User): string {
+    if (user.full_name) {
+        return user.full_name;
+    }
+    return user.email;
+}
