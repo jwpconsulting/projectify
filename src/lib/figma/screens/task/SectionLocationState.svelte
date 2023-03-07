7892aa291f97f746678e46a76512a20abe4badfb
Justus Perlwitz <hello@justus.pw>
2023-03-07 16:34:38 +0900

    Lib/Figma: Add SectionLocationState

---
+++ src/lib/figma/screens/task/SectionLocationState.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    export let label: string;
+
+    const dispatch = createEventDispatcher();
+    function click() {
+        dispatch("click");
+    }
+</script>
+
+<button
+    on:click={click}
+    class="rounded-lg border border-transparent bg-task-hover p-2 text-xxs font-bold hover:bg-task-pressed focus:border-border-focus focus:bg-background focus:outline-none active:bg-secondary-hover"
+>
+    {label}
+</button>
