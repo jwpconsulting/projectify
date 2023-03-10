e8b79cce530f67ab516bb06575c85bcf9d2037f6
Justus Perlwitz <hello@justus.pw>
2023-03-10 18:05:28 +0900

    Bin: Update new_component

--- bin/new_component.sh
+++ bin/new_component.sh
@@ -2,16 +2,27 @@
 set -e
 path=$1
 component=$2
-cat <<EOF > "src/lib/$path/$component.svelte"
+component_file="lib/$path/$component.svelte"
+full_component_file="src/$component_file"
+stories_file="src/stories/$path/$component.stories.ts"
+
+if test ! -e "$component_file"
+then
+    cat <<EOF > "$component_file"
 <script lang="ts">
 </script>
 
 Hello, World
 EOF
-cat <<EOF > "src/stories/$path/$component.stories.ts"
+fi
+
+if test ! -e "$stories_file"
+then
+    mkdir -p "$(dirname $stories_file)"
+    cat <<EOF > "$stories_file"
 import type { Meta, StoryObj } from "@storybook/svelte";
 
-import $component from "\$lib/$path/$component.svelte";
+import $component from "\$$full_component_file";
 
 const meta: Meta<$component> = {
     component: $component,
@@ -25,3 +36,4 @@ type Story = StoryObj<$component>;
 export const Default: Story = {
 };
 EOF
+fi
