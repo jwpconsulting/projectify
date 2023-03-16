ada6e135740b870a62ef8f5b778e3b1f2c8bcef5
Justus Perlwitz <hello@justus.pw>
2023-03-16 15:40:20 +0900

    Lib/Figma/Navigation/SideNav: Boards

--- src/lib/figma/navigation/SideNav.svelte
+++ src/lib/figma/navigation/SideNav.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import WorkspaceMenu from "$lib/components/dashboard/WorkspaceMenu.svelte";
-    import SideNavBoards from "$lib/components/dashboard/SideNavBoards.svelte";
+    import Boards from "$lib/figma/navigation/side-nav/Boards.svelte";
     import SideNavMembers from "$lib/components/dashboard/SideNavMembers.svelte";
     import FilterLabelCollapsible from "$lib/figma/composites/FilterLabelCollapsible.svelte";
     import SideNavBulkSelect from "$lib/components/dashboard/SideNavBulkSelect.svelte";
@@ -9,7 +9,7 @@
 <nav class="flex h-full w-72 shrink-0 flex-col bg-base-100 py-4 pr-px">
     <WorkspaceMenu />
     <div class="flex flex-col overflow-x-auto overflow-y-scroll">
-        <SideNavBoards />
+        <Boards />
         <SideNavMembers />
         <FilterLabelCollapsible />
         <SideNavBulkSelect />
