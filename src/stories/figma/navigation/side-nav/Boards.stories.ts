2f15c35f0d3eb176fa5e262c94701fa7437e5e1b
Justus Perlwitz <hello@justus.pw>
2023-03-20 18:49:25 +0900

    Lib/Figma: Add Boards stories

---
+++ src/stories/figma/navigation/side-nav/Boards.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+
+import Boards from "$lib/figma/navigation/side-nav/Boards.svelte";
+
+import { workspace } from "$lib/storybook";
+
+const meta: Meta<Boards> = {
+    component: Boards,
+    argTypes: {},
+    args: {
+        workspace,
+    },
+};
+export default meta;
+
+type Story = StoryObj<Boards>;
+
+export const Default: Story = {};
