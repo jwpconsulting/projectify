9a187e0230788a27e71663a9777f851f90f4270d
Justus Perlwitz <hello@justus.pw>
2023-03-23 16:50:19 +0900

    Lib/Figma: Update SideNav stories

--- src/stories/figma/navigation/SideNav.stories.ts
+++ src/stories/figma/navigation/SideNav.stories.ts
@@ -2,12 +2,21 @@ import type { Meta, StoryObj } from "@storybook/svelte";
 
 import SideNav from "$lib/figma/navigation/SideNav.svelte";
 
+import { mobileParameters } from "$lib/storybook";
+
 const meta: Meta<SideNav> = {
     component: SideNav,
     argTypes: {},
+    parameters: {
+        layout: "fullscreen",
+    },
 };
 export default meta;
 
 type Story = StoryObj<SideNav>;
 
 export const Default: Story = {};
+
+export const Mobile: Story = {
+    parameters: mobileParameters,
+};
