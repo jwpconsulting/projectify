d256351d5dea7de202474a4fd34d0f4a5a6b7c87
Justus Perlwitz <hello@justus.pw>
2023-03-24 17:45:22 +0900

    Lib/Components/Dashboard: dashboard -> Dashboard

--- src/routes/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
+++ src/routes/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import AuthGuard from "$lib/components/authGuard.svelte";
-    import Dashboard from "$lib/components/dashboard/dashboard.svelte";
+    import Dashboard from "$lib/components/dashboard/Dashboard.svelte";
     import PageLayout from "$lib/components/layouts/pageLayout.svelte";
 </script>
 
