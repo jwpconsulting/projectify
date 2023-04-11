18bee6b620233f767a840f135c7d9cecb2f94c52
Justus Perlwitz <hello@justus.pw>
2023-04-11 15:16:26 +0900

    Lib/Routes: Populate new-task

--- src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/new-task/+page.svelte
+++ src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/new-task/+page.svelte
@@ -1,2 +1,39 @@
 <script lang="ts">
+    import Loading from "$lib/components/loading.svelte";
+    import { page } from "$app/stores";
+    import TaskUpdateCard from "$lib/figma/screens/task/TaskUpdateCard.svelte";
+    import type { NewTask } from "$lib/types/workspace";
+    import type { TaskOrNewTask } from "$lib/types/ui";
+
+    import {
+        currentWorkspaceBoardSection,
+        currentWorkspaceBoardSectionUuid,
+    } from "$lib/stores/dashboard";
+
+    $: {
+        $currentWorkspaceBoardSectionUuid =
+            $page.params["workspaceBoardSectionUuid"];
+        console.log($currentWorkspaceBoardSectionUuid);
+    }
+
+    let newTask: NewTask;
+    let taskOrNewTask: TaskOrNewTask;
+
+    $: {
+        if ($currentWorkspaceBoardSection) {
+            newTask = {
+                workspace_board_section: $currentWorkspaceBoardSection,
+            };
+            taskOrNewTask = {
+                kind: "newTask",
+                newTask,
+            };
+        }
+    }
 </script>
+
+{#if taskOrNewTask}
+    <TaskUpdateCard {taskOrNewTask} />
+{:else}
+    <Loading />
+{/if}
