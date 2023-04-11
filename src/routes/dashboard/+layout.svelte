16a174f704f0f829a1211df99bb687b467bed254
Justus Perlwitz <hello@justus.pw>
2023-04-11 11:16:55 +0900

    Routes: Refactor layout

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,93 +1,8 @@
 <script lang="ts">
     import AuthGuard from "$lib/components/AuthGuard.svelte";
     import HeaderDashboard from "$lib/figma/navigation/header/Dashboard.svelte";
-    import SideNav from "$lib/figma/navigation/SideNav.svelte";
-    import { openContextMenu } from "$lib/stores/global-ui";
-
-    import type {
-        LabelSearchModule,
-        SideNavModule,
-        WorkspaceBoardSearchModule,
-        WorkspaceSearchModule,
-        WorkspaceUserSearchModule,
-    } from "$lib/types/stores";
-
-    import {
-        currentWorkspace,
-        currentWorkspaceBoard,
-        currentWorkspaceBoardUuid,
-        deselectLabel,
-        deselectWorkspaceUser,
-        labelSearch,
-        labelSearchResults,
-        selectLabel,
-        selectWorkspaceUser,
-        selectedLabels,
-        selectedWorkspaceUser,
-        setWorkspaces,
-        sideNavOpen,
-        tasksPerUser,
-        toggleSideNavOpen,
-        workspaceUserSearch,
-        workspaceUserSearchResults,
-        workspaces,
-    } from "$lib/stores/dashboard";
 
     import { user } from "$lib/stores/user";
-
-    const workspaceSearchModule: WorkspaceSearchModule = {
-        workspaces,
-        currentWorkspace,
-        setWorkspaces,
-    };
-
-    const workspaceBoardSearchModule: WorkspaceBoardSearchModule = {
-        currentWorkspace,
-        currentWorkspaceBoard,
-        currentWorkspaceBoardUuid,
-    };
-    const workspaceUserSearchModule: WorkspaceUserSearchModule = {
-        select: selectWorkspaceUser,
-        deselect: deselectWorkspaceUser,
-        selected: selectedWorkspaceUser,
-        tasksPerUser,
-        search: workspaceUserSearch,
-        searchResults: workspaceUserSearchResults,
-    };
-
-    const labelSearchModule: LabelSearchModule = {
-        select: selectLabel,
-        deselect: deselectLabel,
-        selected: selectedLabels,
-        search: labelSearch,
-        searchResults: labelSearchResults,
-    };
-
-    let sideNavModule: SideNavModule;
-    $: sideNavModule = {
-        sideNavOpen,
-        toggleSideNavOpen,
-        showWorkspaceContextMenu: (anchor: HTMLElement) => {
-            openContextMenu(
-                { kind: "workspace", workspaceSearchModule },
-                anchor
-            );
-        },
-        showSideNavContextMenu: (anchor: HTMLElement) => {
-            if (!$currentWorkspace) {
-                throw new Error("Expected $currentWorkspace");
-            }
-            openContextMenu(
-                {
-                    kind: "sideNav",
-                    workspace: $currentWorkspace,
-                    // XXX circular reference, not cool Justus 2023-03-30
-                    sideNavModule,
-                },
-                anchor
-            );
-        },
-    };
 </script>
 
 <AuthGuard>
@@ -95,14 +10,6 @@
         {#if $user}
             <HeaderDashboard user={$user} />
         {/if}
-        <div class="flex grow flex-row">
-            <SideNav
-                {workspaceBoardSearchModule}
-                {workspaceUserSearchModule}
-                {labelSearchModule}
-                {sideNavModule}
-            />
-            <slot />
-        </div>
+        <slot />
     </div>
 </AuthGuard>
--- src/routes/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
+++ src/routes/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
@@ -2,20 +2,109 @@
     import Loading from "$lib/components/loading.svelte";
     import Dashboard from "$lib/components/dashboard/Dashboard.svelte";
     import { page } from "$app/stores";
+    import SideNav from "$lib/figma/navigation/SideNav.svelte";
+
+    import { openContextMenu } from "$lib/stores/global-ui";
+
+    import type {
+        LabelSearchModule,
+        SideNavModule,
+        WorkspaceBoardSearchModule,
+        WorkspaceSearchModule,
+        WorkspaceUserSearchModule,
+    } from "$lib/types/stores";
 
     import {
+        currentWorkspace,
         currentWorkspaceBoard,
         currentWorkspaceBoardUuid,
+        deselectLabel,
+        deselectWorkspaceUser,
+        labelSearch,
+        labelSearchResults,
+        selectLabel,
+        selectWorkspaceUser,
+        selectedLabels,
+        selectedWorkspaceUser,
+        setWorkspaces,
+        sideNavOpen,
+        tasksPerUser,
+        toggleSideNavOpen,
+        workspaceUserSearch,
+        workspaceUserSearchResults,
+        workspaces,
     } from "$lib/stores/dashboard";
 
     $: {
         $currentWorkspaceBoardUuid = $page.params["workspaceBoardUuid"];
         console.log($currentWorkspaceBoardUuid);
     }
+
+    const workspaceSearchModule: WorkspaceSearchModule = {
+        workspaces,
+        currentWorkspace,
+        setWorkspaces,
+    };
+
+    const workspaceBoardSearchModule: WorkspaceBoardSearchModule = {
+        currentWorkspace,
+        currentWorkspaceBoard,
+        currentWorkspaceBoardUuid,
+    };
+    const workspaceUserSearchModule: WorkspaceUserSearchModule = {
+        select: selectWorkspaceUser,
+        deselect: deselectWorkspaceUser,
+        selected: selectedWorkspaceUser,
+        tasksPerUser,
+        search: workspaceUserSearch,
+        searchResults: workspaceUserSearchResults,
+    };
+
+    const labelSearchModule: LabelSearchModule = {
+        select: selectLabel,
+        deselect: deselectLabel,
+        selected: selectedLabels,
+        search: labelSearch,
+        searchResults: labelSearchResults,
+    };
+
+    let sideNavModule: SideNavModule;
+    $: sideNavModule = {
+        sideNavOpen,
+        toggleSideNavOpen,
+        showWorkspaceContextMenu: (anchor: HTMLElement) => {
+            openContextMenu(
+                { kind: "workspace", workspaceSearchModule },
+                anchor
+            );
+        },
+        showSideNavContextMenu: (anchor: HTMLElement) => {
+            if (!$currentWorkspace) {
+                throw new Error("Expected $currentWorkspace");
+            }
+            openContextMenu(
+                {
+                    kind: "sideNav",
+                    workspace: $currentWorkspace,
+                    // XXX circular reference, not cool Justus 2023-03-30
+                    sideNavModule,
+                },
+                anchor
+            );
+        },
+    };
 </script>
 
 {#if $currentWorkspaceBoard}
-    <Dashboard workspaceBoard={$currentWorkspaceBoard} />
+    <div class="flex grow flex-row">
+        <SideNav
+            {workspaceBoardSearchModule}
+            {workspaceUserSearchModule}
+            {labelSearchModule}
+            {sideNavModule}
+        />
+        <Dashboard workspaceBoard={$currentWorkspaceBoard} />
+    </div>
 {:else}
     <Loading />
 {/if}
