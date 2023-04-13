43905b2b059d22082207b69cfe37e2d418de4d89
Justus Perlwitz <hello@justus.pw>
2023-04-13 16:17:11 +0900

    Lib/Figma: Fix TaskCard

--- src/lib/figma/cards/TaskCard.svelte
+++ src/lib/figma/cards/TaskCard.svelte
@@ -17,57 +17,64 @@
 </script>
 
 <a
-    class="box-border block w-full rounded-lg border border-base-300 bg-base-100 p-2"
+    class="block rounded-llg border-2 border-transparent focus:border-base-content"
     href="/"
 >
-    <div class="flex flex-col md:hidden">
-        <div class="flex flex-row justify-between">
-            <div class="">
-                <Title {task} />
+    <div class="w-full rounded-lg border border-base-300 bg-base-100 p-2">
+        <div class="flex flex-col md:hidden">
+            <div class="flex flex-row justify-between">
+                <div class="">
+                    <Title {task} />
+                </div>
+                <div class="flex flex-row items-center gap-2 justify-self-end">
+                    <Chevrons
+                        {task}
+                        {isFirst}
+                        {isLast}
+                        {workspaceBoardSection}
+                    />
+                    <MenuButton {task} {workspaceBoardSection} />
+                </div>
             </div>
-            <div class="flex flex-row items-center gap-2 justify-self-end">
-                <Chevrons {task} {isFirst} {isLast} {workspaceBoardSection} />
-                <MenuButton {task} {workspaceBoardSection} />
+            <div class="flex flex-row justify-between">
+                <div class="col-span-3 flex flex-row sm:col-span-4">
+                    <Labels {task} />
+                </div>
+                <div
+                    class="col-span-3 flex flex-row justify-self-end sm:col-span-2"
+                >
+                    <div class="flex flex-row items-center gap-4">
+                        <SubTaskProgress {task} />
+                    </div>
+                    <div class="flex flex-row items-center">
+                        <WorkspaceUser {task} />
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="flex flex-row justify-between">
-            <div class="col-span-3 flex flex-row sm:col-span-4">
+        <div class="hidden w-full flex-row justify-between md:flex">
+            <div class="flex flex-row items-center">
+                <Title {task} />
+            </div>
+            <div class="flex flex-row items-center justify-start gap-6">
                 <Labels {task} />
             </div>
-            <div
-                class="col-span-3 flex flex-row justify-self-end sm:col-span-2"
-            >
+            <div class="flex flex-row items-center justify-end gap-2">
                 <div class="flex flex-row items-center gap-4">
                     <SubTaskProgress {task} />
                 </div>
-                <div class="flex flex-row items-center">
+                <div class="flex flex-row items-center gap-2">
                     <WorkspaceUser {task} />
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="hidden w-full flex-row justify-between md:flex">
-        <div class="flex flex-row items-center">
-            <Title {task} />
-        </div>
-        <div class="flex flex-row items-center justify-start gap-6">
-            <Labels {task} />
-        </div>
-        <div class="flex flex-row items-center justify-end gap-2">
-            <div class="flex flex-row items-center gap-4">
-                <SubTaskProgress {task} />
-            </div>
-            <div class="flex flex-row items-center gap-2">
-                <WorkspaceUser {task} />
-                <div>
-                    <div class="flex flex-row items-center">
-                        <Chevrons
-                            {task}
-                            {isFirst}
-                            {isLast}
-                            {workspaceBoardSection}
-                        />
-                        <MenuButton {task} {workspaceBoardSection} />
+                    <div>
+                        <div class="flex flex-row items-center">
+                            <Chevrons
+                                {task}
+                                {isFirst}
+                                {isLast}
+                                {workspaceBoardSection}
+                            />
+                            <MenuButton {task} {workspaceBoardSection} />
+                        </div>
                     </div>
                 </div>
             </div>
