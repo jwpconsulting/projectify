ccba1115fa8a1a773b0ee78ce47ea1f6968ae7ba
Justus Perlwitz <hello@justus.pw>
2023-05-02 17:16:13 +0900

    Lib/Figma: Update LabelMenu stories

--- src/stories/figma/composites/LabelMenu.stories.ts
+++ src/stories/figma/composites/LabelMenu.stories.ts
@@ -3,14 +3,31 @@ import type { Meta, StoryObj } from "@storybook/svelte";
 import LabelMenu from "$lib/figma/composites/LabelMenu.svelte";
 
 import { labelSearchModule } from "$lib/storybook";
+import { filterLabelMenuStates } from "$lib/figma/types";
 
 const meta: Meta<LabelMenu> = {
     component: LabelMenu,
-    argTypes: {},
-    args: { labelSearchModule },
+    argTypes: {
+        state: {
+            control: "radio",
+            options: filterLabelMenuStates,
+        },
+    },
+    args: {
+        labelSearchModule,
+    },
 };
 export default meta;
 
 type Story = StoryObj<LabelMenu>;
 
-export const Default: Story = {};
+export const List: Story = {
+    args: {
+        state: "list",
+    },
+};
+export const Create: Story = {
+    args: {
+        state: "create",
+    },
+};
