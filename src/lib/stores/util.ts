1ea0ea752a0ab62f33a1533613e375d370cf58dd
Justus Perlwitz <hello@justus.pw>
2023-05-08 18:19:24 +0900

    Lib/Stores: Add ws derive util WIP

--- src/lib/stores/util.ts
+++ src/lib/stores/util.ts
@@ -1,5 +1,7 @@
-import { readonly, writable } from "svelte/store";
+import { derived, readonly, writable } from "svelte/store";
 import type { Readable, Writable } from "svelte/store";
+import { browser } from "$app/environment";
+import { getSubscriptionForCollection } from "$lib/stores/dashboardSubscription";
 
 export function internallyWritable<T>(theThing: T): {
     pub: Readable<T>;
@@ -10,5 +12,50 @@ export function internallyWritable<T>(theThing: T): {
     return {
         priv,
         pub,
+    };
+}
+
+type Unsubscriber = () => void;
+
+type UuidStore = Readable<string>;
+
+export function createWsStore<T>(
+    collection: "workspace" | "workspace-board" | "task",
+    uuidReadable: UuidStore,
+    getter: (uuid: string) => Promise<T>
+): Readable<T | null> {
+    const derive = ($uuidReadable: string, set: (t: T) => void) => {
+        if (!browser) {
+            return;
+        }
+        if (!$uuidReadable) {
+            return;
+        }
+        getter($uuidReadable).then((result) => set(result));
+        const subscription = getSubscriptionForCollection(
+            collection,
+            $uuidReadable
+        );
+        if (!subscription) {
+            throw new Error("Expected currentWorkspaceBoardSubscription");
+        }
+        const unsubscriber: Unsubscriber = subscription.subscribe(
+            async (_value) => {
+                console.log("Refetching workspaceBoard", $uuidReadable);
+                set(await getter($uuidReadable));
+            }
+        );
+        // Supposedly, return this function will allow svelte/store
+        // to call this when the derived store is unsubscribed fully
+        // TODO test this
+        return unsubscriber;
+    };
+    const { subscribe }: Readable<T | null> = derived<UuidStore, T | null>(
+        uuidReadable,
+        derive,
+        null
+    );
+    return {
+        subscribe,
     };
 }
