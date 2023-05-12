9bacdfbe509fd3d32fe7afc2f2b46fbf4167d6ed
Justus Perlwitz <hello@justus.pw>
2023-05-12 15:54:50 +0900

    Routes: Simplify create-task

--- src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/create-task/+page.svelte
+++ src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/create-task/+page.svelte
@@ -1,88 +1,66 @@
 <script lang="ts">
     import { readable, derived, writable } from "svelte/store";
     import Loading from "$lib/components/loading.svelte";
-    import { page } from "$app/stores";
     import TaskCreateCard from "$lib/figma/screens/task/TaskCreateCard.svelte";
     import type { CreateTask, WorkspaceUser } from "$lib/types/workspace";
     import type { TasksPerUser, WorkspaceUserSelection } from "$lib/types/ui";
     import { createLabelSearchModule } from "$lib/stores/modules";
     import type { CreateTaskModule } from "$lib/types/stores";
     import { createTask as createTaskFn } from "$lib/repository/workspace";
+    import type { PageData } from "./$types";
 
-    import {
-        currentWorkspaceBoardSection,
-        currentWorkspaceBoardSectionUuid,
-    } from "$lib/stores/dashboard";
+    export let data: PageData;
 
-    $: {
-        $currentWorkspaceBoardSectionUuid =
-            $page.params["workspaceBoardSectionUuid"];
-        console.log($currentWorkspaceBoardSectionUuid);
-    }
+    const { workspaceBoardSection } = data;
 
-    let createTaskModule: CreateTaskModule | null = null;
     const createTask = writable<Partial<CreateTask>>({});
 
-    $: {
-        // current workspace board section should be handed as data to this
-        // page
-        if ($currentWorkspaceBoardSection) {
-            const newTask = {
-                workspace_board_section: $currentWorkspaceBoardSection,
-            };
-            const { workspace_board: workspaceBoard } =
-                $currentWorkspaceBoardSection;
-            if (!workspaceBoard) {
-                throw new Error("Expected workspaceBoard");
-            }
-            const { workspace } = workspaceBoard;
-            if (!workspace) {
-                throw new Error("Expected workspace");
-            }
-
-            const labelSearchModule = createLabelSearchModule(
-                workspace,
-                null,
-                () => console.error("Not implemented")
-            );
-            createTaskModule = {
-                createOrUpdateTask: createOrUpdateTask,
-                createTask,
-                newTask,
-                canCreateOrUpdate: derived<[typeof createTask], boolean>(
-                    [createTask],
-                    ([$createTask], set) => {
-                        set($createTask !== null);
-                    },
-                    false
-                ),
-                // XXX this requires special logic because we can only
-                // assign users/labels after the task is already created
-                // TODO make workspace user menu so that "all" can not be
-                // selected
-                workspaceUserSearchModule: {
-                    select: console.error,
-                    deselect: console.error,
-                    selected: writable<WorkspaceUserSelection>(),
-                    // XXX find a way to postpone this, albeit useful, showing
-                    // the amount of tasks per users right from the beginning
-                    // will be more work
-                    tasksPerUser: readable<TasksPerUser>(),
-                    search: writable(""),
-                    searchResults: readable<WorkspaceUser[]>([]),
-                },
-                // TODO make label menu so that "all" can not be selected
-                labelSearchModule,
-            };
-        }
+    const newTask = {
+        workspace_board_section: workspaceBoardSection,
+    };
+    const { workspace_board: workspaceBoard } = workspaceBoardSection;
+    if (!workspaceBoard) {
+        throw new Error("Expected workspaceBoard");
+    }
+    const { workspace } = workspaceBoard;
+    if (!workspace) {
+        throw new Error("Expected workspace");
     }
 
+    const labelSearchModule = createLabelSearchModule(workspace, null, () =>
+        console.error("Not implemented")
+    );
+    const createTaskModule: CreateTaskModule = {
+        createOrUpdateTask: createOrUpdateTask,
+        createTask,
+        newTask,
+        canCreateOrUpdate: derived<[typeof createTask], boolean>(
+            [createTask],
+            ([$createTask], set) => {
+                set($createTask !== null);
+            },
+            false
+        ),
+        // XXX this requires special logic because we can only
+        // assign users/labels after the task is already created
+        // TODO make workspace user menu so that "all" can not be
+        // selected
+        workspaceUserSearchModule: {
+            select: console.error,
+            deselect: console.error,
+            selected: writable<WorkspaceUserSelection>(),
+            // XXX find a way to postpone this, albeit useful, showing
+            // the amount of tasks per users right from the beginning
+            // will be more work
+            tasksPerUser: readable<TasksPerUser>(),
+            search: writable(""),
+            searchResults: readable<WorkspaceUser[]>([]),
+        },
+        // TODO make label menu so that "all" can not be selected
+        labelSearchModule,
+    };
+
     function createOrUpdateTask() {
-        // TODO
-        // Get rid of $currentWorkspaceBoardSection store
-        if (!createTaskModule) {
-            throw new Error("FIXME");
-        }
         const { title, description } = $createTask;
         if (!title || !description) {
             throw new Error("Expected title and description");
--- src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/create-task/+page.ts
+++ src/routes/dashboard/workspace-board-section/[workspaceBoardSectionUuid]/create-task/+page.ts
@@ -1,1 +1,22 @@
+import type { PageLoadEvent } from "./$types";
+import type { WorkspaceBoardSection } from "$lib/types/workspace";
+import { getWorkspaceBoardSection } from "$lib/repository/workspace";
+
+interface Data {
+    workspaceBoardSection: WorkspaceBoardSection;
+}
+
+export async function load({
+    params: { workspaceBoardSectionUuid },
+    fetch,
+}: PageLoadEvent): Promise<Data> {
+    // If thing is fetched, use the fetch argument above
+    const workspaceBoardSection = await getWorkspaceBoardSection(
+        workspaceBoardSectionUuid,
+        { fetch }
+    );
+    return { workspaceBoardSection };
+}
+
 export const prerender = false;
+export const ssr = false;
