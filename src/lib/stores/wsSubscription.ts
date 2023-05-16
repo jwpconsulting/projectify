5a0ba7b8b2aa0f5a757fd0fc25835938ba66185d
Justus Perlwitz <hello@justus.pw>
2023-05-16 13:31:18 +0900

    Lib: Introduce sarus for ws

--- src/lib/stores/wsSubscription.ts
+++ src/lib/stores/wsSubscription.ts
@@ -1,4 +1,4 @@
-import delay from "delay";
+import Sarus from "@anephenix/sarus";
 import type { Subscriber } from "svelte/store";
 import { writable } from "svelte/store";
 import { browser } from "$app/environment";
@@ -14,42 +14,15 @@ const wsSubscriptionStores = new Map<string, WSSubscriptionStore>();
 export const activeWSSubscriptions = writable(0);
 export const activeWSConnections = writable(0);
 
-const retryTimeStart = 2000;
-const retryTimeMult = 1.1;
-const retryTimeJitter = 500;
-const maxRetryTime = 20000;
-
 export class WSSubscriptionStore {
-    wsMessage: WSMessage | null = null;
+    sarus: Sarus;
     subscribers: WSSubscriber[] = [];
-    socket: WebSocket;
-    socketActive = true;
     url: string;
-
-    retryingConnection = false;
-
-    retryTime: number;
 
     constructor(url: string) {
         this.url = url;
-        this.retryTime = retryTimeStart;
-        this.socket = this.createNewConnection();
-        startWatchDog();
-        this.debug("constructor");
-    }
-
-    deleteConnection(): void {
-        this.socket.onmessage = null;
-        this.socket.onclose = null;
-        this.socket.onerror = null;
-        this.socket.close();
-        this.socketActive = false;
-    }
-
-    createNewConnection(): WebSocket {
-        this.debug("Creating new connection");
-        const socket = new WebSocket(this.url);
-        socket.onmessage = ({
+        this.sarus = new Sarus({ url });
+        const onmessage = ({
             data,
             timeStamp,
         }: {
@@ -57,81 +30,33 @@ export class WSSubscriptionStore {
             timeStamp: number;
         }) => {
             this.debug("socket.onmessage", data, timeStamp);
-            this.wsMessage = {
+            this.dispatch({
                 message: data,
                 at: timeStamp,
-            };
-            this.dispatch();
+            });
         };
-        socket.onopen = (event) => {
-            this.debug("socket.onopen", event);
-            this.retryTime = retryTimeStart;
-            this.retryingConnection = false;
-            // this.dispatch();
-        };
-        socket.onclose = async (event) => {
-            this.debug("socket.onclose", event);
-            await this.retryConnection();
-        };
-        socket.onerror = async (event) => {
-            this.debug("socket.onerror", event);
-            await this.retryConnection();
-        };
-        return socket;
+        this.sarus.on("message", onmessage);
     }
 
-    recreateConnection() {
-        this.debug("Recreating connection");
-        this.deleteConnection();
-        this.createNewConnection();
-    }
-
-    async retryConnection(now = false) {
-        this.debug("Retrying connection", { now });
-        if (this.retryingConnection) {
-            this.debug("Already retrying connection");
-            return;
-        }
-
-        this.retryingConnection = true;
-
-        const delayTime = this.retryTime + Math.random() * retryTimeJitter;
-
-        this.retryTime *= retryTimeMult;
-        this.retryTime = Math.min(this.retryTime, maxRetryTime);
-
-        if (now) {
-            this.retryTime = 0;
-        } else {
-            await delay(delayTime);
-        }
-
-        if (this.socket.readyState == WebSocket.CLOSED) {
-            this.debug("Socket is closed");
-            this.recreateConnection();
-        }
-    }
-
-    dispatch() {
-        this.debug("Dispatching", this.wsMessage, this.subscribers);
+    dispatch(wsMessage: WSMessage) {
+        this.debug("Dispatching", wsMessage, "to", this.subscribers);
         this.subscribers.forEach((subscriber) => {
-            subscriber(this.wsMessage);
+            subscriber(wsMessage);
         });
     }
 
-    public subscribe(subscriber: WSSubscriber): () => Promise<void> {
+    public subscribe(subscriber: WSSubscriber): () => void {
         this.subscribers.push(subscriber);
-        this.debug("Pushing", subscriber);
+        this.debug("Subscribing", subscriber);
 
-        return async () => {
+        return () => {
             const index = this.subscribers.indexOf(subscriber);
             if (index !== -1) {
                 this.subscribers.splice(index, 1);
             }
             if (!this.subscribers.length) {
-                this.deleteConnection();
+                this.sarus.disconnect();
                 wsSubscriptionStores.delete(this.url);
-                await stopWatchDog();
             }
         };
     }
@@ -154,79 +79,6 @@ export function getSubscriptionFor(url: string): WSSubscriptionStore | null {
     wsSubscriptionStores.set(url, newStore);
     return newStore;
 }
-
-let watchDogTimer: number | null = null;
-let watchDogLastTime = 0;
-const watchDogInterval = 1000;
-
-function startWatchDog(): void {
-    if (!browser) {
-        return;
-    }
-    if (watchDogTimer) {
-        console.log("Watchdog timer already created");
-        return;
-    }
-    if (watchDogLastTime == 0) {
-        watchDogLastTime = Date.now();
-    }
-    if (watchDogTimer) {
-        window.clearInterval(watchDogTimer);
-    }
-    watchDogTimer = window.setInterval(async () => {
-        const now = Date.now();
-        watchDogLastTime = now;
-
-        await checkAllConnectionStatus();
-    }, watchDogInterval);
-}
-
-async function stopWatchDog(): Promise<void> {
-    if (!watchDogTimer) {
-        throw new Error("Expected watchDogTimer");
-    }
-    const connectionActive = wsSubscriptionStores.size > 0;
-    if (!connectionActive) {
-        window.clearInterval(watchDogTimer);
-    }
-    await checkAllConnectionStatus();
-}
-
-async function checkAllConnectionStatus() {
-    const activeWSS = wsSubscriptionStores.size;
-    let activeCon = 0;
-
-    wsSubscriptionStores.forEach((wsSubscriptionStore) => {
-        const { socketActive } = wsSubscriptionStore;
-        const { readyState } = wsSubscriptionStore.socket;
-        const openOrConnecting =
-            readyState == WebSocket.OPEN || readyState == WebSocket.CONNECTING;
-        const { retryingConnection } = wsSubscriptionStore;
-        const isConnected = socketActive && openOrConnecting;
-        if (isConnected || retryingConnection) {
-            activeCon++;
-        } else {
-            console.log({ wsSubscriptionStore, socketActive, readyState });
-        }
-    });
-
-    activeWSSubscriptions.set(activeWSS);
-    activeWSConnections.set(activeCon);
-
-    if (activeWSS === activeCon) {
-        return;
-    }
-    console.log("Retrying all connections");
-    const promises = [...wsSubscriptionStores.values()].map(
-        (wsSubscriptionStore) => {
-            return wsSubscriptionStore.retryConnection(true);
-        }
-    );
-    await Promise.all(promises);
-    // XXX
-    // This enters an infite loop here
-}
-
 // Online connection
 
 export const online = writable(true);
