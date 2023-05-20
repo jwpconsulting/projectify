1b89d90d9bc6c32a5471ec7bd6c08817e58e244c
Justus Perlwitz <hello@justus.pw>
2023-05-20 16:30:31 +0900

    Type more

--- user/schema/mutation.py
+++ user/schema/mutation.py
@@ -1,9 +1,19 @@
 """User schema mutations."""
+from typing import (
+    TYPE_CHECKING,
+    Type,
+    cast,
+)
+
 from django.contrib import (
     auth,
 )
 
 import strawberry
+
+from graphql import (
+    GraphQLResolveInfo,
+)
 
 from ..emails import (
     UserEmailConfirmationEmail,
@@ -12,6 +22,10 @@ from ..emails import (
 from . import (
     types,
 )
+
+
+if TYPE_CHECKING:
+    from ..models import User as _User  # noqa: F401
 
 
 @strawberry.input
@@ -68,14 +82,14 @@ class Mutation:
     @strawberry.mutation
     def signup(self, input: SignupInput) -> types.User:
         """Mutate."""
-        User = auth.get_user_model()
+        User = cast(Type["_User"], auth.get_user_model())
         user = User.objects.create_user(
             email=input.email,
             password=input.password,
         )
         mail = UserEmailConfirmationEmail(user)
         mail.send()
-        return user
+        return user  # type: ignore
 
     @strawberry.mutation
     def email_confirmation(
@@ -83,47 +97,53 @@ class Mutation:
         input: EmailConfirmationInput,
     ) -> types.User | None:
         """Mutate."""
-        User = auth.get_user_model()
+        User = cast(Type["_User"], auth.get_user_model())
         user = User.objects.get_by_natural_key(input.email)
         if user.check_email_confirmation_token(input.token):
             user.is_active = True
             user.save()
-            return user
+            return user  # type: ignore
+        return None
 
     @strawberry.mutation
-    def login(self, input: LoginInput, info) -> types.User | None:
+    def login(
+        self, input: LoginInput, info: GraphQLResolveInfo
+    ) -> types.User | None:
         """Mutate."""
         from django.contrib.auth.backends import (
             ModelBackend,
         )
 
-        user = ModelBackend().authenticate(
-            info.context,
-            username=input.email,
-            password=input.password,
+        user = cast(
+            "_User",
+            ModelBackend().authenticate(
+                info.context,
+                username=input.email,
+                password=input.password,
+            ),
         )
         if user is None:
-            return
+            return None
         auth.login(
             info.context,
             user,
             backend="django.contrib.auth.backends.ModelBackend",
         )
-        return user
+        return user  # type: ignore
 
     @strawberry.mutation
-    def logout(self, info) -> types.User | None:
+    def logout(self, info: GraphQLResolveInfo) -> types.User | None:
         """Mutate."""
-        user = info.context.user
+        user = cast("_User", info.context.user)
         if user.is_anonymous:
-            return
+            return None
         auth.logout(info.context)
-        return user
+        return user  # type: ignore
 
     @strawberry.mutation
     def request_password_reset(self, input: RequestPasswordResetInput) -> str:
         """Mutate."""
-        User = auth.get_user_model()
+        User = cast(Type["_User"], auth.get_user_model())
         user = User.objects.get_by_natural_key(input.email)
         password_reset_email = UserPasswordResetEmail(user)
         password_reset_email.send()
@@ -135,24 +155,24 @@ class Mutation:
         input: ConfirmPasswordResetInput,
     ) -> types.User | None:
         """Mutate."""
-        User = auth.get_user_model()
+        User = cast(Type["_User"], auth.get_user_model())
         user = User.objects.get_by_natural_key(input.email)
         if user.check_password_reset_token(input.token):
             user.set_password(input.new_password)
             user.save()
-            return user
-        return
+            return user  # type: ignore
+        return None
 
     @strawberry.mutation
     def update_profile(
         self,
         input: UpdateProfileInput,
-        info,
+        info: GraphQLResolveInfo,
     ) -> types.User | None:
         """Mutate."""
-        user = info.context.user
+        user = cast("_User", info.context.user)
         if not user.is_authenticated:
-            return
+            return None
         user.full_name = input.full_name
         user.save()
-        return user
+        return user  # type: ignore
