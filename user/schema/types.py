e85b3d75a0f6d3b8021f8ea8796071ce5556b99e
Justus Perlwitz <hello@justus.pw>
2023-05-20 16:21:02 +0900

    Mypy: Refactor config

--- user/schema/types.py
+++ user/schema/types.py
@@ -1,9 +1,18 @@
 """User schema types."""
+from typing import (
+    TYPE_CHECKING,
+    cast,
+)
+
 from django.contrib import (
     auth,
 )
 
 import strawberry
+
+
+if TYPE_CHECKING:
+    from ..models import User as _User  # noqa: F401
 
 
 @strawberry.django.type(auth.get_user_model())
@@ -16,5 +25,7 @@ class User:
     @strawberry.field
     def profile_picture(self) -> str | None:
         """Resolve profile_picture."""
-        if self.profile_picture:
-            return self.profile_picture.url
+        user = cast("_User", self)
+        if user.profile_picture:
+            return user.profile_picture.url
+        return None
