f7bb6e8a63e603ce2e32106fac5fa313b53a51de
Justus Perlwitz <hello@justus.pw>
2023-05-25 21:18:23 +0900

    Type rules

--- workspace/test/test_rules.py
+++ workspace/test/test_rules.py
@@ -1,4 +1,8 @@
 """Test workspace app rules."""
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+)
+
 import pytest
 
 from corporate import models as corporate_models
@@ -11,9 +15,11 @@ from .. import (
 
 
 @pytest.fixture
-def observer(workspace, user):
+def observer(
+    workspace: models.Workspace, user: AbstractBaseUser
+) -> models.WorkspaceUser:
     """Return an observer workspace user."""
-    return factory.WorkspaceUserFactory(
+    return factory.WorkspaceUserFactory.create(
         workspace=workspace,
         user=user,
         role=models.WorkspaceUserRoles.OBSERVER,
@@ -24,55 +30,86 @@ def observer(workspace, user):
 class TestPredicates:
     """Test predicates."""
 
-    def test_is_at_least_observer(self, user, workspace, observer):
+    def test_is_at_least_observer(
+        self,
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_observer."""
         # In the beginning the user is owner
         assert rules.is_at_least_observer(user, workspace)
 
-    def test_is_at_least_observer_other_workspace(self, user, other_workspace):
+    def test_is_at_least_observer_other_workspace(
+        self,
+        user: AbstractBaseUser,
+        other_workspace: models.Workspace,
+    ) -> None:
         """Test is_at_least_observer with other workspace."""
         assert not rules.is_at_least_observer(user, other_workspace)
 
-    def test_is_at_least_member(self, user, workspace, observer):
+    def test_is_at_least_member(
+        self,
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_member."""
         assert not rules.is_at_least_member(user, workspace)
         observer.assign_role(models.WorkspaceUserRoles.MEMBER)
         assert rules.is_at_least_member(user, workspace)
 
     def test_is_at_least_member_other_workspace(
-        self, user, other_workspace, observer
-    ):
+        self,
+        user: AbstractBaseUser,
+        other_workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_member with other workspace."""
         assert not rules.is_at_least_member(user, other_workspace)
 
-    def test_is_at_least_maintainer(self, user, workspace, observer):
+    def test_is_at_least_maintainer(
+        self,
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_maintainer."""
         assert not rules.is_at_least_maintainer(user, workspace)
         observer.assign_role(models.WorkspaceUserRoles.MAINTAINER)
         assert rules.is_at_least_maintainer(user, workspace)
 
     def test_is_at_least_maintainer_other_workspace(
-        self, user, other_workspace
-    ):
+        self, user: AbstractBaseUser, other_workspace: models.Workspace
+    ) -> None:
         """Test is_at_least_maintainer with other workspace."""
         assert not rules.is_at_least_maintainer(user, other_workspace)
 
-    def test_is_at_least_owner(self, user, workspace, observer):
+    def test_is_at_least_owner(
+        self,
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_owner."""
         assert not rules.is_at_least_owner(user, workspace)
         observer.assign_role(models.WorkspaceUserRoles.OWNER)
         assert rules.is_at_least_owner(user, workspace)
 
-    def test_is_at_least_owner_other_workspace(self, user, other_workspace):
+    def test_is_at_least_owner_other_workspace(
+        self,
+        user: AbstractBaseUser,
+        other_workspace: models.WorkspaceUser,
+    ) -> None:
         """Test is_at_least_owner with other workspace."""
         assert not rules.is_at_least_owner(user, other_workspace)
 
     def test_belongs_to_active_workspace(
         self,
-        user,
-        workspace,
-        observer,
-    ):
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test belongs_to_active_workspace."""
         # Active
         assert rules.belongs_to_active_workspace(
@@ -89,8 +126,11 @@ class TestPredicates:
         )
 
     def test_belongs_to_active_workspace_no_customer(
-        self, user, workspace, observer
-    ):
+        self,
+        user: AbstractBaseUser,
+        workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test belongs_to_active_workspace."""
         # The workspace fixture creates an active customer so we have to delete
         # it
@@ -103,8 +143,11 @@ class TestPredicates:
         )
 
     def test_belongs_to_active_workspace_other_workspace(
-        self, user, other_workspace, observer
-    ):
+        self,
+        user: AbstractBaseUser,
+        other_workspace: models.Workspace,
+        observer: models.WorkspaceUser,
+    ) -> None:
         """Test belongs_to_active_workspace with other workspace."""
         assert not rules.belongs_to_active_workspace(
             user,
