32f4c200ecf8cc28df6e07b27a6c018bc8a5fe48
Justus Perlwitz <hello@justus.pw>
2023-05-25 16:59:43 +0900

    Projectify: Type more

--- projectify/test/test_utils.py
+++ projectify/test/test_utils.py
@@ -1,5 +1,9 @@
 """Test projectify utils."""
 import os
+from typing import (
+    TYPE_CHECKING,
+    Any,
+)
 from unittest import (
     mock,
 )
@@ -11,24 +15,34 @@ from .. import (
 )
 
 
+if TYPE_CHECKING:
+    from django.db.models import (
+        FieldFile,
+    )
+
+
 class TestCropImage:
     """Test crop_image."""
 
     @pytest.fixture
-    def image(self):
+    def image(self) -> "FieldFile":
         """Image fixture."""
         image = mock.MagicMock()
         image.name = "hello_world"
         image.url = "https://www.example.com/hello_world.jpg"
         return image
 
-    def test_with_none(self):
+    def test_with_none(self) -> None:
         """Test with nothing."""
         url = utils.crop_image(None, 100, 100)
         assert url is None
 
     @mock.patch.dict(os.environ, {"CLOUDINARY_URL": "https://example.com"})
-    def test_with_cloudinary(self, image, settings):
+    def test_with_cloudinary(
+        self,
+        image: "FieldFile",
+        settings: Any,
+    ) -> None:
         """Test with cloudinary file storage."""
         settings.DEFAULT_FILE_STORAGE = settings.MEDIA_CLOUDINARY_STORAGE
         url = utils.crop_image(image, 100, 100, cloud_name="bbbbbbbbb")
@@ -37,7 +51,7 @@ class TestCropImage:
             "/image/upload/c_thumb,g_face,h_100,w_100/hello_world"
         )
 
-    def test_with_local(self, image):
+    def test_with_local(self, image: "FieldFile") -> None:
         """Test with cloudinary file storage."""
         url = utils.crop_image(image, 100, 100)
         assert url == image.url
