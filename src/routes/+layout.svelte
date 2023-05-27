1be4dea95b31a5274a28c369c7ce057a46d358b1
Justus Perlwitz <hello@justus.pw>
2023-05-27 15:02:02 +0900

    Routes: Refactor layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,14 @@
 <script lang="ts">
     import "../app.scss";
     import "../i18n.js";
-    import ConnectionStatus from "$lib/components/connectionStatus.svelte";
-    import GlobalDropDown from "$lib/components/globalDropDown.svelte";
-    import OverlayContainer from "$lib/components/OverlayContainer.svelte";
-    import ContextMenuContainer from "$lib/components/ContextMenuContainer.svelte";
-    import DestructiveOverlay from "$lib/figma/overlays/DestructiveOverlay.svelte";
-    import ConstructiveOverlay from "$lib/figma/overlays/constructive/ConstructiveOverlay.svelte";
-    import {
-        closeConstructiveOverlay,
-        closeDestructiveOverlay,
-        constructiveOverlayState,
-        destructiveOverlayState,
-        performDestructiveOverlay,
-    } from "$lib/stores/globalUi";
 </script>
 
 <svelte:head>
     <title>Projectify</title>
-    <meta name="description" content="Projects menagemet app" />
+    <meta
+        name="description"
+        content="Projectify, free open source project managementProjects management app"
+    />
 </svelte:head>
 
 <slot />
-
-<ConnectionStatus />
-
-<GlobalDropDown />
-
-<OverlayContainer
-    overlay={DestructiveOverlay}
-    store={destructiveOverlayState}
-    close={closeDestructiveOverlay}
-    perform={performDestructiveOverlay}
-/>
-
-<OverlayContainer
-    overlay={ConstructiveOverlay}
-    store={constructiveOverlayState}
-    close={closeConstructiveOverlay}
-    perform={null}
-/>
-
-<ContextMenuContainer />
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
     import AuthGuard from "$lib/components/AuthGuard.svelte";
     import HeaderDashboard from "$lib/figma/navigation/header/Dashboard.svelte";
+    import ConnectionStatus from "$lib/components/connectionStatus.svelte";
+    import GlobalDropDown from "$lib/components/globalDropDown.svelte";
+    import OverlayContainer from "$lib/components/OverlayContainer.svelte";
+    import ContextMenuContainer from "$lib/components/ContextMenuContainer.svelte";
+    import DestructiveOverlay from "$lib/figma/overlays/DestructiveOverlay.svelte";
+    import ConstructiveOverlay from "$lib/figma/overlays/constructive/ConstructiveOverlay.svelte";
+    import {
+        closeConstructiveOverlay,
+        closeDestructiveOverlay,
+        constructiveOverlayState,
+        destructiveOverlayState,
+        performDestructiveOverlay,
+    } from "$lib/stores/globalUi";
 
     import { user } from "$lib/stores/user";
 </script>
@@ -13,3 +26,23 @@
         <slot />
     </div>
 </AuthGuard>
+
+<ConnectionStatus />
+
+<GlobalDropDown />
+
+<OverlayContainer
+    overlay={DestructiveOverlay}
+    store={destructiveOverlayState}
+    close={closeDestructiveOverlay}
+    perform={performDestructiveOverlay}
+/>
+
+<OverlayContainer
+    overlay={ConstructiveOverlay}
+    store={constructiveOverlayState}
+    close={closeConstructiveOverlay}
+    perform={null}
+/>
+
+<ContextMenuContainer />
