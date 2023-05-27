a138a2efb75397da1d0c34d8d5d171fd1cdb3b67
Justus Perlwitz <hello@justus.pw>
2023-05-27 15:22:30 +0900

    Routes: Create storefront

---
+++ src/routes/(storefront)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Continue from "$lib/figma/navigation/header/Continue.svelte";
+    import Landing from "$lib/figma/navigation/header/Landing.svelte";
+    import { user } from "$lib/stores/user";
+</script>
+
+<div class="flex flex-col">
+    {#if user}
+        <Continue />
+    {:else}
+        <Landing />
+    {/if}
+    <slot />
+</div>
