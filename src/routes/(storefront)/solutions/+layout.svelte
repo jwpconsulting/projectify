fa03c3d33dc87db580e80350f7e234845c708541
Justus Perlwitz <hello@justus.pw>
2023-05-28 00:56:39 +0900

    Routes/Storefront: Fix solutions layout

--- src/routes/(storefront)/solutions/+layout.svelte
+++ src/routes/(storefront)/solutions/+layout.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-    import PageLayout from "$lib/components/layouts/pageLayout.svelte";
 </script>
 
-<PageLayout headerMode={"landing"}>
-    <main class="page bg-base-100">
-        <slot />
-    </main>
-</PageLayout>
+<slot />
