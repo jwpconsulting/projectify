939227b11d2ff66dd08726d1b8fcd84cb8db584d
Justus Perlwitz <hello@justus.pw>
2023-07-11 10:41:24 +0900

    Lib/Figma: Update WorkspaceBoardContextMenu
    Open up overlay. At this point, the overlay doesn't do much, so it needs
    to be finished.

--- src/lib/figma/overlays/context-menu/WorkspaceBoardContextMenu.svelte
+++ src/lib/figma/overlays/context-menu/WorkspaceBoardContextMenu.svelte
@@ -3,17 +3,34 @@
     import { Archive, Pencil } from "@steeze-ui/heroicons";
     import ContextMenuButton from "$lib/figma/buttons/ContextMenuButton.svelte";
     import type { WorkspaceBoard } from "$lib/types/workspace";
+    import type { ConstructiveOverlayType } from "$lib/types/ui";
+    import { openConstructiveOverlay } from "$lib/stores/globalUi";
 
     export let workspaceBoard: WorkspaceBoard;
 
     // TODO
-    console.log(workspaceBoard);
+    function editBoard() {
+        // open constructive dialog
+        const constructiveOverlayType: ConstructiveOverlayType = {
+            kind: "updateWorkspaceBoard",
+            workspaceBoard,
+        };
+        const action = {
+            kind: "sync" as const,
+            action: console.error.bind(null, "Don't call me"),
+        };
+        openConstructiveOverlay(constructiveOverlayType, action);
+    }
+
+    function archiveBoard() {
+        // open archive dialog
+    }
 </script>
 
 <ContextMenuButton
     kind={{
         kind: "button",
-        action: () => console.error("edit board not implemented"),
+        action: editBoard,
     }}
     label={$_("workspace-board-overlay.edit-board")}
     state="normal"
@@ -22,7 +39,7 @@
 <ContextMenuButton
     kind={{
         kind: "button",
-        action: () => console.error("archive board not implemented"),
+        action: archiveBoard,
     }}
     label={$_("workspace-board-overlay.archive-board")}
     state="normal"
