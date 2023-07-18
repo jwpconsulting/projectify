f9ca486b96e32d92869e07e4d2d61a62e04a3595
Justus Perlwitz <hello@justus.pw>
2023-07-18 12:15:17 +0900

    Routes: Refactor email-confirmation-link-sent

--- src/routes/(storefront)/email-confirmation-link-sent/+page.svelte
+++ src/routes/(storefront)/email-confirmation-link-sent/+page.svelte
@@ -7,15 +7,15 @@
 <div class="flex h-full flex-col items-center justify-center">
     <div class="flex flex-col items-center gap-12">
         <h1 class="text-4xl font-bold">
-            {$_("sign-up.confirmation-link-sent.header")}
+            {$_("auth.confirmation-link-sent.header")}
         </h1>
         <img
             src={illustration}
-            alt={$_("sign-up.confirmation-link-sent.illustration-alt")}
+            alt={$_("auth.confirmation-link-sent.illustration-alt")}
         />
-        <p>{$_("sign-up.confirmation-link-sent.body")}</p>
+        <p>{$_("auth.confirmation-link-sent.body")}</p>
         <Button
-            label={$_("sign-up.confirmation-link-sent.back-to-homepage")}
+            label={$_("auth.confirmation-link-sent.back-to-homepage")}
             size="medium"
             style={{ kind: "primary" }}
             color="blue"
