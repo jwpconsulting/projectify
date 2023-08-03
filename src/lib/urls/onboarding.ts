52463a143774db40ca64114ce6401638f061b917
Justus Perlwitz <hello@justus.pw>
2023-08-03 11:26:24 +0900

    Routes: Move onboarding up

--- src/lib/urls/onboarding.ts
+++ src/lib/urls/onboarding.ts
@@ -1,16 +1,16 @@
 // TODO type these with workspace / workspace board section etc. instances
 export function getNewWorkspaceBoardUrl(workspaceUuid: string): string {
-    return `/user/onboarding/new-workspace-board/${workspaceUuid}`;
+    return `/onboarding/new-workspace-board/${workspaceUuid}`;
 }
 
 export function getNewTaskUrl(workspaceBoardSectionUuid: string): string {
-    return `/user/onboarding/new-task/${workspaceBoardSectionUuid}`;
+    return `/onboarding/new-task/${workspaceBoardSectionUuid}`;
 }
 
 export function getNewLabelUrl(taskUuid: string): string {
-    return `/user/onboarding/new-label/${taskUuid}`;
+    return `/onboarding/new-label/${taskUuid}`;
 }
 
 export function getAssignTaskUrl(taskUuid: string): string {
-    return `/user/onboarding/assign-task/${taskUuid}`;
+    return `/onboarding/assign-task/${taskUuid}`;
 }
