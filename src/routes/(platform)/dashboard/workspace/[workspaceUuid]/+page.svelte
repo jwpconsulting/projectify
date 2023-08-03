bb9623de5c47f8f1b06919db09bc1dc9aa1ee25c
Justus Perlwitz <hello@justus.pw>
2023-08-03 11:22:14 +0900

    Routes/Dashboard: Redirect on missing ws board

--- src/routes/(platform)/dashboard/workspace/[workspaceUuid]/+page.svelte
+++ src/routes/(platform)/dashboard/workspace/[workspaceUuid]/+page.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-    import type { PageData } from "./$types";
-    export let data: PageData;
-    const { workspace } = data;
-</script>
-
-There are no workspace boards for {workspace}.
+This page should never be reached.
--- src/routes/(platform)/dashboard/workspace/[workspaceUuid]/+page.ts
+++ src/routes/(platform)/dashboard/workspace/[workspaceUuid]/+page.ts
@@ -3,29 +3,32 @@ import type { PageLoadEvent } from "./$types";
 import type { Workspace } from "$lib/types/workspace";
 import { getWorkspace } from "$lib/repository/workspace";
 import { getDashboardWorkspaceBoardUrl } from "$lib/urls";
+import { getNewWorkspaceBoardUrl } from "$lib/urls/user/onboarding";
 
-interface Data {
-    workspace: Workspace;
-}
 export const prerender = false;
 export const ssr = false;
 
 export async function load({
     params: { workspaceUuid },
     fetch,
-}: PageLoadEvent): Promise<Data> {
-    const workspace = await getWorkspace(workspaceUuid, {
+}: PageLoadEvent): Promise<void> {
+    const workspace: Workspace = await getWorkspace(workspaceUuid, {
         fetch,
     });
 
-    const { workspace_boards } = workspace;
+    const { uuid, workspace_boards } = workspace;
 
-    if (workspace_boards?.length) {
-        const { uuid } = workspace_boards[0];
+    if (!workspace_boards) {
+        throw new Error("Expected workspace_boards");
+    }
+    const first_workspace_board = workspace_boards.at(0);
+    if (first_workspace_board) {
+        const { uuid } = first_workspace_board;
         // eslint-disable-next-line @typescript-eslint/no-throw-literal
         throw redirect(302, getDashboardWorkspaceBoardUrl(uuid));
     }
-    // TODO
-    console.error("TODO handle no workspace boards");
-    return { workspace };
+    // TODO maybe throw in a nice notification to the user here that we have
+    // not found any workspace board for this workspace
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw redirect(302, getNewWorkspaceBoardUrl(uuid));
 }
