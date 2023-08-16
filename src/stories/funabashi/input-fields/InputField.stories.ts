762f9919bea38d6882d67ada108f7d5311749349
Justus Perlwitz <hello@justus.pw>
2023-08-17 08:57:03 +0900

    Lib/Funabashi: Update InputField
    Add more stories to cover all use cases

--- src/stories/funabashi/input-fields/InputField.stories.ts
+++ src/stories/funabashi/input-fields/InputField.stories.ts
@@ -55,10 +55,67 @@ export default meta;
 
 type Story = StoryObj<InputField>;
 
-export const Default: Story = {
+export const Search: Story = {
     args: {
-        placeholder: "This is a placeholder",
-        name: "input-field",
+        placeholder: "Enter something to search for",
+        name: "search",
+        style: { kind: "search" },
+        label: "This is a label for the search",
+        value: undefined,
+        anchorTop: {
+            href: "#",
+            label: "I am the anchor on the top",
+        },
+        anchorBottom: {
+            href: "#",
+            label: "I am the anchor on the bottom",
+        },
+        readonly: false,
+    },
+};
+
+export const SubTask: Story = {
+    args: {
+        placeholder: "Enter the name of a sub task",
+        name: "sub-task",
+        style: { kind: "subTask" },
+        label: "Your sub task",
+        value: undefined,
+        anchorTop: {
+            href: "#",
+            label: "I am the anchor on the top",
+        },
+        anchorBottom: {
+            href: "#",
+            label: "I am the anchor on the bottom",
+        },
+        readonly: false,
+    },
+};
+
+export const Text: Story = {
+    args: {
+        placeholder: "This is a placeholder for your text",
+        name: "text",
+        style: { kind: "field", inputType: "text" },
+        label: "This is a label",
+        value: undefined,
+        anchorTop: {
+            href: "#",
+            label: "I am the anchor on the top",
+        },
+        anchorBottom: {
+            href: "#",
+            label: "I am the anchor on the bottom",
+        },
+        readonly: false,
+    },
+};
+
+export const Password: Story = {
+    args: {
+        placeholder: "Enter a password. It will look password-y",
+        name: "password",
         style: { kind: "field", inputType: "text" },
         label: "This is a label",
         value: "",
@@ -70,5 +127,44 @@ export const Default: Story = {
             href: "#",
             label: "I am the anchor on the bottom",
         },
+        readonly: false,
+    },
+};
+
+export const Date: Story = {
+    args: {
+        placeholder: "A date comes here",
+        name: "date",
+        style: { kind: "field", inputType: "date" },
+        label: "Date for when the thing happens",
+        value: "",
+        anchorTop: {
+            href: "#",
+            label: "I am the anchor on the top",
+        },
+        anchorBottom: {
+            href: "#",
+            label: "I am the anchor on the bottom",
+        },
+        readonly: false,
+    },
+};
+
+export const Readonly: Story = {
+    args: {
+        placeholder: "This will never be shown",
+        name: "date",
+        style: { kind: "field", inputType: "date" },
+        label: "A date, but it's readonly",
+        value: "2012-12-31",
+        anchorTop: {
+            href: "#",
+            label: "I am the anchor on the top",
+        },
+        anchorBottom: {
+            href: "#",
+            label: "I am the anchor on the bottom",
+        },
+        readonly: true,
     },
 };
