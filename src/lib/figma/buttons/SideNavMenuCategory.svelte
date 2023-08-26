ed0ad14228e50a38492b7e42447e54233f852214
Justus Perlwitz <hello@justus.pw>
2023-08-26 15:27:45 +0900

    Lib/Figma: Update SideNavMenuCategory
    Explicitly handle button vs anchor

--- src/lib/figma/buttons/SideNavMenuCategory.svelte
+++ src/lib/figma/buttons/SideNavMenuCategory.svelte
@@ -18,28 +18,47 @@
     $: containedStyle = contained
         ? "group-hover:text-base-content"
         : "hover:text-base-content";
+
+    $: outerClass = `flex w-full flex-row justify-between gap-2 px-4 py-2 text-utility ${containedStyle}`;
 </script>
 
-<svelte:element
-    this={contained ? "div" : "button"}
-    on:click={contained ? undefined : click}
-    on:keydown={contained ? undefined : click}
-    class={`flex w-full flex-row justify-between gap-2 px-4 py-2 text-utility ${containedStyle}`}
->
-    <div class="flex flex-row items-center gap-4">
-        <div class="flex flex-row items-center gap-2">
-            <Icon src={icon} theme="outline" class="h-5 w-5" />
-            <div class="text-xs font-bold uppercase">
-                {label}
+<!-- XXX duplication aaaarg -->
+{#if contained}
+    <div class={outerClass}>
+        <div class="flex flex-row items-center gap-4">
+            <div class="flex flex-row items-center gap-2">
+                <Icon src={icon} theme="outline" class="h-5 w-5" />
+                <div class="text-xs font-bold uppercase">
+                    {label}
+                </div>
             </div>
+            {#if filtered}
+                <div class="h-3 w-3 rounded-full bg-primary" />
+            {/if}
         </div>
-        {#if filtered}
-            <div class="h-3 w-3 rounded-full bg-primary" />
-        {/if}
+        <Icon
+            src={open ? ChevronUp : ChevronDown}
+            theme="outline"
+            class="h-4 w-4"
+        />
     </div>
-    <Icon
-        src={open ? ChevronUp : ChevronDown}
-        theme="outline"
-        class="h-4 w-4"
-    />
-</svelte:element>
+{:else}
+    <button on:click={click} on:keydown={click} class={outerClass}>
+        <div class="flex flex-row items-center gap-4">
+            <div class="flex flex-row items-center gap-2">
+                <Icon src={icon} theme="outline" class="h-5 w-5" />
+                <div class="text-xs font-bold uppercase">
+                    {label}
+                </div>
+            </div>
+            {#if filtered}
+                <div class="h-3 w-3 rounded-full bg-primary" />
+            {/if}
+        </div>
+        <Icon
+            src={open ? ChevronUp : ChevronDown}
+            theme="outline"
+            class="h-4 w-4"
+        />
+    </button>
+{/if}
