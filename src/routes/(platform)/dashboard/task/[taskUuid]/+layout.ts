81a9787e1a60f745a09a9f76a8aaa1c460c554c8
Justus Perlwitz <hello@justus.pw>
2023-08-29 16:02:12 +0900

    Routes/Task: Refactor data loading
    We do it once per layout, DRY

--- src/routes/(platform)/dashboard/task/[taskUuid]/+layout.ts
+++ src/routes/(platform)/dashboard/task/[taskUuid]/+layout.ts
@@ -5,32 +5,52 @@ import {
     currentWorkspaceUuid,
     currentTaskUuid,
 } from "$lib/stores/dashboard";
+import type {
+    Task,
+    Workspace,
+    WorkspaceBoard,
+    WorkspaceBoardSection,
+} from "$lib/types/workspace";
 
 export const prerender = false;
 export const ssr = false;
 
-export function load({ params: { taskUuid } }: LayoutLoadEvent) {
-    // XXX Ideally we would already have set an instance of the task here,
-    // then inside +page.svelte we would not have a nullable Task
-    // XXX perhaps we can even init taskModule here
-    currentTaskUuid.set(taskUuid);
+interface Data {
+    task: Task;
+    workspaceBoardSection: WorkspaceBoardSection;
+    workspaceBoard: WorkspaceBoard;
+    workspace: Workspace;
+}
 
-    currentTask.subscribe(($currentTask) => {
-        if (!$currentTask) {
-            return;
-        }
-        const { workspace_board_section } = $currentTask;
-        if (!workspace_board_section) {
-            throw new Error("Expected workspace_board_section");
-        }
-        const { workspace_board } = workspace_board_section;
-        if (!workspace_board) {
-            throw new Error("Expected workspace_board");
-        }
-        const { workspace } = workspace_board;
-        if (!workspace) {
-            throw new Error("Expected workspace");
-        }
-        currentWorkspaceUuid.set(workspace.uuid);
+export async function load({
+    params: { taskUuid },
+}: LayoutLoadEvent): Promise<Data> {
+    currentTaskUuid.set(taskUuid);
+    return new Promise<Data>((resolve) => {
+        currentTask.subscribe(($currentTask) => {
+            if (!$currentTask) {
+                return;
+            }
+            const { workspace_board_section: workspaceBoardSection } =
+                $currentTask;
+            if (!workspaceBoardSection) {
+                throw new Error("Expected workspace_board_section");
+            }
+            const { workspace_board: workspaceBoard } = workspaceBoardSection;
+            if (!workspaceBoard) {
+                throw new Error("Expected workspace_board");
+            }
+            const { workspace } = workspaceBoard;
+            if (!workspace) {
+                throw new Error("Expected workspace");
+            }
+            currentWorkspaceUuid.set(workspace.uuid);
+            resolve({
+                task: $currentTask,
+                workspaceBoardSection,
+                workspaceBoard,
+                workspace,
+            });
+        });
     });
 }
