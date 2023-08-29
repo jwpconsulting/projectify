cd8086ce9ff5852c2deb52b550e02c811ff0fe5d
Justus Perlwitz <hello@justus.pw>
2023-08-29 18:21:30 +0900

    Lib/Figma: Update CreateTaskCard
    Bake form fields into component

--- src/lib/figma/screens/task/CreateTaskCard.svelte
+++ src/lib/figma/screens/task/CreateTaskCard.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-    // TODO rename to CreateTaskCard.svelte
     import TaskC from "$lib/components/dashboard/task/Task.svelte";
-    import CreateTaskFormFields from "$lib/figma/screens/task/CreateTaskFormFields.svelte";
+    import TaskDescription from "$lib/figma/screens/task/TaskDescription.svelte";
+    import TaskFieldsTemplate from "$lib/figma/screens/task/TaskFieldsTemplate.svelte";
+    import TaskSection from "$lib/figma/screens/task/TaskSection.svelte";
+    import TaskTitle from "$lib/figma/screens/task/TaskTitle.svelte";
     import TopBar from "$lib/figma/screens/task/TopBar.svelte";
     import type { CreateTaskModule } from "$lib/types/stores";
 
     export let createTaskModule: CreateTaskModule;
+
+    let { newTask, createTask } = createTaskModule;
 
     $: taskOrNewTask = {
         kind: "newTask" as const,
@@ -15,5 +19,16 @@
 
 <TaskC>
     <TopBar slot="top-bar" {taskOrNewTask} taskModule={createTaskModule} />
-    <CreateTaskFormFields slot="content" {createTaskModule} />
+
+    <TaskFieldsTemplate slot="content">
+        <TaskTitle slot="title" bind:title={$createTask.title} />
+        <TaskSection
+            slot="section"
+            workspaceBoardSection={newTask.workspace_board_section}
+        />
+        <TaskDescription
+            slot="description"
+            bind:description={$createTask.description}
+        />
+    </TaskFieldsTemplate>
 </TaskC>
