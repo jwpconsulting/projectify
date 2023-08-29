48a1ab6d5e383cd8f036332241710639acf78790
Justus Perlwitz <hello@justus.pw>
2023-08-29 19:16:43 +0900

    Lib/Env: Remove gql endpoint configs

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,12 +1,5 @@
 export default {
     GRAPHQL_ENDPOINT: import.meta.env.VITE_GRAPHQL_ENDPOINT as string,
-    // TODO remove me Justus 2023-08-03
-    GRAPHQL_ENDPOINT_BATCH: import.meta.env
-        .VITE_GRAPHQL_ENDPOINT_BATCH as string,
-    // TODO remove me Justus 2023-08-03
-    GRAPHQL_ENDPOINT_SUBSCRIPTIONS: import.meta.env
-        .VITE_GRAPHQL_ENDPOINT_SUBSCRIPTIONS as string,
-
     WS_ENDPOINT: import.meta.env.VITE_WS_ENDPOINT as string,
     API_ENDPOINT: import.meta.env.VITE_API_ENDPOINT as string,
 };
