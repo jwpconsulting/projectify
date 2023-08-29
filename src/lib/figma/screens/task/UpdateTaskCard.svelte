d051bc43e2892db75a92fbcfea87f93b9eca3e74
Justus Perlwitz <hello@justus.pw>
2023-08-29 18:20:06 +0900

    Lib/Figma: Update UpdateTaskCard
    Bake fields into here

--- src/lib/figma/screens/task/UpdateTaskCard.svelte
+++ src/lib/figma/screens/task/UpdateTaskCard.svelte
@@ -1,15 +1,55 @@
 <script lang="ts">
     import TaskC from "$lib/components/dashboard/task/Task.svelte";
     import TaskUpdateBar from "$lib/figma/buttons/TaskUpdateBar.svelte";
+    import SubTaskBarComposite from "$lib/figma/screens/task/SubTaskBarComposite.svelte";
+    import TaskDescription from "$lib/figma/screens/task/TaskDescription.svelte";
+    import TaskDueDate from "$lib/figma/screens/task/TaskDueDate.svelte";
+    import TaskFieldsTemplate from "$lib/figma/screens/task/TaskFieldsTemplate.svelte";
+    import TaskLabel from "$lib/figma/screens/task/TaskLabel.svelte";
+    import TaskSection from "$lib/figma/screens/task/TaskSection.svelte";
+    import TaskTitle from "$lib/figma/screens/task/TaskTitle.svelte";
+    import TaskUser from "$lib/figma/screens/task/TaskUser.svelte";
     import TopBar from "$lib/figma/screens/task/TopBar.svelte";
-    import UpdateTaskFormFields from "$lib/figma/screens/task/UpdateTaskFormFields.svelte";
     import type { TaskUpdateBarState } from "$lib/figma/types";
     import type { TaskModule } from "$lib/types/stores";
-    import type { Task } from "$lib/types/workspace";
+    import type {
+        Label,
+        SubTask,
+        Task,
+        WorkspaceUser,
+    } from "$lib/types/workspace";
+    import { unwrap } from "$lib/utils/type";
 
+    // if this is in a store, we can get rid of this param
     export let task: Task;
     export let taskModule: TaskModule;
     export let state: TaskUpdateBarState = "task";
+
+    $: workspaceBoardSection = unwrap(
+        task.workspace_board_section,
+        "Expected workspace_board_section"
+    );
+
+    let title: string = task.title;
+    let description: string | undefined = task.description;
+    let assignedUser: WorkspaceUser | null = task.assignee ?? null;
+    let labels: Label[] = task.labels;
+    let dueDate: string | null = null;
+    let subTasks: SubTask[] = [];
+
+    $: {
+        // XXX what does this code here do?
+        taskModule.updateTask.set({
+            ...task,
+            title: title,
+            description: description,
+            // XXX What would RMS do??
+            assignee: assignedUser ?? undefined,
+            labels: labels,
+            deadline: dueDate ?? undefined,
+            sub_tasks: subTasks,
+        });
+    }
 
     $: taskOrNewTask = { kind: "task" as const, task };
 </script>
@@ -18,5 +58,21 @@
     <TopBar slot="top-bar" {taskOrNewTask} {taskModule} />
     <TaskUpdateBar slot="tab-bar-mobile" kind="mobile" {state} {task} />
     <TaskUpdateBar slot="tab-bar-desktop" kind="mobile" {state} {task} />
-    <UpdateTaskFormFields slot="content" {task} {taskModule} />
+    <TaskFieldsTemplate slot="content">
+        <TaskTitle slot="title" bind:title />
+        <TaskUser
+            slot="assignee"
+            action={taskModule.showUpdateWorkspaceUser}
+            workspaceUser={assignedUser}
+        />
+        <TaskLabel
+            slot="labels"
+            action={taskModule.showUpdateLabel}
+            {labels}
+        />
+        <TaskSection slot="section" {workspaceBoardSection} />
+        <TaskDueDate slot="due-date" date={dueDate} />
+        <TaskDescription slot="description" bind:description />
+    </TaskFieldsTemplate>
+    <SubTaskBarComposite {subTasks} />
 </TaskC>
