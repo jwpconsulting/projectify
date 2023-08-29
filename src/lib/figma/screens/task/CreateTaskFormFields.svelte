5cde5247915ea28389e998d6c809239f301eb7a2
Justus Perlwitz <hello@justus.pw>
2023-08-29 16:01:12 +0900

    Lib/Figma: Update CreateTaskFormFields
    Show section here (non-editable)

--- src/lib/figma/screens/task/CreateTaskFormFields.svelte
+++ src/lib/figma/screens/task/CreateTaskFormFields.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
     import TaskFieldsTemplate from "$lib/figma/screens/task/TaskFieldsTemplate.svelte";
     import TaskUpdateDescription from "$lib/figma/screens/task/TaskUpdateDescription.svelte";
+    import TaskUpdateSection from "$lib/figma/screens/task/TaskUpdateSection.svelte";
     import TaskUpdateTitle from "$lib/figma/screens/task/TaskUpdateTitle.svelte";
     import type { CreateTaskModule } from "$lib/types/stores";
 
     export let createTaskModule: CreateTaskModule;
 
-    let { createTask } = createTaskModule;
+    let { newTask, createTask } = createTaskModule;
 </script>
 
 <TaskFieldsTemplate>
     <TaskUpdateTitle slot="title" bind:title={$createTask.title} />
+    <TaskUpdateSection
+        slot="section"
+        workspaceBoardSection={newTask.workspace_board_section}
+    />
     <TaskUpdateDescription
         slot="description"
         bind:description={$createTask.description}
