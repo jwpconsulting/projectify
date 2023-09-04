e59858da27c8fb907cbfbccce16f7849aca35550
Justus Perlwitz <hello@justus.pw>
2023-09-04 22:30:38 +0900

    Lib/Figm: Update SearchButton
    Use hero icon

--- src/lib/figma/buttons/SearchButton.svelte
+++ src/lib/figma/buttons/SearchButton.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import IconPlus from "$lib/components/icons/icon-plus.svelte";
-    import IconSearch from "$lib/components/icons/icon-search.svelte";
+    import { Plus, Search } from "@steeze-ui/heroicons";
+    import { Icon } from "@steeze-ui/svelte-icon";
 </script>
 
 <div
     class="flex h-fit w-fit flex-row items-center gap-2 rounded-lg border border-border p-2 hover:bg-task-hover focus:outline-none focus:ring-1 focus:ring-border-focus focus:ring-offset-2 active:bg-task-pressed"
 >
-    <IconSearch />
+    <Icon src={Search} class="h-6" theme="outline" />
     <div class="flex flex-row items-center gap-1">
         <div class="rounded-lg border border-border px-2 py-1 text-xs">⌘</div>
-        <IconPlus />
+        <Icon src={Plus} class="shrink-0 h-6 w-6" theme="outline" />
         <div class="rounded-lg border border-border px-2 py-1 text-xs">K</div>
     </div>
 </div>
