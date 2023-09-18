072f460c31952629ab44f6660705b9ebd437fa1f
Justus Perlwitz <hello@justus.pw>
2023-09-18 23:31:30 +0900

    Routes: Refactor dashboard layout
    This puts task / ws settings inside the same side nav layout thing

---
+++ src/routes/(platform)/dashboard/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import SideNav from "$lib/figma/navigation/SideNav.svelte";
+    import {
+        currentWorkspace,
+        currentWorkspaces,
+    } from "$lib/stores/dashboard";
+
+    $: workspaces = $currentWorkspaces ?? [];
+    $: workspace = $currentWorkspace;
+</script>
+
+<div class="flex h-full grow flex-row">
+    <!-- this breakpoint is in tune with the mobile menu breakpoint -->
+    <div class="hidden lg:block">
+        <SideNav {workspaces} {workspace} />
+    </div>
+    <div class="grow">
+        <slot />
+    </div>
+</div>
--- src/routes/(platform)/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
+++ src/routes/(platform)/dashboard/workspace-board/[workspaceBoardUuid]/+page.svelte
@@ -2,25 +2,13 @@
     import type { PageData } from "./$types";
 
     import Dashboard from "$lib/components/dashboard/Dashboard.svelte";
-    import SideNav from "$lib/figma/navigation/SideNav.svelte";
-    import {
-        currentWorkspace,
-        currentWorkspaceBoard,
-    } from "$lib/stores/dashboard";
+    import { currentWorkspaceBoard } from "$lib/stores/dashboard";
 
     export let data: PageData;
 
-    let { workspaceBoard, workspace } = data;
-    const { workspaces } = data;
+    let { workspaceBoard } = data;
 
     $: workspaceBoard = $currentWorkspaceBoard ?? workspaceBoard;
-    $: workspace = $currentWorkspace ?? workspace;
 </script>
 
-<div class="flex h-full grow flex-row">
-    <!-- this breakpoint is in tune with the mobile menu breakpoint -->
-    <div class="hidden lg:block">
-        <SideNav {workspaces} {workspace} />
-    </div>
-    <Dashboard {workspaceBoard} />
-</div>
+<Dashboard {workspaceBoard} />
