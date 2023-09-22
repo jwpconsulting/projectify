464681a7cab64ca2a4f25315a5438d10a02f2980
Justus Perlwitz <hello@justus.pw>
2023-09-22 14:17:17 +0900

    Workspace: Make task updateable

--- mypy/stubs/rest_framework/test.pyi
+++ mypy/stubs/rest_framework/test.pyi
@@ -1,4 +1,5 @@
 from typing import (
+    Any,
     Optional,
 )
 
@@ -8,6 +9,10 @@ from django.contrib.auth.models import (
 from django.test.client import Client as DjangoClient
 from django.test.client import RequestFactory as DjangoRequestFactory
 
+from rest_framework.response import (
+    Response,
+)
+
 class APIRequestFactory(DjangoRequestFactory): ...
 
 class APIClient(APIRequestFactory, DjangoClient):
@@ -16,3 +21,20 @@ class APIClient(APIRequestFactory, DjangoClient):
         user: Optional[AbstractBaseUser] = None,
         token: Optional[str] = None,
     ) -> None: ...
+    # This is the exact same signature as in rest_framework/test.py
+    # But our django type stubs are in disagreement here!
+    def get(  # type: ignore[override]
+        self,
+        path: str,
+        data: Optional[Any] = None,
+        follow: bool = False,
+        **extra: object
+    ) -> Response: ...
+    def put(  # type: ignore[override]
+        self,
+        path: str,
+        data: Optional[Any],
+        format: Optional[str] = None,
+        content_type: Optional[str] = None,
+        **extra: object
+    ) -> Response: ...
