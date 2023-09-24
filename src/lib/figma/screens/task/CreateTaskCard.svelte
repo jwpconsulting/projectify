673a9e3ee3724f50648d8f1d9147d02947f71458
Justus Perlwitz <hello@justus.pw>
2023-09-25 05:29:00 +0900

    Lib/Figma: Refactor CreateTaskCard
    Remove task module and stores, simplify drastically.

--- src/lib/figma/screens/task/CreateTaskCard.svelte
+++ src/lib/figma/screens/task/CreateTaskCard.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { derived, writable } from "svelte/store";
     import { _ } from "svelte-i18n";
 
     import { goto } from "$lib/navigation";
@@ -19,73 +18,44 @@
     import TopBar from "$lib/figma/screens/task/TopBar.svelte";
     import Button from "$lib/funabashi/buttons/Button.svelte";
     import { createTask as createTaskFn } from "$lib/repository/workspace";
-    import { createLabelAssignment } from "$lib/stores/dashboard/labelAssignment";
-    import type { CreateTaskModule } from "$lib/types/stores";
-    import type { WorkspaceUserSelection } from "$lib/types/ui";
     import type {
         CreateTask,
         WorkspaceBoardSection,
     } from "$lib/types/workspace";
+    import { unwrap } from "$lib/utils/type";
 
     export let workspaceBoardSection: WorkspaceBoardSection;
 
-    const createTask = writable<Partial<CreateTask>>({});
+    // form fields
+    let title: string | undefined = undefined;
+    let description: string | undefined = undefined;
 
-    const newTask = {
-        workspace_board_section: workspaceBoardSection,
-    };
-    const { workspace_board: workspaceBoard } = workspaceBoardSection;
-    if (!workspaceBoard) {
-        throw new Error("Expected workspaceBoard");
-    }
-    const { workspace } = workspaceBoard;
-    if (!workspace) {
-        throw new Error("Expected workspace");
-    }
-
-    const labelAssignment = createLabelAssignment();
-    const canCreateOrUpdate = derived<typeof createTask, boolean>(
-        createTask,
-        ($createTask, set) => {
-            set(
-                $createTask.title !== undefined &&
-                    $createTask.description !== undefined
-            );
-        },
-        false
+    const workspaceBoard = unwrap(
+        workspaceBoardSection.workspace_board,
+        "Expected workspaceBoard"
     );
-    const createTaskModule: CreateTaskModule = {
-        createTask,
-        newTask,
-        // XXX this requires special logic because we can only
-        // assign users/labels after the task is already created
-        // TODO make workspace user menu so that "all" can not be
-        // selected
-        workspaceUserAssignment: {
-            select: console.error,
-            deselect: console.error,
-            selected: writable<WorkspaceUserSelection>(),
-        },
-        // TODO make label menu so that "all" can not be selected
-        labelAssignment,
-    };
+
+    // TODO add me back:
+    // const labelAssignment = createLabelAssignment();
+
+    $: canCreateOrUpdate = title !== undefined && description !== undefined;
 
     async function createOrUpdateTask() {
-        const { title, description } = $createTask;
         if (!title || !description) {
             throw new Error("Expected title and description");
         }
-        const { newTask } = createTaskModule;
         const createTaskFull: CreateTask = {
             title,
             description,
-            workspace_board_section: newTask.workspace_board_section,
+            workspace_board_section: workspaceBoardSection,
             // TODO
             labels: [],
             // TODO
             deadline: null,
         };
         await createTaskFn(createTaskFull);
+        // TODO allow player to keep editing this task
+        // Like django admin's save then continue editing
         await goto(
             getDashboardWorkspaceBoardSectionUrl(workspaceBoardSection.uuid)
         );
@@ -117,7 +87,7 @@
             action={{
                 kind: "button",
                 action: createOrUpdateTask,
-                disabled: !$canCreateOrUpdate,
+                disabled: !canCreateOrUpdate,
             }}
             color="blue"
             size="small"
@@ -127,14 +97,8 @@
     </TopBar>
 
     <TaskFieldsTemplate slot="content">
-        <TaskTitle slot="title" bind:title={$createTask.title} />
-        <TaskSection
-            slot="section"
-            workspaceBoardSection={newTask.workspace_board_section}
-        />
-        <TaskDescription
-            slot="description"
-            bind:description={$createTask.description}
-        />
+        <TaskTitle slot="title" bind:title />
+        <TaskSection slot="section" {workspaceBoardSection} />
+        <TaskDescription slot="description" bind:description />
     </TaskFieldsTemplate>
 </TaskC>
