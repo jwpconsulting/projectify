bb9be6c43c5241902297d904f9eba606548b4d89
Justus Perlwitz <hello@justus.pw>
2023-09-25 20:42:51 +0900

    Workspace: Refactor WorkspaceUser model

--- workspace/models/__init__.py
+++ workspace/models/__init__.py
@@ -8,12 +8,10 @@ from typing import (
     Callable,
     ClassVar,
     Optional,
+    Self,
     cast,
 )
 
-from django.conf import (
-    settings,
-)
 from django.contrib.auth.models import (
     AbstractBaseUser,
 )
@@ -30,11 +28,10 @@ from django_extensions.db.models import (
     TimeStampedModel,
     TitleDescriptionModel,
 )
-from typing_extensions import (
-    Self,
-)
 
-from .const import WorkspaceUserRoles as WorkspaceUserRoles
+from .const import (
+    WorkspaceUserRoles,
+)
 from .label import (
     Label,
 )
@@ -49,8 +46,13 @@ from .types import (
     GetOrder,
     SetOrder,
 )
-from .workspace import Workspace as Workspace
-from .workspace import WorkspaceQuerySet as WorkspaceQuerySet
+from .workspace import (
+    Workspace,
+    WorkspaceQuerySet,
+)
+from .workspace_user import (
+    WorkspaceUser,
+)
 from .workspace_user_invite import (
     WorkspaceUserInvite,
 )
@@ -60,78 +62,10 @@ from .workspace_user_invite import (
 
 
 if TYPE_CHECKING:
-    from django.db.models.fields.related import RelatedField  # noqa: F401
     from django.db.models.manager import RelatedManager  # noqa: F401
-
-    from user.models import (  # noqa: F401
-        User,
-        UserInvite,
-    )
 
 
 Pks = list[str]
-
-
-class WorkspaceUserQuerySet(models.QuerySet["WorkspaceUser"]):
-    """Workspace user queryset."""
-
-    def filter_by_workspace_pks(self, workspace_pks: Pks) -> Self:
-        """Filter by workspace pks."""
-        return self.filter(workspace__pk__in=workspace_pks)
-
-    def get_by_workspace_and_user(
-        self, workspace: Workspace, user: AbstractBaseUser
-    ) -> "WorkspaceUser":
-        """Get by workspace and user."""
-        return self.get(workspace=workspace, user=user)
-
-
-class WorkspaceUser(TimeStampedModel, models.Model):
-    """Workspace to user mapping."""
-
-    workspace = models.ForeignKey["Workspace"](
-        Workspace,
-        on_delete=models.PROTECT,
-    )
-    # This defo depends on the User in user/ app
-    user = models.ForeignKey["User"](
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-    role = models.CharField(
-        max_length=10,
-        choices=WorkspaceUserRoles.choices,
-        default=WorkspaceUserRoles.OBSERVER,
-    )
-    job_title = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-    )
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-
-    # Sort of nonsensical
-    objects: ClassVar[WorkspaceUserQuerySet] = cast(  # type: ignore[assignment]
-        WorkspaceUserQuerySet, WorkspaceUserQuerySet.as_manager()
-    )
-
-    if TYPE_CHECKING:
-        # Related
-        user_invite: RelatedField[None, "UserInvite"]
-
-    def assign_role(self, role: str) -> None:
-        """
-        Assign a new role.
-
-        Saves.
-        """
-        self.role = role
-        self.save()
-
-    class Meta:
-        """Meta."""
-
-        unique_together = ("workspace", "user")
 
 
 class WorkspaceBoardQuerySet(models.QuerySet["WorkspaceBoard"]):
@@ -412,5 +346,7 @@ __all__ = (
     "TaskQuerySet",
     "Workspace",
     "WorkspaceQuerySet",
+    "WorkspaceUser",
     "WorkspaceUserInvite",
+    "WorkspaceUserRoles",
 )
