e9e407c8d0762702454f41419a3b346551923c7a
Justus Perlwitz <hello@justus.pw>
2023-09-25 19:39:09 +0900

    Workspace: Refactor sub task model

---
+++ workspace/models/sub_task.py
@@ -0,0 +1,114 @@
+"""Contain sub task model and manager."""
+import uuid
+from typing import (
+    ClassVar,
+    cast,
+)
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+)
+from django.db import (
+    models,
+    transaction,
+)
+from django.utils.translation import gettext_lazy as _
+
+from django_extensions.db.models import (
+    TimeStampedModel,
+    TitleDescriptionModel,
+)
+from typing_extensions import (
+    Self,
+)
+
+from .task import (
+    Task,
+)
+from .types import (
+    Pks,
+)
+from .workspace import Workspace as Workspace
+
+
+class SubTaskQuerySet(models.QuerySet["SubTask"]):
+    """Sub task queryset."""
+
+    def filter_by_task_pks(self, task_pks: Pks) -> Self:
+        """Filter by task pks."""
+        return self.filter(task__pk__in=task_pks)
+
+    def filter_for_user_and_uuid(
+        self, user: AbstractBaseUser, uuid: uuid.UUID
+    ) -> Self:
+        """Get sub task for a certain user and sub task uuid."""
+        kwargs = {
+            "task__workspace_board_section__workspace_board__"
+            "workspace__users": user,
+            "uuid": uuid,
+        }
+        return self.filter(**kwargs)
+
+
+class SubTask(
+    TitleDescriptionModel,
+    TimeStampedModel,
+    models.Model,
+):
+    """SubTask, belongs to Task."""
+
+    task = models.ForeignKey[Task](
+        Task,
+        on_delete=models.CASCADE,
+    )
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    done = models.BooleanField(
+        default=False,
+        help_text=_("Designate whether this sub task is done"),
+    )
+
+    objects: ClassVar[SubTaskQuerySet] = cast(  # type: ignore[assignment]
+        SubTaskQuerySet, SubTaskQuerySet.as_manager()
+    )
+
+    # Ordering related
+    _order: int
+
+    def move_to(self, order: int) -> None:
+        """
+        Move to specified order n within task.
+
+        No save required.
+        """
+        neighbor_subtasks = self.task.subtask_set.select_for_update()
+        with transaction.atomic():
+            # Force queryset to be evaluated to lock them for the time of
+            # this transaction
+            len(neighbor_subtasks)
+            current_task = self.task
+            # Django docs wrong, need to cast to list
+            order_list = list(current_task.get_subtask_order())
+            # The list is ordered by pk, which is not uuid for us
+            current_object_index = order_list.index(self.pk)
+            # Mutate to perform move operation
+            order_list.insert(order, order_list.pop(current_object_index))
+            # Set new order
+            current_task.set_subtask_order(order_list)
+            current_task.save()
+
+    @property
+    def workspace(self) -> Workspace:
+        """Get workspace instance."""
+        return self.task.workspace_board_section.workspace_board.workspace
+
+    class Meta:
+        """Meta."""
+
+        order_with_respect_to = "task"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["task", "_order"],
+                name="unique_sub_task_order",
+                deferrable=models.Deferrable.DEFERRED,
+            )
+        ]
