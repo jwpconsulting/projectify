b81198dbaf3a32bd06f70ab4654d5fb56082118f
Justus Perlwitz <hello@justus.pw>
2023-09-25 18:47:47 +0900

    Workspace: Refactor task model

--- workspace/models/types.py
+++ workspace/models/types.py
@@ -1,2 +1,14 @@
 """Contain some common types used among all models and querysets."""
+from collections.abc import (
+    Iterable,
+    Sequence,
+)
+from typing import (
+    Callable,
+)
+
+
 Pks = list[str]
+
+GetOrder = Callable[[], Iterable[int]]
+SetOrder = Callable[[Sequence[int]], None]
