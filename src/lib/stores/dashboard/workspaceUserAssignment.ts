9dba4478f6531728f165e658787505838ec3c02a
Justus Perlwitz <hello@justus.pw>
2023-09-26 18:19:39 +0900

    Lib/Stores: Remove unused code

--- src/lib/stores/dashboard/workspaceUserAssignment.ts
+++ src/lib/stores/dashboard/workspaceUserAssignment.ts
@@ -1,26 +1,11 @@
 import { derived, readonly, writable } from "svelte/store";
 
-import { assignUserToTask } from "$lib/repository/workspace";
 import type { WorkspaceUserAssignment } from "$lib/types/stores";
 import type {
     WorkspaceUserAssignmentInput,
     WorkspaceUserAssignmentState,
-    WorkspaceUserSelectionInput,
 } from "$lib/types/ui";
 import type { Task, WorkspaceUser } from "$lib/types/workspace";
-
-export async function assignWorkspaceUser(
-    task: Task,
-    selection: WorkspaceUserSelectionInput
-) {
-    if (selection.kind === "unassigned") {
-        await assignUserToTask(null, task.uuid);
-    } else if (selection.kind === "allWorkspaceUsers") {
-        throw new Error("Unsupported");
-    } else {
-        await assignUserToTask(selection.workspaceUser.user.email, task.uuid);
-    }
-}
 
 export function createWorkspaceUserAssignment(
     task: Task
