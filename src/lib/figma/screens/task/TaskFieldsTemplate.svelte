742e6b425de2854f9fa863d9348c7905f5fa2f9a
Justus Perlwitz <hello@justus.pw>
2023-09-26 20:21:34 +0900

    Lib/Figma: Update TaskFieldsTemplate
    Change to use tabe

--- src/lib/figma/screens/task/TaskFieldsTemplate.svelte
+++ src/lib/figma/screens/task/TaskFieldsTemplate.svelte
@@ -1,42 +1,63 @@
 <script lang="ts">
     import { _ } from "svelte-i18n";
+
+    const leftSideClass = "min-w-content text-left font-bold align-top";
+    const rightSideClass = "max-w-xs";
 </script>
 
-<dl class="inline-grid max-w-sm auto-cols-min grid-cols-2 items-center gap-8">
-    <dt class="font-bold">
-        {$_("task-screen.task-title")}
-    </dt>
-    <dd>
-        <slot name="title" />
-    </dd>
-    <dt class="font-bold">
-        {$_("task-screen.assignee")}
-    </dt>
-    <dd>
-        <slot name="assignee" />
-    </dd>
-    <dt class="font-bold">
-        {$_("task-screen.labels")}
-    </dt>
-    <dd>
-        <slot name="labels" />
-    </dd>
-    <dt class="font-bold">
-        {$_("task-screen.section")}
-    </dt>
-    <dd>
-        <slot name="section" />
-    </dd>
-    <dt class="font-bold">
-        {$_("task-screen.due-date")}
-    </dt>
-    <dd>
-        <slot name="due-date" />
-    </dd>
-    <dt class="font-bold">
-        {$_("task-screen.description")}
-    </dt>
-    <dd>
-        <slot name="description" />
-    </dd>
-</dl>
+<table class="w-full max-w-4xl table-auto border-separate border-spacing-y-4">
+    <colgroup>
+        <col />
+        <col />
+    </colgroup>
+    <tbody>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.task-title")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="title" />
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.assignee")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="assignee" />
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.labels")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="labels" />
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.section")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="section" />
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.due-date")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="due-date" />
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class={leftSideClass}>
+                {$_("task-screen.description")}
+            </th>
+            <td class={rightSideClass}>
+                <slot name="description" />
+            </td>
+        </tr>
+    </tbody>
+</table>
