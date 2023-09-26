2092dadd1dbf35c4870e2b92b4a8ea2c04891b1b
Justus Perlwitz <hello@justus.pw>
2023-09-26 18:18:37 +0900

    Lib/Figma: Update WorkspaceUser
    Allow assigning user from here directly

--- src/lib/figma/cards/task-card/WorkspaceUser.svelte
+++ src/lib/figma/cards/task-card/WorkspaceUser.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import AvatarVariant from "$lib/figma/navigation/AvatarVariant.svelte";
+    import { updateTask } from "$lib/repository/workspace";
     import { createWorkspaceUserAssignment } from "$lib/stores/dashboard/workspaceUserAssignment";
     import { openContextMenu } from "$lib/stores/globalUi";
     import type { ContextMenuType } from "$lib/types/ui";
@@ -9,24 +10,34 @@
 
     let userPickerBtnRef: HTMLElement;
 
-    function openUserPicker() {
+    $: workspaceUserAssignment = createWorkspaceUserAssignment(task);
+
+    $: assignee = $workspaceUserAssignment;
+
+    async function openUserPicker() {
         const contextMenuType: ContextMenuType = {
             kind: "updateMember",
-            workspaceUserAssignment: createWorkspaceUserAssignment(task),
+            workspaceUserAssignment,
         };
-        openContextMenu(contextMenuType, userPickerBtnRef);
+        await openContextMenu(contextMenuType, userPickerBtnRef);
+        const workspaceUser = $workspaceUserAssignment;
+        await updateTask(
+            task,
+            task.labels.map((label) => label.uuid),
+            workspaceUser
+        );
     }
 </script>
 
-{#if task.assignee}
+{#if assignee}
     <AvatarVariant
-        content={{ kind: "single", user: task.assignee.user }}
+        content={{ kind: "single", user: assignee.user }}
         size="small"
     />
 {:else}
     <button
         bind:this={userPickerBtnRef}
-        on:click|preventDefault={() => openUserPicker()}
+        on:click|preventDefault={openUserPicker}
     >
         <AvatarVariant content={{ kind: "single", user: null }} size="small" />
     </button>
