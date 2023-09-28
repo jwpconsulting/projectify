e1dcab23ac091195752de1d933398e222b1a2d14
Justus Perlwitz <hello@justus.pw>
2023-09-28 11:14:45 +0900

    Vite: Configure API proxy
    Make it possible to use the page server from npm run dev on another device
    connected on this network without having to mess around with the backend
    settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
+import type { UserConfig, ProxyOptions, UserConfigFnObject } from "vite";
+import type { ConfigEnv } from "vite";
+import { loadEnv } from "vite";
 
-const config: UserConfig = {
+const configDefaults: UserConfig = {
     plugins: [sveltekit()],
     esbuild: {
         // It could be useful to drop console etc. here using the following
@@ -13,5 +16,46 @@ const config: UserConfig = {
     },
 };
 
-// eslint-disable-next-line
+const config: UserConfigFnObject = defineConfig(({ mode }: ConfigEnv) => {
+    const env = loadEnv(mode, process.cwd());
+
+    function getFromEnv(key: string): string {
+        const value = env[key];
+        if (!value) {
+            throw new Error(`Expected ${key} to be in env`);
+        }
+        return value;
+    }
+    const proxyConfig: Record<string, ProxyOptions> | undefined =
+        env.VITE_USE_LOCAL_PROXY !== undefined
+            ? {
+                  // We crash here if these variables are not there
+                  "/graphql": {
+                      target: getFromEnv("VITE_PROXY_GRAPHQL_ENDPOINT"),
+                      changeOrigin: true,
+                      rewrite: (path: string) =>
+                          path.replace(/^\/graphql/, ""),
+                  },
+                  "/ws": {
+                      target: getFromEnv("VITE_PROXY_WS_ENDPOINT"),
+                      changeOrigin: true,
+                      ws: true,
+                      rewrite: (path: string) => path.replace(/^\/ws/, ""),
+                  },
+                  "/api": {
+                      target: getFromEnv("VITE_PROXY_API_ENDPOINT"),
+                      changeOrigin: true,
+                      rewrite: (path: string) => path.replace(/^\/api/, ""),
+                  },
+              }
+            : undefined;
+
+    return {
+        ...configDefaults,
+        server: {
+            proxy: proxyConfig,
+        },
+    };
+});
+
 export default config;
