aa2e411b965296aba687810c135f74494e79a66d
Justus Perlwitz <hello@justus.pw>
2023-10-16 22:17:18 +0900

    Workspace: Implement sub task nested CRUD

---
+++ workspace/serializers/sub_task.py
@@ -0,0 +1,314 @@
+"""Custom serializer behaviors for sub task."""
+from collections.abc import (
+    Sequence,
+)
+from typing import (
+    Any,
+    NotRequired,
+    Optional,
+    TypedDict,
+)
+from uuid import (
+    UUID,
+)
+
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import (
+    serializers,
+)
+
+from workspace.serializers.base import (
+    TaskBaseSerializer,
+)
+
+from .. import (
+    models,
+)
+from ..models import (
+    SubTask,
+)
+
+
+class ValidatedDatum(TypedDict):
+    """A validated datum, i.e. a single sub task."""
+
+    title: str
+    description: NotRequired[str]
+    done: bool
+    _order: int
+
+
+class ValidatedDatumWithUuid(ValidatedDatum):
+    """A validated datum with UUID required."""
+
+    uuid: UUID
+
+
+class ValidatedData(TypedDict):
+    """Validated data, split by create and update sub tasks."""
+
+    create_sub_tasks: Optional[list[ValidatedDatum]]
+    # TODO
+    # If we are really fancy, we could make update_sub_tasks a dict[str,
+    # UpdateSubTaskDatum] ??
+    update_sub_tasks: Optional[list[ValidatedDatumWithUuid]]
+
+
+class SubTaskListSerializer(serializers.ListSerializer[SubTask]):
+    """
+    Override sub task list behavior.
+
+    This supports two cases:
+    - UUID is given -> Update a given sub task by that uuid
+    - UUID not given -> Create a new sub task by that uuid
+
+    Also, adjust the order of sub tasks given by the order of appearance within
+    the list.
+
+    Allow restricting sub task selection to only sub tasks from the same task
+    by passing task in as a context variable.
+
+    NOTE: Do run this inside a transaction. Validation -> create/update
+    depend on each other here, so we can't just wrap each individual method
+    in a transaction.atomic.
+    """
+
+    validated_data: ValidatedData
+    parent: Optional[TaskBaseSerializer]
+    _task: Optional[models.Task] = None
+
+    @property
+    def task(self) -> Optional[models.Task]:
+        """
+        Return a task, either by looking inside context or parent.
+
+        Cache the result.
+        """
+        task: Optional[models.Task]
+        if self._task:
+            return self._task
+        if "task" in self.context:
+            task = self.context.pop("task")
+        elif self.parent and self.parent.instance:
+            task = self.parent.instance
+        else:
+            task = None
+        self._task = task
+        return task
+
+    def to_internal_value(self, data: list[Any]) -> Any:
+        """Ensure that we have an instance."""
+        if self.instance and self.task:
+            raise Exception(
+                "You may not provide an instance and a task in the "
+                "context simultaneously"
+            )
+        if self.task and self.instance is None:
+            self.instance = list(self.task.subtask_set.all())
+        return super().to_internal_value(data)
+
+    def validate(self, validated_data: list[Any]) -> ValidatedData:
+        """Ensure that this task has no sub tasks when creating."""
+        # We might be creating new sub tasks.
+        # 1) determine which sub tasks have to be newly created
+        create_sub_tasks: list[ValidatedDatum] = [
+            {
+                "title": sub_task["title"],
+                "description": sub_task.get("description"),
+                "done": sub_task["done"],
+                "_order": order,
+            }
+            for order, sub_task in enumerate(validated_data)
+            if "uuid" not in sub_task
+        ]
+        # 2) determine which sub tasks have to be updated
+        update_sub_tasks: list[ValidatedDatumWithUuid] = [
+            {
+                "uuid": sub_task["uuid"],
+                "title": sub_task["title"],
+                "description": sub_task.get("description"),
+                "done": sub_task["done"],
+                "_order": order,
+            }
+            for order, sub_task in enumerate(validated_data)
+            if "uuid" in sub_task
+        ]
+        # 2a) If there are updatable sub tasks, we must have instance data
+        if self.instance is None and len(update_sub_tasks):
+            raise serializers.ValidationError(
+                _(
+                    "Sub tasks to be updated have been specified, but no sub "
+                    "task instances were provided."
+                )
+            )
+        # 3) check that UUIDs are not duplicated
+        unique_uuids = set(sub_task["uuid"] for sub_task in update_sub_tasks)
+        if len(unique_uuids) < len(update_sub_tasks):
+            raise serializers.ValidationError(
+                _("Duplicate UUID found among sub tasks to be updated")
+            )
+        # 4) check that all update UUIDs are part of instance sub tasks
+        #
+        # Otherwise, that would mean we are updating a sub task that we did not
+        # pass in as an instance. We only need to check this if we are
+        # updating. create_sub_tasks by definition can not contain UUIDs
+        if self.instance is not None:
+            instance_uuids = set(sub_task.uuid for sub_task in self.instance)
+            not_contained = unique_uuids - instance_uuids
+            if len(not_contained) > 0:
+                raise serializers.ValidationError(
+                    _(
+                        "At least one sub task UUID ({uuid}) could not be "
+                        "found amount the instance data. Check whether "
+                        "you have correctly passed all task's sub task "
+                        "instances."
+                    )
+                )
+        # But: The opposite, a UUID in our update sub task UUIDs not being
+        # present does not mean it was forgotten. It means that the sub task
+        # shall be deleted in update()
+        #
+        # 5) Check that when... what was I going to write here?
+        return {
+            "create_sub_tasks": create_sub_tasks,
+            "update_sub_tasks": update_sub_tasks,
+        }
+
+    def save(self, **kwargs: Any) -> list[SubTask]:
+        """Override save, allow passing complex validated data object."""
+        if self.instance is not None:
+            self.instance = self.update(self.instance, self.validated_data)
+            assert (
+                self.instance is not None
+            ), "`update()` did not return an object instance."
+        else:
+            self.instance = self.create(self.validated_data)
+            assert (
+                self.instance is not None
+            ), "`create()` did not return an object instance."
+        return self.instance
+
+    def create(self, validated_data: ValidatedData) -> list[SubTask]:
+        """Create several sub tasks."""
+        # XXX select task and sub tasks for update?
+        create_sub_tasks = validated_data["create_sub_tasks"]
+        tasks: list[SubTask] = super().create(create_sub_tasks)
+        return tasks
+
+    def update(
+        self,
+        instance: Sequence[SubTask],
+        validated_data: ValidatedData,
+    ) -> list[SubTask]:
+        """Update sub tasks."""
+        sub_tasks: list[SubTask] = []
+
+        create_sub_tasks = validated_data["create_sub_tasks"]
+        update_sub_tasks = validated_data["update_sub_tasks"]
+
+        # 1) delete missing sub tasks
+        existing_sub_task_uuids = set(sub_task.uuid for sub_task in instance)
+        update_sub_tasks_uuids = set(
+            sub_task.get("uuid") for sub_task in update_sub_tasks or []
+        )
+        # All the existing sub task uuids that are not in update sub task uuids
+        sub_tasks_to_delete = existing_sub_task_uuids - update_sub_tasks_uuids
+
+        deleted, _ = SubTask.objects.filter(
+            # XXX check if uuid is indexed
+            uuid__in=sub_tasks_to_delete
+        ).delete()
+
+        # Check for consistency after deletion
+        if not deleted == len(sub_tasks_to_delete):
+            raise Exception("Not all sub tasks were deleted")
+
+        # 2) update sub tasks and append to results list
+        if update_sub_tasks:
+            sub_task_mapping: dict[UUID, SubTask] = {
+                sub_task.uuid: sub_task for sub_task in instance
+            }
+            update_instances: list[SubTask] = []
+            for sub_task in update_sub_tasks:
+                current_instance = sub_task_mapping[sub_task["uuid"]]
+                current_instance.title = sub_task["title"]
+                current_instance.description = sub_task.get("description")
+                current_instance.done = sub_task["done"]
+                # pyright doesn't like this one here:
+                current_instance._order = sub_task["_order"]
+                update_instances.append(current_instance)
+            SubTask.objects.bulk_update(
+                update_instances,
+                ("title", "description", "done", "_order"),
+            )
+        # 3) create sub tasks and append to results list
+        if create_sub_tasks:
+            create_instances: list[SubTask] = [
+                SubTask(
+                    task=self.task,
+                    title=create_sub_task["title"],
+                    description=create_sub_task.get("description"),
+                    done=create_sub_task["done"],
+                    _order=create_sub_task["_order"],
+                )
+                for create_sub_task in create_sub_tasks
+            ]
+            SubTask.objects.bulk_create(create_instances)
+            sub_tasks += create_instances
+        # 4) fix order
+        return sub_tasks
+
+
+class SubTaskCreateUpdateSerializer(serializers.ModelSerializer[SubTask]):
+    """A sub task serializer that accepts a missing UUID."""
+
+    uuid = serializers.UUIDField(required=False)
+
+    def validate_uuid(self, value: Optional[UUID]) -> Optional[UUID]:
+        """
+        Validate that when instance is given, there will be no UUID.
+
+        | instance | value | valid? |
+        |       no |    no |     no |
+        |       no |   yes |    yes |
+        |      yes |    no |    yes |
+        |      yes |   yes |     no |
+
+        validity = instance XOR value
+        """
+        if self.instance is not None and value is None:
+            raise serializers.ValidationError(
+                _("Must specify UUID when updating a sub task")
+            )
+        # XXX instance isn't always given, so the following code will not work
+        # :(
+        # if self.instance is None and value is not None:
+        #     # raise serializers.ValidationError(
+        #     raise serializers.ValidationError(
+        #         _("Can't specify UUID when creating a sub task")
+        #     )
+        return value
+
+    def create(self, validated_data: dict[str, Any]) -> SubTask:
+        """Override create behavior and add task id to validated data."""
+        # We can't pop the value here, if this is called as part of a list
+        # serializer, the task will disappear
+        task: models.Task = self.context["task"]
+        validated_data = {
+            **validated_data,
+            "task_id": task.id,
+        }
+        return super().create(validated_data)
+
+    class Meta:
+        """Use the modified ListSerializer."""
+
+        model = SubTask
+        list_serializer_class = SubTaskListSerializer
+        fields = (
+            "uuid",
+            "title",
+            "description",
+            "done",
+        )
---
+++ workspace/test/serializers/test_sub_task.py
@@ -0,0 +1,410 @@
+"""Test sub task serializers."""
+from typing import (
+    Any,
+    Union,
+)
+from uuid import (
+    UUID,
+)
+
+import pytest
+
+from ...factory import (
+    SubTaskFactory,
+)
+from ...models import (
+    SubTask,
+    Task,
+)
+from ...serializers.sub_task import (
+    SubTaskCreateUpdateSerializer,
+)
+
+
+PayloadSingle = Any
+Context = dict[str, object]
+
+
+@pytest.fixture
+def payload_single() -> PayloadSingle:
+    """Return a single payload for a new sub task."""
+    # That's all we need! uuid can't exist yet, description is allowed
+    # to be empty
+    return {
+        "title": "This is a spectacular sub task",
+        "done": False,
+    }
+
+
+@pytest.fixture
+def context(task: Task) -> Context:
+    """Return serializer context."""
+    return {"task": task}
+
+
+@pytest.mark.django_db
+class TestSubTaskListSerializer:
+    """Test SubTaskListSerializer."""
+
+    # Serializing just existing stuff
+    # I don't assume we will use this serializer to serialize lists
+    # of sub tasks, since that is already covered by the default ListSerializer
+    # behavior
+    def test_empty_list(self) -> None:
+        """Test that nothing exciting happens when passing in an empty list."""
+        serializer = SubTaskCreateUpdateSerializer([], many=True)
+        assert serializer.data == []
+
+    def test_list_existing(self, sub_task: SubTask) -> None:
+        """Test that a list containing one instance is returned correctly."""
+        serializer = SubTaskCreateUpdateSerializer([sub_task], many=True)
+        expected: list[dict[str, Union[str, bool, None]]] = [
+            {
+                "uuid": str(sub_task.uuid),
+                "title": sub_task.title,
+                "description": sub_task.description,
+                "done": False,
+            }
+        ]
+        assert serializer.data == expected
+
+    # Serializing data
+    def test_empty_data(self) -> None:
+        """Test that nothing exciting happens when data is empty list."""
+        serializer = SubTaskCreateUpdateSerializer(data=[], many=True)
+        assert serializer.is_valid()
+        assert serializer.validated_data == {
+            "create_sub_tasks": [],
+            "update_sub_tasks": [],
+        }
+
+    def test_new_sub_task(
+        self,
+        payload_single: PayloadSingle,
+        context: Context,
+    ) -> None:
+        """Assert correct behavior when serializing existing task."""
+        assert SubTask.objects.count() == 0
+        serializer = SubTaskCreateUpdateSerializer(
+            data=[payload_single],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid()
+        assert serializer.validated_data == {
+            "create_sub_tasks": [
+                # Since description is optional, we will receive None back
+                # after serialization.
+                {**payload_single, "_order": 0, "description": None}
+            ],
+            "update_sub_tasks": [],
+        }
+        serializer.save()
+        assert SubTask.objects.count() == 1
+        new_sub_task = SubTask.objects.get()
+        assert new_sub_task.title == payload_single["title"]
+        assert new_sub_task.done == payload_single["done"]
+
+    def test_several_new_sub_tasks(
+        self,
+        payload_single: PayloadSingle,
+        context: Context,
+    ) -> None:
+        """Assert we can create several new sub tasks."""
+        n = 5
+        serializer = SubTaskCreateUpdateSerializer(
+            data=[payload_single] * n,
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid()
+        serializer.save()
+        assert SubTask.objects.count() == n
+
+    def test_update_instance_missing(self, sub_task: SubTask) -> None:
+        """Test we get a validation error when instance is missing."""
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {
+                    "uuid": str(sub_task.uuid),
+                    "title": "asd",
+                    "done": True,
+                }
+            ],
+            many=True,
+        )
+        assert serializer.is_valid() is False
+        assert (
+            "no sub task instances" in serializer.errors["non_field_errors"][0]
+        )
+
+    def test_delete_one(self, sub_task: SubTask, context: Context) -> None:
+        """Test a sub task is deleted when empty data are passed."""
+        assert SubTask.objects.count() == 1
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == 0, SubTask.objects.all()
+
+    def test_empty_instance_list(self, sub_task: SubTask) -> None:
+        """Test that passing an empty instance list will fail."""
+        assert SubTask.objects.count() == 1
+        serializer = SubTaskCreateUpdateSerializer(
+            [],
+            data=[{"uuid": str(sub_task.uuid), "title": "n/a", "done": False}],
+            many=True,
+        )
+        assert serializer.is_valid() is False, serializer.errors
+
+    def test_update_existing_sub_task(
+        self,
+        sub_task: SubTask,
+        context: Context,
+    ) -> None:
+        """Test updating sub task."""
+        new_title = "This is a new title, made for this sub task"
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {
+                    "uuid": str(sub_task.uuid),
+                    "title": new_title,
+                    "done": True,
+                }
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == 1
+        sub_task.refresh_from_db()
+        assert sub_task.title == new_title
+
+    def test_update_several_existing_sub_tasks(
+        self, task: Task, context: Context
+    ) -> None:
+        """Test updating several existing sub tasks."""
+        N = 5
+        sub_tasks = SubTaskFactory.create_batch(N, task=task)
+        new_title = "fancy factory fabricates fakes"
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {
+                    "uuid": str(sub_task.uuid),
+                    "title": new_title,
+                    "done": False,
+                }
+                for sub_task in sub_tasks
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == N
+        for sub_task in task.subtask_set.all():
+            assert sub_task.title == new_title
+
+    def test_update_one_create_one(
+        self, task: Task, context: Context, sub_task: SubTask
+    ) -> None:
+        """
+        Test with multiple sub tasks.
+
+        1) Order shall be adjusted
+        2) New sub task shall be created
+        3) Existing sub task title shall be updated
+        """
+        assert task.subtask_set.count() == 1
+        update_title = "Foo the bar, baz the flub"
+        new_title = "I am complete, new and creative, hello"
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                # We change the order for some xtra spice
+                {"title": new_title, "done": False},
+                {
+                    "uuid": str(sub_task.uuid),
+                    "title": update_title,
+                    "done": False,
+                },
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert task.subtask_set.count() == 2
+        a, b = task.subtask_set.all()
+        assert a.title == new_title
+        assert b.title == update_title
+
+    def test_change_order(self, task: Task, context: Context) -> None:
+        """Test changing the order of several sub tasks."""
+        N = 5
+        a, b, c, d, e = SubTaskFactory.create_batch(N, task=task)
+        title = "asd"
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {
+                    "uuid": str(c.uuid),
+                    "title": title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(b.uuid),
+                    "title": title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(d.uuid),
+                    "title": title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(a.uuid),
+                    "title": title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(e.uuid),
+                    "title": title,
+                    "done": False,
+                },
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == N
+        new_order: list[UUID] = list(
+            task.subtask_set.values_list("uuid", flat=True)
+        )
+        assert new_order == [c.uuid, b.uuid, d.uuid, a.uuid, e.uuid]
+
+    def test_create_one_delete_one(
+        self, task: Task, context: Context, sub_task: SubTask
+    ) -> None:
+        """
+        Test creating and deleting.
+
+        1) The new sub task shall be created
+        2) The missing sub task shall be deleted
+        """
+        assert task.subtask_set.count() == 1
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {"title": "new sub task who dis", "done": False},
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert task.subtask_set.count() == 1
+        new_sub_task = task.subtask_set.get()
+        assert new_sub_task.title == "new sub task who dis"
+        assert new_sub_task.uuid != sub_task.uuid
+
+    def test_create_and_change_order(
+        self, task: Task, context: Context
+    ) -> None:
+        """
+        Test creating sub tasks and changing the order of existing ones.
+
+        1) The new tasks shall be inserted at the right place.
+        2) Updated tasks shall be moved.
+        """
+        a, b = SubTaskFactory.create_batch(2, task=task)
+        title = "asd"
+        new_title = "i am a new sub task"
+        serializer = SubTaskCreateUpdateSerializer(
+            None,
+            data=[
+                {
+                    "title": new_title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(b.uuid),
+                    "title": title,
+                    "done": False,
+                },
+                {
+                    "title": new_title,
+                    "done": False,
+                },
+                {
+                    "uuid": str(a.uuid),
+                    "title": title,
+                    "done": False,
+                },
+            ],
+            many=True,
+            context=context,
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == 4
+        new_order: list[UUID] = list(
+            task.subtask_set.values_list("uuid", flat=True)
+        )
+        assert new_order[1] == b.uuid, new_order
+        assert new_order[3] == a.uuid, new_order
+
+
+@pytest.mark.django_db
+class TestSubTaskCreateUpdateSerializer:
+    """Test SubTaskCreateUpdateSerializer."""
+
+    # Serialize single existing
+    def test_existing_subtask(self, sub_task: SubTask) -> None:
+        """Test that nothing exciting happens when passing in an empty list."""
+        serializer = SubTaskCreateUpdateSerializer(sub_task)
+        assert serializer.data == {
+            "uuid": str(sub_task.uuid),
+            "title": sub_task.title,
+            "description": sub_task.description,
+            "done": False,
+        }
+
+    def test_new_sub_task(
+        self,
+        payload_single: PayloadSingle,
+        context: Context,
+    ) -> None:
+        """Test we can create a new sub task."""
+        serializer = SubTaskCreateUpdateSerializer(
+            data=payload_single, context=context
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == 1
+
+    def test_update_sub_task(
+        self,
+        sub_task: SubTask,
+        payload_single: PayloadSingle,
+    ) -> None:
+        """Test we can update an existing sub task."""
+        serializer = SubTaskCreateUpdateSerializer(
+            sub_task,
+            data={**payload_single, "uuid": str(sub_task.uuid)},
+        )
+        assert serializer.is_valid(), serializer.errors
+        serializer.save()
+        assert SubTask.objects.count() == 1
+        sub_task.refresh_from_db()
+        assert sub_task.title == payload_single["title"]
+        assert sub_task.done == payload_single["done"]
