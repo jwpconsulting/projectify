f5f3153a94936a695dc1266dca6035ce25b4dac7
Justus Perlwitz <hello@justus.pw>
2023-10-18 22:58:40 +0900

    Lib/Figma: InviteMember -> InviteWorkspaceUser

--- src/lib/figma/overlays/constructive/ConstructiveOverlay.svelte
+++ src/lib/figma/overlays/constructive/ConstructiveOverlay.svelte
@@ -2,8 +2,8 @@
     import CreateWorkspace from "$lib/figma/overlays/constructive/CreateWorkspace.svelte";
     import CreateWorkspaceBoard from "$lib/figma/overlays/constructive/CreateWorkspaceBoard.svelte";
     import CreateWorkspaceBoardSection from "$lib/figma/overlays/constructive/CreateWorkspaceBoardSection.svelte";
-    import InviteMember from "$lib/figma/overlays/constructive/InviteMember.svelte";
     import InviteMemberError from "$lib/figma/overlays/constructive/InviteMemberError.svelte";
+    import InviteWorkspaceUser from "$lib/figma/overlays/constructive/InviteWorkspaceUser.svelte";
     import RecoverWorkspaceBoard from "$lib/figma/overlays/constructive/RecoverWorkspaceBoard.svelte";
     import SkipOnboarding from "$lib/figma/overlays/constructive/SkipOnboarding.svelte";
     import UpdateWorkspaceBoard from "$lib/figma/overlays/constructive/UpdateWorkspaceBoard.svelte";
@@ -17,7 +17,7 @@
 {:else if target.kind === "createWorkspaceBoard"}
     <CreateWorkspaceBoard workspace={target.workspace} />
 {:else if target.kind === "inviteTeamMembers"}
-    <InviteMember workspace={target.workspace} />
+    <InviteWorkspaceUser workspace={target.workspace} />
 {:else if target.kind === "inviteTeamMembersNoSeatsLeft"}
     <InviteMemberError workspace={target.workspace} />
 {:else if target.kind === "createWorkspaceBoardSection"}
