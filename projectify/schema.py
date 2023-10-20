91573b3899687358a502b62bd0ed95409d1312b1
Justus Perlwitz <hello@justus.pw>
2023-10-20 11:05:52 +0900

    Remove gql queries

--- projectify/schema.py
+++ projectify/schema.py
@@ -7,12 +7,17 @@ import workspace.schema
 
 
 @strawberry.type
-class Query(
-    workspace.schema.Query,
-    user.schema.Query,
-    corporate.schema.Query,
-):
+class Query:
     """Query object."""
+
+    @strawberry.field
+    def use_drf(self) -> None:
+        """
+        Return nothing.
+
+        Please migrate everything to DRF.
+        """
+        return None
 
 
 @strawberry.type
