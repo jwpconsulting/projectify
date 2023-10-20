ab524e43afd732da5cbd20b35c4a65b7053551e4
Justus Perlwitz <hello@justus.pw>
2023-10-20 14:14:09 +0900

    Repository: Delegate error parsing to caller

--- src/lib/repository/util.ts
+++ src/lib/repository/util.ts
@@ -2,7 +2,7 @@ import vars from "$lib/env";
 import type { RepositoryContext } from "$lib/types/repository";
 import { getCookie } from "$lib/utils/cookie";
 
-import type { ApiResponse, ClientError } from "./types";
+import type { ApiResponse } from "./types";
 
 const getOptions: RequestInit = { credentials: "include" };
 
@@ -21,32 +21,19 @@ function putPostOptions<T>(method: "POST" | "PUT", data: T): RequestInit {
     };
 }
 
-function decodeClientError(error: unknown): ClientError {
-    if (Array.isArray(error)) {
-        const errors = error.flatMap((e) => (typeof e === "string" ? e : []));
-        return { kind: "list", errors };
-    } else if (error === null) {
-        throw new Error("Error was null");
-    } else if (error === undefined) {
-        throw new Error("Error was undefined");
-    }
-    // Not exactly clean...
-    return { kind: "dict", dict: error as Record<string, string> };
-}
-
-async function parseResponse<T>(response: Response): Promise<ApiResponse<T>> {
+async function parseResponse<T, E = unknown>(
+    response: Response
+): Promise<ApiResponse<T, E>> {
     const data: unknown = await response.json();
     if (response.ok) {
         return { kind: "ok", ok: true, data: data as T };
     }
+    const error: E = data as E;
     if (response.status === 400) {
-        const error = decodeClientError(data);
         return { kind: "badRequest", ok: false, error };
     } else if (response.status === 403) {
-        const error = decodeClientError(data);
         return { kind: "forbidden", ok: false, error };
     } else if (response.status === 404) {
-        const error = decodeClientError(data);
         return { kind: "notFound", ok: false, error };
     } else {
         // Unrecoverable
@@ -60,45 +47,45 @@ async function parseResponse<T>(response: Response): Promise<ApiResponse<T>> {
     }
 }
 
-export async function getWithCredentialsJson<T>(
+export async function getWithCredentialsJson<T, E = unknown>(
     url: string,
     repositoryContext: RepositoryContext
-): Promise<ApiResponse<T>> {
+): Promise<ApiResponse<T, E>> {
     const { fetch } = repositoryContext;
     const response = await fetch(`${vars.API_ENDPOINT}${url}`, getOptions);
-    return await parseResponse<T>(response);
+    return await parseResponse<T, E>(response);
 }
 
-export async function postWithCredentialsJson<T>(
+export async function postWithCredentialsJson<T, E = unknown>(
     url: string,
     data: unknown,
     repositoryContext: RepositoryContext
-): Promise<ApiResponse<T>> {
+): Promise<ApiResponse<T, E>> {
     const { fetch } = repositoryContext;
     const response = await fetch(
         `${vars.API_ENDPOINT}${url}`,
         putPostOptions("POST", data)
     );
-    return await parseResponse<T>(response);
+    return await parseResponse<T, E>(response);
 }
 
-export async function putWithCredentialsJson<T>(
+export async function putWithCredentialsJson<T, E = unknown>(
     url: string,
     data: unknown,
     repositoryContext: RepositoryContext
-): Promise<ApiResponse<T>> {
+): Promise<ApiResponse<T, E>> {
     const { fetch } = repositoryContext;
     const response = await fetch(
         `${vars.API_ENDPOINT}${url}`,
         putPostOptions("PUT", data)
     );
-    return await parseResponse<T>(response);
+    return await parseResponse<T, E>(response);
 }
 
 /*
  * Convenience method for callers that only care about 404 or not
  */
-export function handle404<T>(result: ApiResponse<T>): T | undefined {
+export function handle404<T>(result: ApiResponse<T, unknown>): T | undefined {
     if (result.kind === "notFound") {
         return undefined;
     } else if (result.kind === "ok") {
