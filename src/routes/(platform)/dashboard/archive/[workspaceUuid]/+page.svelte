022560d49f77e9b4f7963aae97ba45d4b8b0aa05
Justus Perlwitz <hello@justus.pw>
2023-11-02 17:45:13 +0900

    Lib: Clean up old settings page

--- src/routes/(platform)/dashboard/archive/[workspaceUuid]/+page.svelte
+++ src/routes/(platform)/dashboard/archive/[workspaceUuid]/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
     import WorkspaceArchive from "$lib/components/dashboard/workspaceArchive.svelte";
-    import SettingsPage from "$lib/components/SettingsPage.svelte";
 </script>
 
-<SettingsPage title="Archives">
-    <WorkspaceArchive />
-</SettingsPage>
+<WorkspaceArchive />
