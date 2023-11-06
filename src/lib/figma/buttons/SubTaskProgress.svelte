53ba11fada28b6801ef7e9bd2b0d957b9e558853
Justus Perlwitz <hello@justus.pw>
2023-11-06 18:01:02 +0900

    Lib: Fix sub task completion display

--- src/lib/figma/buttons/SubTaskProgress.svelte
+++ src/lib/figma/buttons/SubTaskProgress.svelte
@@ -1,34 +1,41 @@
 <script lang="ts">
     import { ViewList } from "@steeze-ui/heroicons";
     import { Icon } from "@steeze-ui/svelte-icon";
+    import { number } from "svelte-i18n";
 
     import type { Task } from "$lib/types/workspace";
 
     export let task: Task;
-    let subTaskCompletionPercentage: number | null = null;
+    let subTaskCompletionPercentage: number | undefined = undefined;
 
-    if (task.sub_tasks) {
-        const completed = task.sub_tasks.filter(
-            (subTask) => subTask.done
-        ).length;
-        const total = task.sub_tasks.length;
-        if (completed == total) {
-            subTaskCompletionPercentage = 100;
+    $: {
+        if (task.sub_tasks !== undefined && task.sub_tasks.length > 0) {
+            const completed = task.sub_tasks.filter(
+                (subTask) => subTask.done
+            ).length;
+            const total = task.sub_tasks.length;
+            if (completed == total) {
+                subTaskCompletionPercentage = 1;
+            } else {
+                subTaskCompletionPercentage = completed / total;
+            }
         } else {
-            subTaskCompletionPercentage = (completed / total) * 100;
+            subTaskCompletionPercentage = undefined;
         }
     }
 </script>
 
-{#if subTaskCompletionPercentage !== null}
-    <div class="flex shrink-0 flex-row items-center gap-2 px-2 py-1">
+<div class="flex shrink-0 flex-row items-center gap-2 px-2 py-1">
+    {#if subTaskCompletionPercentage !== undefined}
         <div class="flex flex-row items-center gap-2">
             <div class="flex h-5 w-5 flex-row items-center">
                 <Icon src={ViewList} theme="outline" class="text-primary" />
             </div>
             <div class="text-sm font-bold text-primary">
-                {subTaskCompletionPercentage} %
+                {$number(subTaskCompletionPercentage, {
+                    style: "percent",
+                })}
             </div>
         </div>
-    </div>
-{/if}
+    {/if}
+</div>
