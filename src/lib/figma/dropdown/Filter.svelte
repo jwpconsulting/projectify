5ba39e7a859d853ac7dfb05ebbd6076ea7eb283e
Justus Perlwitz <hello@justus.pw>
2023-11-08 15:48:41 +0900

    Lib/Figma/Dropdown: Fix Filter
    Fix flex shrinkage

--- src/lib/figma/dropdown/Filter.svelte
+++ src/lib/figma/dropdown/Filter.svelte
@@ -18,13 +18,13 @@
     on:click={click}
     class="flex max-w-full flex-row items-center gap-2 overflow-x-hidden rounded-lg border border-border p-2 hover:bg-secondary-hover"
 >
-    <Icon src={icon} theme="outline" class="h-6 w-6" />
+    <Icon src={icon} theme="outline" class="h-4 w-4 shrink-0" />
     <div class="min-w-0 shrink truncate text-left text-sm font-bold">
         {label}
     </div>
     <Icon
         src={open ? ChevronUp : ChevronDown}
         theme="outline"
-        class="h-4 w-4"
+        class="h-4 w-4 shrink-0"
     />
 </button>
