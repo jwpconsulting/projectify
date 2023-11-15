f81b9a67578ca8c89f484d3f5a5118ea8f021164
Justus Perlwitz <hello@justus.pw>
2023-11-15 18:11:29 +0900

    Refactor help page layout
    Share the layout with solutions for now

--- src/lib/components/solutions/Layout.svelte
+++ src/lib/components/solutions/Layout.svelte
@@ -1,11 +1,22 @@
+<script lang="ts"></script>
+
 <!-- Common layout for solution layouts -->
-<div class="flex flex-col items-center gap-20 pb-20">
+<div class="flex flex-col items-center gap-20 bg-foreground pb-20">
     <div class="flex w-full flex-col items-center bg-background p-6 sm:p-16">
         <div class="w-full max-w-3xl">
             <slot name="hero" />
         </div>
     </div>
-    <div class="w-full max-w-3xl p-6">
-        <slot name="content" />
-    </div>
+    {#if $$slots.side}
+        <div
+            class="flex w-full max-w-5xl grow flex-col gap-4 bg-foreground p-6 sm:flex-row sm:gap-8"
+        >
+            <slot name="side" />
+            <slot name="content" />
+        </div>
+    {:else}
+        <div class="w-full max-w-3xl p-6">
+            <slot name="content" />
+        </div>
+    {/if}
 </div>
--- src/routes/(storefront)/help/+layout.svelte
+++ src/routes/(storefront)/help/+layout.svelte
@@ -1,36 +1,31 @@
 <script lang="ts">
     import { _ } from "svelte-i18n";
 
+    import Layout from "$lib/components/solutions/Layout.svelte";
     import HelpDropdown from "$lib/figma/dropdown/HelpDropdown.svelte";
 </script>
 
-<div class="flex h-full flex-col">
-    <header class="bg-background px-4 py-8 md:px-16">
-        <div class="flex flex-col gap-4 sm:grid-cols-2 md:grid">
-            <heading class="flex flex-col gap-4">
-                <h1 class="text-4xl font-bold md:text-6xl">
-                    {$_("help.hero.header.text")}
-                </h1>
-                <p class="max-w-md text-2xl">
-                    {$_("help.hero.header.subtext")}
-                </p>
-            </heading>
-            <div class="min-w-full max-w-md">
-                <img
-                    src="/assets/help/help-hero.png"
-                    alt={$_("help.hero.image.alt")}
-                />
-            </div>
+<Layout>
+    <div slot="hero" class="flex flex-col gap-4 sm:grid-cols-2 md:grid">
+        <heading class="flex flex-col gap-4">
+            <h1 class="text-4xl font-bold md:text-6xl">
+                {$_("help.hero.header.text")}
+            </h1>
+            <p class="max-w-md text-2xl">
+                {$_("help.hero.header.subtext")}
+            </p>
+        </heading>
+        <div class="min-w-full max-w-md">
+            <img
+                src="/assets/help/help-hero.png"
+                alt={$_("help.hero.image.alt")}
+            />
         </div>
-    </header>
-    <main
-        class="flex grow flex-col gap-4 bg-foreground px-4 py-2 sm:flex-row sm:gap-8 sm:p-8 sm:px-12"
-    >
-        <div class="grow">
-            <HelpDropdown />
-        </div>
-        <div class="flex flex-col gap-7 bg-foreground">
-            <slot />
-        </div>
-    </main>
-</div>
+    </div>
+    <div slot="side" class="grow">
+        <HelpDropdown />
+    </div>
+    <div slot="content" class="flex flex-col gap-4">
+        <slot />
+    </div>
+</Layout>
