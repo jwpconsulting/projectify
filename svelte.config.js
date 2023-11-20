a1f785961a0ba74d28903ed9ffb1888c9ded5435
Justus Perlwitz <hello@justus.pw>
2023-11-20 15:44:28 +0900

    Add CSP directive
    Only allow script-src self
    
    See the followings links:
    - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/Sources#sources
    - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,11 @@ const config = {
             precompress: false,
             strict: true,
         }),
+        csp: {
+            directives: {
+                "script-src": ["self"],
+            },
+        },
         typescript: {
             config(config) {
                 return {
