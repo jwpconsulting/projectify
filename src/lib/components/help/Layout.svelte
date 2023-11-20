7693d38f56019b497703c3028fa7bc6ec0b7844a
Justus Perlwitz <hello@justus.pw>
2023-11-20 14:12:10 +0900

    Factor SkipLinks into help layout

--- src/lib/components/help/Layout.svelte
+++ src/lib/components/help/Layout.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
+    import { _ } from "svelte-i18n";
+
     import HeroLayout from "$lib/components/layouts/HeroLayout.svelte";
     import SolutionsHero from "$lib/components/solutions/SolutionsHero.svelte";
     import HelpDropdown from "$lib/figma/dropdown/HelpDropdown.svelte";
+    import Anchor from "$lib/funabashi/typography/Anchor.svelte";
     import type { SolutionsHeroContent } from "$lib/types/ui";
 
     import HelpSections from "./HelpSections.svelte";
-    import SkipLinks from "./SkipLinks.svelte";
 
     export let heroContent: SolutionsHeroContent;
     export let sections: { id: string; content: string; title: string }[];
 </script>
 
 <HeroLayout>
-    <!-- TODO just a placeholder -->
-    <!-- Might have to put all this inside the help layout instead -->
     <SolutionsHero slot="hero" {heroContent} />
     <div slot="side" class="grow sm:max-w-xs">
         <HelpDropdown />
     </div>
     <div slot="content" class="flex flex-col gap-4">
         <h2 class="text-4xl font-bold">{heroContent.title}</h2>
-        <SkipLinks {sections} />
+        <!-- skip links -->
+        <nav class="flex flex-col gap-4">
+            <h3 class="text-xl font-bold">{$_("help.skip")}</h3>
+            <ul class="flex list-inside list-disc flex-col gap-1">
+                {#each sections as section}
+                    <li>
+                        <Anchor
+                            href={`#${section.id}`}
+                            label={section.title}
+                        />
+                    </li>
+                {/each}
+            </ul>
+        </nav>
         <HelpSections {sections} />
     </div>
 </HeroLayout>
