45c1777e7de0b74b3c517865c94d795118f16ce8
Justus Perlwitz <hello@justus.pw>
2023-11-21 14:58:12 +0900

    Fix redirect after email confirmed

--- src/routes/(storefront)/(auth)/user/confirm-email/[email]/[token]/+page.ts
+++ src/routes/(storefront)/(auth)/user/confirm-email/[email]/[token]/+page.ts
@@ -1,11 +1,12 @@
 import { redirect } from "@sveltejs/kit";
 
 import { emailConfirmation } from "$lib/stores/user";
+import { logInUrl } from "$lib/urls/user";
 
 import type { PageLoadEvent } from "./$types";
 
 export async function load({ params: { email, token } }: PageLoadEvent) {
     await emailConfirmation(email, token);
 
-    throw redirect(302, "/login");
+    throw redirect(302, logInUrl);
 }
