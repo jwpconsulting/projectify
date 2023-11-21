f56460f3f3c5edcc467b55682fc61997921ece9a
Justus Perlwitz <hello@justus.pw>
2023-11-21 16:57:28 +0900

    Switch to ruff

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 target="${1-.}"
+echo "Testing $target"
 if poetry run mypy "$target"
 then
     echo "mypy ran successfully"
@@ -8,13 +9,28 @@ else
     echo "There was an error running mypy"
     exit 1
 fi
-if poetry run flake8 "$target"
+if ! poetry run ruff format --check "$target"
 then
-    echo "flake8 ran successfully"
-else
-    echo "There was an error running flake8"
-    exit 1
+    echo "There was an error running ruff format. Attempting to fix"
+    if ! poetry run ruff format "$target"
+    then
+        echo "ruff format didn't finish successfully"
+        exit 1
+    fi
 fi
+echo "ruff format ran successfully"
+
+if ! poetry run ruff "$target"
+then
+    echo "ruff didn't finish successfully"
+    if ! poetry run ruff --fix "$target"
+    then
+        echo "ruff didn't finish successfully"
+        exit 1
+    fi
+fi
+echo "ruff ran successfully"
+
 if poetry run pytest -n auto --maxprocesses=8 --lf "$target"
 then
     echo "pytest ran successfully"
