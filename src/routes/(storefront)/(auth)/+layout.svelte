32eea0146ffb7efbb8d876556bb93cbccc0a5cd5
Justus Perlwitz <hello@justus.pw>
2023-11-21 14:49:45 +0900

    Tweak storefront auth layout

--- src/routes/(storefront)/(auth)/+layout.svelte
+++ src/routes/(storefront)/(auth)/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center bg-background lg:p-16">
+<div class="flex grow flex-col items-center bg-background p-8 lg:p-16">
     <div class="w-full max-w-2xl rounded-lg bg-foreground lg:w-1/2">
         <slot />
     </div>
