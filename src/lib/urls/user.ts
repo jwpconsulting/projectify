82367c321e5bb92d2ffc6f7f583f09003394e9e4
Justus Perlwitz <hello@justus.pw>
2023-11-21 14:57:09 +0900

    Fix sentEmailConfirmationLinkUrl

--- src/lib/urls/user.ts
+++ src/lib/urls/user.ts
@@ -3,4 +3,4 @@ export const logOutUrl = "/user/log-out";
 export const signUpUrl = "/user/sign-up";
 export const requestPasswordResetUrl = "/user/request-password-reset";
 export const sentEmailConfirmationLinkUrl =
-    "/user/email-confirmation-link-sent";
+    "/user/sent-email-confirmation-link";
