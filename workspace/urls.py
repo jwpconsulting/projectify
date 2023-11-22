133c5ec879d2fa5b64c7c03162aa5aaa37145937
Justus Perlwitz <hello@justus.pw>
2023-11-22 18:10:31 +0900

    Refactor and enhance board CRUD with create

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,6 +1,12 @@
 """Workspace url patterns."""
 from django.urls import (
+    include,
     path,
+)
+
+from workspace.views.workspace_board import (
+    WorkspaceBoardCreate,
+    WorkspaceBoardRead,
 )
 
 from . import (
@@ -23,6 +29,24 @@ from .views.workspace_user import (
 
 app_name = "workspace"
 
+workspace_board_patterns = (
+    # Create
+    path(
+        "workspace-board",
+        WorkspaceBoardCreate.as_view(),
+        name="create",
+    ),
+    # Read
+    path(
+        "workspace-board/<uuid:workspace_board_uuid>",
+        WorkspaceBoardRead.as_view(),
+        name="read",
+    ),
+    # Update
+    # Delete
+)
+
+# TODO Rename all views to use standard CRUD terminology
 urlpatterns = (
     # Workspace
     # Create
@@ -41,6 +65,12 @@ urlpatterns = (
         "workspace/<uuid:workspace_uuid>",
         WorkspaceRetrieve.as_view(),
         name="workspace",
+    ),
+    # Archived workspace boards
+    path(
+        "workspace/<uuid:workspace_uuid>/workspace-boards-archived/",
+        views.WorkspaceBoardArchivedList.as_view(),
+        name="workspace-boards-archived",
     ),
     # Update
     # Delete
@@ -65,21 +95,10 @@ urlpatterns = (
         WorkspaceUserDestroy.as_view(),
         name="workspace-user-delete",
     ),
-    # WorkspaceBoard
-    # Create
-    # Read
     path(
-        "workspace-board/<uuid:workspace_board_uuid>",
-        views.WorkspaceBoardRetrieve.as_view(),
-        name="workspace-board",
+        "workspace-board/",
+        include((workspace_board_patterns, "workspace-boards")),
     ),
-    path(
-        "workspace/<uuid:workspace_uuid>/workspace-boards-archived/",
-        views.WorkspaceBoardArchivedList.as_view(),
-        name="workspace-boards-archived",
-    ),
-    # Update
-    # Delete
     # WorkspaceBoardSection
     # Create
     # Read
