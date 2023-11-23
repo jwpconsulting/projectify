5798f6f64483a1094534ff81d5bcf3003ac80dd2
Justus Perlwitz <hello@justus.pw>
2023-11-23 11:18:15 +0900

    Workspace: Refactor section views

--- workspace/views/workspace_board_section.py
+++ workspace/views/workspace_board_section.py
@@ -3,12 +3,16 @@ from uuid import UUID
 
 from django.utils.translation import gettext_lazy as _
 
-from rest_framework import serializers
+from rest_framework import generics, serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from workspace.models import WorkspaceBoard, WorkspaceBoardSection
+from workspace.models import (
+    WorkspaceBoard,
+    WorkspaceBoardSection,
+    WorkspaceBoardSectionQuerySet,
+)
 from workspace.serializers.workspace_board_section import (
     WorkspaceBoardSectionDetailSerializer,
 )
@@ -62,3 +66,39 @@ class WorkspaceBoardSectionCreate(APIView):
             instance=workspace_board_section,
         )
         return Response(data=output_serializer.data, status=201)
+
+
+# Read
+class WorkspaceBoardSectionRead(
+    generics.RetrieveAPIView[
+        WorkspaceBoardSection,
+        WorkspaceBoardSectionQuerySet,
+        WorkspaceBoardSectionDetailSerializer,
+    ]
+):
+    """Workspace board retrieve view."""
+
+    queryset = WorkspaceBoardSection.objects.prefetch_related(
+        "task_set",
+        "task_set__assignee",
+        "task_set__assignee__user",
+        "task_set__labels",
+        "task_set__subtask_set",
+    ).select_related(
+        "workspace_board",
+        "workspace_board__workspace",
+    )
+    serializer_class = WorkspaceBoardSectionDetailSerializer
+
+    def get_object(self) -> WorkspaceBoardSection:
+        """Return queryset with authenticated user in mind."""
+        user = self.request.user
+        qs = self.get_queryset()
+        qs = qs.filter_for_user_and_uuid(
+            user,
+            self.kwargs["workspace_board_section_uuid"],
+        )
+        workspace_board_section: WorkspaceBoardSection = (
+            generics.get_object_or_404(qs)
+        )
+        return workspace_board_section
