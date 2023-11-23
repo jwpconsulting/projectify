e5bbd85c8775a043b2318ae602957e85e98569cd
Justus Perlwitz <hello@justus.pw>
2023-11-23 11:54:14 +0900

    Workspace: Refactor chat message model
    Create some empty files for views and services. Chat messages will not
    be present in the release, so we don't do anything further.

---
+++ workspace/services/chat_message.py
@@ -0,0 +1,5 @@
+"""
+Chat message services.
+
+Chat messages will not be in the initial launch.
+"""
---
+++ workspace/views/chat_message.py
@@ -0,0 +1,9 @@
+"""
+Chat message views.
+
+Chat messages will not be in the initial release. Therefore, there will be
+no replacement DRF views here and the mutations can just be deleted.
+
+Maybe some brief remarks here would be good to show which permission checks
+were done so far.
+"""
