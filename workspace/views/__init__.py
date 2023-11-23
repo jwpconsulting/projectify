0669105621add25ddd1710b1bfa5d986807e5896
Justus Perlwitz <hello@justus.pw>
2023-11-23 11:18:15 +0900

    Workspace: Refactor section views

--- workspace/views/__init__.py
+++ workspace/views/__init__.py
@@ -13,41 +13,6 @@ from .. import (
 )
 
 
-class WorkspaceBoardSectionRetrieve(
-    generics.RetrieveAPIView[
-        models.WorkspaceBoardSection,
-        models.WorkspaceBoardSectionQuerySet,
-        serializers.WorkspaceBoardSectionDetailSerializer,
-    ]
-):
-    """Workspace board retrieve view."""
-
-    queryset = models.WorkspaceBoardSection.objects.prefetch_related(
-        "task_set",
-        "task_set__assignee",
-        "task_set__assignee__user",
-        "task_set__labels",
-        "task_set__subtask_set",
-    ).select_related(
-        "workspace_board",
-        "workspace_board__workspace",
-    )
-    serializer_class = serializers.WorkspaceBoardSectionDetailSerializer
-
-    def get_object(self) -> models.WorkspaceBoardSection:
-        """Return queryset with authenticated user in mind."""
-        user = self.request.user
-        qs = self.get_queryset()
-        qs = qs.filter_for_user_and_uuid(
-            user,
-            self.kwargs["workspace_board_section_uuid"],
-        )
-        workspace_board_section: models.WorkspaceBoardSection = (
-            get_object_or_404(qs)
-        )
-        return workspace_board_section
-
-
 class WorkspaceBoardArchivedList(
     generics.ListAPIView[
         models.WorkspaceBoard,
