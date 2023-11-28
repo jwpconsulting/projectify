a89f5d1996f82f47ed793bb0c181ed88f6577438
Justus Perlwitz <hello@justus.pw>
2023-11-28 18:30:33 +0900

    Workspace: Refactor task_create

--- workspace/test/services/test_task.py
+++ workspace/test/services/test_task.py
@@ -1,4 +1,6 @@
 """Test task services."""
+from datetime import datetime
+
 import pytest
 
 from workspace.factory import TaskFactory, WorkspaceBoardSectionFactory
@@ -6,7 +8,49 @@ from workspace.models import WorkspaceBoard
 from workspace.models.task import Task
 from workspace.models.workspace_board_section import WorkspaceBoardSection
 from workspace.models.workspace_user import WorkspaceUser
-from workspace.services.task import task_move_after
+from workspace.services.task import task_create, task_move_after
+
+
+# Create
+@pytest.mark.django_db
+def test_create_task(
+    workspace_board_section: WorkspaceBoardSection,
+    workspace_user: WorkspaceUser,
+) -> None:
+    """Test adding tasks to a workspace board."""
+    assert workspace_board_section.task_set.count() == 0
+    task = task_create(
+        who=workspace_user.user,
+        workspace_board_section=workspace_board_section,
+        title="foo",
+        description="bar",
+    )
+    assert workspace_board_section.task_set.count() == 1
+    task2 = task_create(
+        who=workspace_user.user,
+        workspace_board_section=workspace_board_section,
+        title="foo",
+        description="bar",
+    )
+    assert workspace_board_section.task_set.count() == 2
+    assert list(workspace_board_section.task_set.all()) == [task, task2]
+
+
+@pytest.mark.django_db
+def test_add_task_deadline(
+    workspace_board_section: WorkspaceBoardSection,
+    workspace_user: WorkspaceUser,
+    now: datetime,
+) -> None:
+    """Test adding a task with a deadline."""
+    task = task_create(
+        workspace_board_section=workspace_board_section,
+        who=workspace_user.user,
+        title="foo",
+        description="bar",
+        deadline=now,
+    )
+    assert task.deadline is not None
 
 
 @pytest.mark.django_db
