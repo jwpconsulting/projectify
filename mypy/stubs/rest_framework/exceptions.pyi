4e85fdeeace2639ffa53a51297dd1584c882080a
Justus Perlwitz <hello@justus.pw>
2023-11-29 16:18:46 +0900

    Update DRF mypy stubs

--- mypy/stubs/rest_framework/exceptions.pyi
+++ mypy/stubs/rest_framework/exceptions.pyi
@@ -1,21 +1,71 @@
-from collections.abc import Mapping
+# Taken from
+# https://raw.githubusercontent.com/encode/django-rest-framework/3.14.0/rest_framework/exceptions.py
+from collections.abc import Mapping, Sequence
 from typing import (
-    Any,
     Optional,
     Union,
 )
 
-class APIException(Exception):
-    def __init__(
-        self, detail: Optional[str] = None, code: Optional[int] = None
-    ) -> None: ...
-
 ErrorMessage = Union[str, Mapping[str, ErrorMessage]]
+Code = str
 
-class ValidationError(APIException):
+class APIException(Exception):
+    status_code: int
+    default_detail: str
+    default_code: str
+    detail: ErrorMessage
+
     def __init__(
         self,
         detail: Optional[ErrorMessage] = None,
-        code: Optional[int] = None,
-        params: Optional[dict[str, Any]] = None,
+        code: Optional[Code] = None,
+    ) -> None: ...
+    def get_codes(self) -> ErrorMessage: ...
+    def get_full_details(self) -> ErrorMessage: ...
+
+class ValidationError(APIException): ...
+class ParseError(APIException): ...
+class AuthenticationFailed(APIException): ...
+class NotAuthenticated(APIException): ...
+class PermissionDenied(APIException): ...
+class NotFound(APIException): ...
+
+class MethodNotAllowed(APIException):
+    def __init__(
+        self,
+        method: object,
+        detail: Optional[ErrorMessage] = None,
+        code: Optional[Code] = None,
+    ) -> None: ...
+
+AvailableRenderers = Sequence[str]
+
+class NotAcceptable(APIException):
+    available_renders: Optional[AvailableRenderers]
+    def __init__(
+        self,
+        method: object,
+        detail: Optional[ErrorMessage] = None,
+        code: Optional[Code] = None,
+        available_renderers: Optional[AvailableRenderers] = None,
+    ) -> None: ...
+
+class UnsupportedMediaType(APIException):
+    def __init__(
+        self,
+        media_type: object,
+        detail: Optional[ErrorMessage] = None,
+        code: Optional[Code] = None,
+    ) -> None: ...
+
+class Throttled(APIException):
+    extra_detail_singular: str
+    extra_detail_plural: str
+    default_code: str
+
+    def __init__(
+        self,
+        wait: Optional[int] = None,
+        detail: Optional[ErrorMessage] = None,
+        code: Optional[Code] = None,
     ) -> None: ...
