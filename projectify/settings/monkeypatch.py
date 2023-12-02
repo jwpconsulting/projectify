264ceef22dacbc35d37f3b9ac0971baddce74ac4
Justus Perlwitz <hello@justus.pw>
2023-12-02 11:53:32 +0900

    Remove factory typings

--- projectify/settings/monkeypatch.py
+++ projectify/settings/monkeypatch.py
@@ -9,8 +9,6 @@ from typing import (
 from django.contrib import admin  # For patching
 from django.views import generic  # For patching
 
-from factory import django  # For patching
-
 # XXX we actually need to patch GenericAPIView
 # from rest_framework import generics  # For patching
 # But if we import it at this point, importing from views will read the django
@@ -20,7 +18,6 @@ from factory import django  # For patching
 patchable_classes: Iterable[Any] = (
     admin.ModelAdmin,
     admin.TabularInline,
-    django.DjangoModelFactory,
     # XXX waiting for next version of DRF to come out with native subscription support
     # for GenericAPIView Justus 2023-07-20
     generic.View,
