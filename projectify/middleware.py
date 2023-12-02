11bb5dac98c0a86ba46d455a14e6cb0fec60ad24
Justus Perlwitz <hello@justus.pw>
2023-12-02 18:06:46 +0900

    Clean up middleware

--- projectify/middleware.py
+++ projectify/middleware.py
@@ -1,35 +1,4 @@
 """Projectify middlewares."""
-# from django.db import (
-#     transaction,
-# )
-#
-# from .loader import (
-#     Loader,
-# )
-
-
-# Graphene middlewares
-# def atomic_transaction_middleware(next, root, info, *args, **kwargs):
-#     """Wrap graphql query/mutation in transaction."""
-#     # Ensure middleware is only run once
-#     if hasattr(info.context, "atomic_transaction_middleware"):
-#         return next(root, info, *args, **kwargs)
-#     info.context.atomic_transaction_middleware = True
-#     with transaction.atomic():
-#         return_value = next(root, info, *args, **kwargs)
-#     return return_value
-#
-#
-# def loader_middleware(next, root, info, *args, **kwargs):
-#     """Add loaders to info.context."""
-#     # Ensure middleware is only run once
-#     if hasattr(info.context, "loader_middleware"):
-#         return next(root, info, *args, **kwargs)
-#     info.context.loader_middleware = True
-#     info.context.loader = Loader()
-#     return next(root, info, *args, **kwargs)
-
-
 # https://stackoverflow.com/a/47888695
 from typing import (
     Callable,
