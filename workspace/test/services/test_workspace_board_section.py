d250919fd5d476708f105f2b971a62c8fe4ed725
Justus Perlwitz <hello@justus.pw>
2023-12-04 11:19:51 +0900

    Workspace: Test non-empty section delete

--- workspace/test/services/test_workspace_board_section.py
+++ workspace/test/services/test_workspace_board_section.py
@@ -1,12 +1,43 @@
 """Test workspace board section services."""
 import pytest
+from rest_framework import serializers
 
 from workspace.models import WorkspaceBoard
+from workspace.models.task import Task
 from workspace.models.workspace_board_section import WorkspaceBoardSection
 from workspace.models.workspace_user import WorkspaceUser
+from workspace.services.task import task_delete
 from workspace.services.workspace_board_section import (
+    workspace_board_section_delete,
     workspace_board_section_move,
 )
+
+
+@pytest.mark.django_db
+def test_delete_non_empty_section(
+    workspace_user: WorkspaceUser,
+    workspace_board_section: WorkspaceBoardSection,
+    # Make sure there is a task
+    task: Task,
+) -> None:
+    """Assert we can't delete a non-empty section."""
+    with pytest.raises(serializers.ValidationError) as error:
+        workspace_board_section_delete(
+            workspace_board_section=workspace_board_section,
+            who=workspace_user.user,
+        )
+    assert error.match("still has tasks")
+
+    # ... but if we delete the task,
+    task_delete(
+        task=task,
+        who=workspace_user.user,
+    )
+    # it will work
+    workspace_board_section_delete(
+        workspace_board_section=workspace_board_section,
+        who=workspace_user.user,
+    )
 
 
 @pytest.mark.django_db
