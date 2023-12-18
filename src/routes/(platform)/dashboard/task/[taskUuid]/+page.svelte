9c722f386eb31daf0e5f91e4ac1d4a856bbf78fd
Justus Perlwitz <hello@justus.pw>
2023-12-18 09:19:19 +0900

    Bake ReadTaskCard into task route

--- src/routes/(platform)/dashboard/task/[taskUuid]/+page.svelte
+++ src/routes/(platform)/dashboard/task/[taskUuid]/+page.svelte
@@ -1,11 +1,124 @@
 <script lang="ts">
-    import ReadTaskCard from "$lib/figma/screens/task/ReadTaskCard.svelte";
+    import { _, number } from "svelte-i18n";
+
+    import TaskUpdateBar from "$lib/figma/buttons/TaskUpdateBar.svelte";
+    import Breadcrumbs from "$lib/figma/screens/task/Breadcrumbs.svelte";
+    import Fields from "$lib/figma/screens/task/Fields.svelte";
+    import Layout from "$lib/figma/screens/task/Layout.svelte";
+    import ReadSubTasks from "$lib/figma/screens/task/ReadSubTasks.svelte";
+    import TaskDescription from "$lib/figma/screens/task/TaskDescription.svelte";
+    import TaskDueDate from "$lib/figma/screens/task/TaskDueDate.svelte";
+    import TaskLabel from "$lib/figma/screens/task/TaskLabel.svelte";
+    import TaskTitle from "$lib/figma/screens/task/TaskTitle.svelte";
+    import TaskUser from "$lib/figma/screens/task/TaskUser.svelte";
+    import TopBar from "$lib/figma/screens/task/TopBar.svelte";
+    import type { TaskUpdateBarState } from "$lib/figma/types";
+    import Button from "$lib/funabashi/buttons/Button.svelte";
+    import SquovalIcon from "$lib/funabashi/buttons/SquovalIcon.svelte";
+    import { currentTask } from "$lib/stores/dashboard";
+    import { openContextMenu } from "$lib/stores/globalUi";
+    import {
+        getDashboardWorkspaceBoardSectionUrl,
+        getDashboardWorkspaceBoardUrl,
+        getTaskEditUrl,
+        getTaskUrl,
+    } from "$lib/urls";
+    import { coerceIsoDate } from "$lib/utils/date";
 
     import type { PageData } from "./$types";
 
-    export let data: PageData;
+    import { goto } from "$app/navigation";
 
-    const { task } = data;
+    export let data: PageData;
+    const state: TaskUpdateBarState = "task";
+
+    $: task = $currentTask ?? data.task;
+    $: subTasks = task.sub_tasks;
+    $: workspaceBoardSection = task.workspace_board_section;
+    $: workspaceBoard = workspaceBoardSection.workspace_board;
+
+    let contextMenuRef: HTMLElement;
+
+    async function onInteract() {
+        await goto(getTaskEditUrl(task.uuid));
+    }
+
+    $: crumbs = [
+        {
+            label: workspaceBoard.title,
+            href: getDashboardWorkspaceBoardUrl(workspaceBoard.uuid),
+        },
+        {
+            label: workspaceBoardSection.title,
+            href: getDashboardWorkspaceBoardSectionUrl(
+                workspaceBoardSection.uuid
+            ),
+        },
+        { label: $number(task.number), href: getTaskUrl(task.uuid) },
+    ];
 </script>
 
-<ReadTaskCard {task} />
+<Layout>
+    <TopBar slot="top-bar" {workspaceBoardSection}>
+        <Breadcrumbs slot="breadcrumbs" {crumbs} />
+        <svelte:fragment slot="buttons">
+            <Button
+                grow={false}
+                color="blue"
+                size="small"
+                style={{ kind: "primary" }}
+                label={$_("task-screen.edit")}
+                action={{ kind: "a", href: getTaskEditUrl(task.uuid) }}
+            />
+            <div bind:this={contextMenuRef}>
+                <SquovalIcon
+                    icon="dotsVertical"
+                    state="active"
+                    action={{
+                        kind: "button",
+                        action: openContextMenu.bind(
+                            null,
+                            {
+                                kind: "task",
+                                task,
+                                location: "task",
+                                workspaceBoardSection,
+                            },
+                            contextMenuRef
+                        ),
+                    }}
+                />
+            </div>
+        </svelte:fragment>
+    </TopBar>
+    <TaskUpdateBar slot="tab-bar-mobile" kind="mobile" {state} {task} />
+    <svelte:fragment slot="content">
+        <Fields>
+            <TaskTitle slot="title" title={task.title} readonly {onInteract} />
+            <TaskUser
+                slot="assignee"
+                workspaceUser={task.assignee}
+                {onInteract}
+            />
+            <TaskLabel
+                slot="labels"
+                labels={task.labels}
+                {onInteract}
+                readonly
+            />
+            <TaskDueDate
+                slot="due-date"
+                dueDate={task.deadline && coerceIsoDate(task.deadline)}
+                readonly
+                {onInteract}
+            />
+            <TaskDescription
+                slot="description"
+                readonly
+                description={task.description}
+                {onInteract}
+            />
+        </Fields>
+        <ReadSubTasks {subTasks} {onInteract} />
+    </svelte:fragment>
+</Layout>
