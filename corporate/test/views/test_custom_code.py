e25e6782bf83bf48b9845184bdda54cd392c09b5
Justus Perlwitz <hello@justus.pw>
2023-12-25 11:47:45 +0900

    Corporate: Make custom codes redemption API

--- corporate/test/views/test_custom_code.py
+++ corporate/test/views/test_custom_code.py
@@ -1,1 +1,73 @@
 """Test custom code views."""
+from django.urls import reverse
+
+import pytest
+from rest_framework.test import APIClient
+
+from corporate.models.custom_code import CustomCode
+from corporate.models.customer import Customer
+from corporate.services.customer import customer_check_active_for_workspace
+from pytest_types import DjangoAssertNumQueries
+from workspace.models.workspace import Workspace
+
+
+@pytest.mark.django_db
+class TestCustomCodeRedeem:
+    """Test redeeming custom codes."""
+
+    @pytest.fixture
+    def resource_url(self, workspace: Workspace) -> str:
+        """Return URL to this view."""
+        return reverse(
+            "corporate:custom-codes:redeem-custom-code",
+            args=(str(workspace.uuid),),
+        )
+
+    def test_redeeming_invalid_code(
+        self,
+        rest_user_client: APIClient,
+        resource_url: str,
+        django_assert_num_queries: DjangoAssertNumQueries,
+        workspace: Workspace,
+    ) -> None:
+        """Test that nothing bad happens with an invalid code."""
+        assert (
+            customer_check_active_for_workspace(workspace=workspace) == "trial"
+        )
+        with django_assert_num_queries(8):
+            response = rest_user_client.post(
+                resource_url,
+                data={"code": "thiscodedoesnotexist"},
+            )
+            assert response.status_code == 400, response.data
+        assert (
+            customer_check_active_for_workspace(workspace=workspace) == "trial"
+        )
+
+    def test_redeeming_valid_code(
+        self,
+        rest_user_client: APIClient,
+        resource_url: str,
+        django_assert_num_queries: DjangoAssertNumQueries,
+        custom_code: CustomCode,
+        workspace: Workspace,
+        unpaid_customer: Customer,
+    ) -> None:
+        """Test that workspace subscription is actived correctly."""
+        assert unpaid_customer.seats != 20
+        assert (
+            customer_check_active_for_workspace(workspace=workspace) == "trial"
+        )
+        with django_assert_num_queries(9):
+            response = rest_user_client.post(
+                resource_url,
+                data={"code": custom_code.code},
+            )
+            assert response.status_code == 204, response.data
+
+        unpaid_customer.refresh_from_db()
+        assert (
+            customer_check_active_for_workspace(workspace=workspace) == "full"
+        )
+        # Taken from seats in conftest.py
+        assert unpaid_customer.seats == 20
--- corporate/views/custom_code.py
+++ corporate/views/custom_code.py
@@ -1,1 +1,42 @@
 """Custom code views."""
+from uuid import UUID
+
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import exceptions, serializers, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from corporate.services.custom_code import custom_code_redeem
+from workspace.selectors.workspace import workspace_find_by_workspace_uuid
+
+
+class CustomCodeRedeem(APIView):
+    """Redeem a custom code for a workspace customer."""
+
+    class InputSerializer(serializers.Serializer):
+        """Serializer that takes in a CustomCode's code."""
+
+        code = serializers.CharField()
+
+    def post(self, request: Request, workspace_uuid: UUID) -> Response:
+        """Handle POST."""
+        workspace = workspace_find_by_workspace_uuid(
+            workspace_uuid=workspace_uuid,
+            who=request.user,
+        )
+        if workspace is None:
+            raise exceptions.NotFound(
+                _("No workspace was found for this workspace uuid")
+            )
+
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        # Redeem custom code here
+        custom_code_redeem(
+            who=request.user, code=data["code"], workspace=workspace
+        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
