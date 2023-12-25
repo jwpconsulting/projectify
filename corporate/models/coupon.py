2fc460b699e6ee1c6f629d3c0b3affa2018f2b0c
Justus Perlwitz <hello@justus.pw>
2023-12-25 13:26:27 +0900

    Corporate: Rename custom code to coupon

--- corporate/models/coupon.py
+++ corporate/models/coupon.py
@@ -1,20 +1,20 @@
 """
-Custom code model.
+Coupon model.
 
-Custom codes are used to help beta testers and other interested users sign
+Coupons are used to help beta testers and other interested users sign
 up for the Projectify application.
 
 Sign up life cycle
 
-1. Admin creates a custom code
-2. Send custom code to user, including instructions on how to use it.
+1. Admin creates a coupon
+2. Send coupon to user, including instructions on how to use it.
 3. User signs up on Projectify, creates Workspace during onboarding
-4. User goes to workspace billing settings and enters custom code.
+4. User goes to workspace billing settings and enters coupon.
 5. Custom plan is unlocked for this workspace, with the available seats set
 to the sign up code seats.
-6. Custom code is marked as used and cannot be reused.
+6. Coupon is marked as used and cannot be reused.
 
-A custom code does not:
+A coupon does not:
 
 - Have an expiry date by which it needs to be used (for now at least)
 - Assign an expiring plan to a customer. Seats will be available indefinitely
@@ -23,7 +23,7 @@ A custom code does not:
 in order to offer rebates and special offers
 
 
-State transitions viewed from CustomCode
+State transitions viewed from Coupon
 
         User enters sign up code
            │
@@ -49,11 +49,11 @@ State transitions viewed from Customer
           ┌──┴───┐       ┌──────┐        ┌──────┐          ┌─────────┐
 ─────────►│Unpaid├──────►│Custom├───────►│Active│────────► │Cancelled│
 Create    └──────┘Enter  └──────┘ Create └──────┘ Subscr.  └─────────┘
-workspace         custom     ▲    subscription│   expires or     │
+workspace         coupon     ▲    subscription│   expires or     │
 and               code       │                │   is cancelled   │
 customer                     └────────────────┘                  │
                              ▲  Enter                            │
-                             │  custom                           │
+                             │  coupon                           │
                              │  code                             │
                              └───────────────────────────────────┘
 
@@ -65,9 +65,9 @@ from projectify.lib.models import BaseModel
 from .customer import Customer
 
 
-class CustomCode(BaseModel):
+class Coupon(BaseModel):
     """
-    Store information about a custom code.
+    Store information about a coupon.
 
     The code can be used up by updating "used" with the current timestamp,
     and setting customer.
@@ -75,15 +75,18 @@ class CustomCode(BaseModel):
     NOTE: a deleted customer will set "customer" back to null, but it does not
     mean that this sign up code has not been used.
 
-    The idea is that we want to avoid re-using custom codes after a related
-    customer has been deleted. We want to keep a permanent record of all custom
-    codes ever created. Deleting a custom code will make it difficult to keep
+    The idea is that we want to avoid re-using coupons after a related customer
+    has been deleted. We want to keep a permanent record of all coupons ever
+    created. Deleting a coupon will make it difficult to keep
     a record.
 
-    A custom code stores how many seats will be unlocked for a customer, when
+    A coupon stores how many seats will be unlocked for a customer, when
     it is used for that customer.
 
-    A custom code MUST not be usable twice, even for the same customer.
+    A coupon MUST not be usable twice, even for the same customer.
+
+    A customer with a custom (coupon-activated) subscription MUST not be able
+    to activate another coupon.
     """
 
     code = models.CharField(max_length=200, unique=True, db_index=True)
---
+++ corporate/test/models/test_coupon.py
@@ -0,0 +1,1 @@
+"""Test coupon model."""
