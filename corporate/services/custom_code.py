522180afe10f0199b2841df9c4835b56347deafd
Justus Perlwitz <hello@justus.pw>
2023-12-25 09:21:28 +0900

    Corporate: Add custom code create function

--- corporate/services/custom_code.py
+++ corporate/services/custom_code.py
@@ -1,17 +1,40 @@
 """Custom code services."""
-# TODO
-# from typing import Optional
-# from projectify.lib.auth import validate_perm
-# from user.models import User
-# from ..models.custom_code import CustomCode
+from typing import Optional
+
+from django.utils.crypto import get_random_string
+
+from projectify.lib.auth import validate_perm
+from user.models import User
+
+from ..models.custom_code import CustomCode
+
+# Took this as an inspiration
+# https://github.com/tytso/pwgen/blob/1459a31e07fa208cddb2c4f3f72071503c37b8bc/pw_rand.c#L20
+# But then figured sticking to lower case is easier to say when dictating
+# custom code over telephone or other flaky communication channels and
+# handwriting. Just in case.
+CUSTOM_CODE_CHARS = "abcdefhijkmnpqrstuvwxy347"
+
+# If we use the formula
+#
+# log_2(len(chars) ^ length)
+# where len(chars) = 25
+#
+# suggested in get_random_string, we have
+# length: 10, bit length =~ 32 bits
+# length: 20, bit length =~ 64 bits
+# Good enough? custom code creation isn't end-user facing anyway, so if
+# in 1 out of 2^32 cases it fails, that's tolerable.
 
 
-# def custom_code_create(
-#     *,
-#     who: User,
-#     prefix: Optional[str],
-#     seats: int,
-# ) -> CustomCode:
-#     """Create a custom code for seats, for a user, with a given prefix."""
-#     # validate_perm("corporate.create_custom_code", who)
-#     # TODO CustomCode.objects.create(prefix
+def custom_code_create(
+    *,
+    who: User,
+    seats: int,
+    prefix: Optional[str] = None,
+) -> CustomCode:
+    """Create a custom code for seats, for a user, with a given prefix."""
+    suffix = get_random_string(10, CUSTOM_CODE_CHARS)
+    code = f"{prefix}-{suffix}"
+    validate_perm("corporate.create_custom_code", who)
+    return CustomCode.objects.create(code=code, used=None, seats=seats)
--- corporate/test/services/test_custom_code.py
+++ corporate/test/services/test_custom_code.py
@@ -1,1 +1,27 @@
 """Test custom code services."""
+from django.core.exceptions import PermissionDenied
+
+import pytest
+
+from corporate.services.custom_code import custom_code_create
+from user.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCustomCodeCreate:
+    """Test custom code creation."""
+
+    def test_authorization(self, user: User, superuser: User) -> None:
+        """Test that superusers can create codes, but no regular user."""
+        seats = 20
+        with pytest.raises(PermissionDenied):
+            custom_code_create(who=user, seats=seats)
+        custom_code_create(who=superuser, seats=seats)
+
+    def test_code_is_unique(self, superuser: User) -> None:
+        """Test that codes will not collide."""
+        seats = 10
+        code1 = custom_code_create(who=superuser, seats=seats, prefix="asd")
+        code2 = custom_code_create(who=superuser, seats=seats, prefix="asd")
+        assert code1.code != code2.code
