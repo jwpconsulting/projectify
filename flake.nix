d903c29b09ef2c64276d94163ec71aff4dfdd103
Justus Perlwitz <hello@justus.pw>
2024-01-06 10:48:16 +0900

    Integrate nix/direnv

--- flake.nix
+++ flake.nix
@@ -1,3 +1,12 @@
 {
-  outputs = inputs: { };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "Projectify frontend";
+      shell = ./shell.nix;
+    };
 }
