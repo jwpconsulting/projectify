794388a597dd9bc4cbf00495347c2f27193af9b6
Justus Perlwitz <hello@justus.pw>
2024-01-09 11:09:58 +0900

    Workspace: Refactor signal send into new module

--- workspace/signals.py
+++ workspace/signals.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
-# Copyright (C) 2022, 2023 JWP Consulting GK
+# Copyright (C) 2022-2024 JWP Consulting GK
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -18,9 +18,6 @@
 import logging
 from typing import (
     TYPE_CHECKING,
-    Any,
-    Union,
-    cast,
 )
 
 from django.db import (
@@ -32,11 +29,6 @@ from django.db.models.signals import (
 )
 from django.dispatch import (
     receiver,
-)
-
-from asgiref.sync import async_to_sync as _async_to_sync
-from channels.layers import (
-    get_channel_layer,
 )
 
 from user.signal_defs import (
@@ -52,10 +44,14 @@ from workspace.models.workspace_board import WorkspaceBoard
 from workspace.models.workspace_board_section import WorkspaceBoardSection
 from workspace.models.workspace_user import WorkspaceUser
 from workspace.models.workspace_user_invite import WorkspaceUserInvite
+from workspace.services.signals import (
+    send_task_change_signal,
+    send_workspace_board_change_signal,
+    send_workspace_change_signal,
+)
 from workspace.services.workspace import (
     workspace_add_user,
 )
-from workspace.types import ConsumerEvent
 
 from . import (
     signal_defs,
@@ -70,28 +66,7 @@ if TYPE_CHECKING:
 Unknown = object
 
 
-HasOrIsWorkspace = Union[
-    Workspace,
-    Label,
-    WorkspaceUser,
-    WorkspaceBoard,
-]
-HasOrIsWorkspaceBoard = Union[
-    WorkspaceBoard,
-    WorkspaceBoardSection,
-    Task,
-    TaskLabel,
-    SubTask,
-]
-HasOrIsTask = Union[Task, TaskLabel, SubTask, ChatMessage]
-
-
 logger = logging.getLogger(__name__)
-
-# TODO AsyncToSync is typed in a newer (unreleased) version of asgiref
-# which we indirectly install with channels, which has not been
-# renewed in a while Justus 2023-05-19
-async_to_sync = cast(Any, _async_to_sync)
 
 
 @receiver(signal_defs.workspace_user_invited)
@@ -104,88 +79,6 @@ def send_invitation_email(instance: WorkspaceUser, **kwargs: object) -> None:
 
     email = emails.WorkspaceUserInviteEmail(instance)
     email.send()
-
-
-def group_send(destination: str, event: ConsumerEvent) -> None:
-    """Send message to a channels group."""
-    channel_layer = get_channel_layer()
-    if not channel_layer:
-        raise Exception("Did not get channel layer")
-    async_to_sync(channel_layer.group_send)(
-        destination,
-        event,
-    )
-
-
-def send_workspace_change_signal(instance: HasOrIsWorkspace) -> None:
-    """Send workspace.change signal to correct group."""
-    match instance:
-        case Workspace():
-            workspace = instance
-        case Label():
-            workspace = instance.workspace
-        case WorkspaceUser():
-            workspace = instance.workspace
-        case WorkspaceBoard():
-            workspace = instance.workspace
-    uuid = str(workspace.uuid)
-    group_send(
-        f"workspace-{uuid}",
-        {
-            "type": "workspace.change",
-            "uuid": uuid,
-        },
-    )
-
-
-def send_workspace_board_change_signal(
-    instance: HasOrIsWorkspaceBoard,
-) -> None:
-    """Send workspace_board.change signal to correct group."""
-    match instance:
-        case WorkspaceBoard():
-            workspace_board = instance
-        case WorkspaceBoardSection():
-            workspace_board = instance.workspace_board
-        case Task():
-            workspace_board = instance.workspace_board_section.workspace_board
-        case TaskLabel():
-            workspace_board = (
-                instance.task.workspace_board_section.workspace_board
-            )
-        case SubTask():
-            workspace_board = (
-                instance.task.workspace_board_section.workspace_board
-            )
-    uuid = str(workspace_board.uuid)
-    group_send(
-        f"workspace-board-{uuid}",
-        {
-            "type": "workspace.board.change",
-            "uuid": uuid,
-        },
-    )
-
-
-def send_task_change_signal(instance: HasOrIsTask) -> None:
-    """Send task.change signal to correct group."""
-    match instance:
-        case Task():
-            task = instance
-        case TaskLabel():
-            task = instance.task
-        case SubTask():
-            task = instance.task
-        case ChatMessage():
-            task = instance.task
-    uuid = str(task.uuid)
-    group_send(
-        f"task-{uuid}",
-        {
-            "type": "task.change",
-            "uuid": uuid,
-        },
-    )
 
 
 @receiver(post_save, sender=Workspace)
