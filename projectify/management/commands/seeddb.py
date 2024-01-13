cbfd278060e2ecd58a2ef412b30901329a06be10
Justus Perlwitz <hello@justus.pw>
2024-01-13 09:10:22 +0900

    Workspace: Rename task.deadline -> due_date

--- projectify/management/commands/seeddb.py
+++ projectify/management/commands/seeddb.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
-# Copyright (C) 2021, 2022, 2023 JWP Consulting GK
+# Copyright (C) 2021-2024 JWP Consulting GK
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -188,7 +188,7 @@ class Command(BaseCommand):
                         TASK_DESCRIPTION_SENTENCES
                     ),
                     workspace_board_section=workspace_board_section,
-                    deadline=self.fake.date_time(tzinfo=timezone.utc),
+                    due_date=self.fake.date_time(tzinfo=timezone.utc),
                     workspace=together["workspace"],
                     _order=_order,
                     number=next(together["number"]),
@@ -334,7 +334,7 @@ class Command(BaseCommand):
                     ),
                     description=self.fake.paragraph(),
                     workspace=workspace,
-                    deadline=self.fake.date_time(tzinfo=timezone.utc),
+                    due_date=self.fake.date_time(tzinfo=timezone.utc),
                 )
                 for workspace in workspaces
                 for _ in range(self.n_workspace_boards)
