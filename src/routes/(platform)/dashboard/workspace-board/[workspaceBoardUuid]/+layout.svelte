646d3ee20917681d8bd6689a315a0f7a324c9e65
Justus Perlwitz <hello@justus.pw>
2024-02-05 17:56:42 +0900

    Only show search when tasks are present

--- src/routes/(platform)/dashboard/workspace-board/[workspaceBoardUuid]/+layout.svelte
+++ src/routes/(platform)/dashboard/workspace-board/[workspaceBoardUuid]/+layout.svelte
@@ -21,6 +21,7 @@
 
     import Button from "$lib/funabashi/buttons/Button.svelte";
     import InputField from "$lib/funabashi/input-fields/InputField.svelte";
+    import type { WorkspaceBoardSectionWithTasks } from "$lib/types/workspace";
     import { getWorkspaceBoardSearchUrl } from "$lib/urls/dashboard";
 
     import type { LayoutData } from "./$types";
@@ -31,37 +32,42 @@
     let searchInput: string | undefined = undefined;
 
     $: canSearch = searchInput !== undefined;
+    $: workspaceBoardHasTasks = workspaceBoard.workspace_board_sections.some(
+        (s: WorkspaceBoardSectionWithTasks) => s.tasks.length > 0,
+    );
 </script>
 
 <div
     class="flex h-full flex-col items-center gap-4 overflow-y-auto bg-background py-4"
 >
-    <form
-        action={getWorkspaceBoardSearchUrl(workspaceBoard)}
-        class="flex w-full max-w-md flex-col gap-2 rounded-xl bg-foreground px-4 py-4"
-    >
-        <!-- XXX definitely not ideal, placeholder will disappear after input -->
-        <InputField
-            style={{ inputType: "text" }}
-            label={$_("dashboard.search-task.input.label")}
-            placeholder={$_("dashboard.search-task.input.placeholder")}
-            name="search"
-            bind:value={searchInput}
-            showClearButton={false}
-            required
-        />
-        <Button
-            label={$_("dashboard.search-task.button")}
-            action={{ kind: "submit", disabled: !canSearch }}
-            style={{
-                kind: "tertiary",
-                icon: { position: "left", icon: Search },
-            }}
-            size="small"
-            color="blue"
-            grow={false}
-        />
-    </form>
+    {#if workspaceBoardHasTasks}
+        <form
+            action={getWorkspaceBoardSearchUrl(workspaceBoard)}
+            class="flex w-full max-w-md flex-col gap-2 rounded-xl bg-foreground px-4 py-4"
+        >
+            <!-- XXX definitely not ideal, placeholder will disappear after input -->
+            <InputField
+                style={{ inputType: "text" }}
+                label={$_("dashboard.search-task.input.label")}
+                placeholder={$_("dashboard.search-task.input.placeholder")}
+                name="search"
+                bind:value={searchInput}
+                showClearButton={false}
+                required
+            />
+            <Button
+                label={$_("dashboard.search-task.button")}
+                action={{ kind: "submit", disabled: !canSearch }}
+                style={{
+                    kind: "tertiary",
+                    icon: { position: "left", icon: Search },
+                }}
+                size="small"
+                color="blue"
+                grow={false}
+            />
+        </form>
+    {/if}
     <!-- shared layout for workspace board and search results -->
     <!-- XXX: setting overflow-x-auto here magically solves an overflowing task card
     Why? Justus 2023-08-28 -->
