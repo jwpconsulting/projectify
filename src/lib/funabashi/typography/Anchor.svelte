8ab283a1561236bffa52af6e6a7e2a4ab5177e04
Justus Perlwitz <hello@justus.pw>
2024-02-05 17:35:41 +0900

    Allow opening Anchor links on new tab

--- src/lib/funabashi/typography/Anchor.svelte
+++ src/lib/funabashi/typography/Anchor.svelte
@@ -1,6 +1,6 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 <!--
-    Copyright (C) 2023 JWP Consulting GK
+    Copyright (C) 2023-2024 JWP Consulting GK
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -16,11 +16,17 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script lang="ts">
+    // This adds coupling to svelte-i18n
+    import { ExternalLink } from "@steeze-ui/heroicons";
+    import { Icon } from "@steeze-ui/svelte-icon";
+    import { _ } from "svelte-i18n";
+
     import type { AnchorSize } from "$lib/funabashi/types";
 
     export let href: string;
     export let label: string;
     export let size: AnchorSize = "normal";
+    export let openBlank = false;
 
     let anchorClass: string;
     $: anchorClass = {
@@ -34,5 +40,12 @@
 <a
     {href}
     class={`font-medium text-primary underline hover:text-primary-hover active:text-primary-pressed ${anchorClass}`}
-    >{label}</a
+    target={openBlank ? "_blank" : "_self"}
+>
+    {label}{#if openBlank}
+        <span class="sr-only">{$_("typography.anchor.new-tab")} </span><Icon
+            src={ExternalLink}
+            class="inline-block h-4 w-4 px-px"
+            theme="outline"
+        />{/if}</a
 >
--- src/stories/funabashi/typography/Anchor.stories.ts
+++ src/stories/funabashi/typography/Anchor.stories.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 /*
- *  Copyright (C) 2023 JWP Consulting GK
+ *  Copyright (C) 2023-2024 JWP Consulting GK
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -27,6 +27,7 @@ const meta: Meta<Anchor> = {
         label: "This is a label",
         href: "#",
         size: "normal",
+        openBlank: false,
     },
     argTypes: {
         size: makeStorybookSelect({
@@ -36,6 +37,7 @@ const meta: Meta<Anchor> = {
         } satisfies Record<string, AnchorSize>),
         href: { control: "text" },
         label: { control: "text" },
+        openBlan: { control: "boolean" },
     },
 };
 export default meta;
@@ -43,3 +45,8 @@ export default meta;
 type Story = StoryObj<Anchor>;
 
 export const Default: Story = {};
+export const OpenBlank: Story = {
+    args: {
+        openBlank: true,
+    },
+};
