c519e47277b4329e733796ab52fc3fe1628e57be
Justus Perlwitz <hello@justus.pw>
2024-02-19 15:15:04 +0900

    FE: Annotate TODO

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -32,6 +32,11 @@ const config = {
         adapter: adapter({
             pages: "build",
             assets: "build",
+            /* XXX we get this error:
+             * > Using @sveltejs/adapter-static
+             * Overwriting build/redirect.html with fallback page. Consider
+             * using a different name for the fallback.
+             */
             fallback: "redirect.html",
             precompress: false,
             strict: true,
