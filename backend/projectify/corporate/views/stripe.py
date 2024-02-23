5bd3248c466283d60da2c297bd34002554afaf81
Justus Perlwitz <hello@justus.pw>
2024-02-23 16:39:07 +0900

    BE: Refactor stripe webhook to be DRF view fn

--- backend/projectify/corporate/views/stripe.py
+++ backend/projectify/corporate/views/stripe.py
@@ -26,6 +26,11 @@ from django.views.decorators.csrf import (
 
 import stripe
 from rest_framework import serializers
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import (
@@ -167,9 +172,9 @@ def _handle_event(event: stripe.Event) -> Response:
     return Response(status=HTTP_200_OK)
 
 
-# TODO Refactor this as a DRF view function so that we can get nicer
-# errors
 @csrf_exempt
+@api_view(["POST"])
+@permission_classes([AllowAny])
 def stripe_webhook(request: Request) -> Response:
     """Construct event type using data coming from stripe."""
     payload = request.body
