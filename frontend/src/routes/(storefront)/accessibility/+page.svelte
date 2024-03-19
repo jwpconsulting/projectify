869ad1a8375b6fea8bc1405083f0e9edbb9e1f53
Justus Perlwitz <hello@justus.pw>
2024-03-19 14:19:35 +0100

    FE: Use markdown for accessibility page

--- frontend/src/routes/(storefront)/accessibility/+page.svelte
+++ frontend/src/routes/(storefront)/accessibility/+page.svelte
@@ -1,6 +1,6 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 <!--
-    Copyright (C) 2023 JWP Consulting GK
+    Copyright (C) 2023-2024 JWP Consulting GK
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -16,11 +16,12 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script lang="ts">
-    import { _, json } from "svelte-i18n";
+    import { _ } from "svelte-i18n";
 
     import Hero from "$lib/components/layouts/Hero.svelte";
     import HeroLayout from "$lib/components/layouts/HeroLayout.svelte";
     import type { SolutionsHeroContent } from "$lib/types/ui";
+    import { toMarkdown } from "$lib/utils/markdown";
 
     import HeroAccessibility from "./hero-accessibility.png";
 
@@ -32,45 +33,7 @@
         },
     } satisfies SolutionsHeroContent;
 
-    $: sections = [
-        {
-            title: $_("accessibility.goals.title"),
-            text: $_("accessibility.goals.text"),
-        },
-        {
-            title: $_("accessibility.measures.title"),
-            list: $json("accessibility.measures.list") as string[],
-        },
-        {
-            title: $_("accessibility.conformance.title"),
-            text: $_("accessibility.conformance.text"),
-        },
-        {
-            title: $_("accessibility.compatibility.title"),
-            text: $_("accessibility.compatibility.text"),
-        },
-        {
-            title: $_("accessibility.platform.title"),
-            text: $_("accessibility.platform.text"),
-        },
-        {
-            title: $_("accessibility.website.title"),
-            list: $json("accessibility.website.list") as string[],
-        },
-        {
-            title: $_("accessibility.limitations.title"),
-            text: $_("accessibility.limitations.text"),
-            list: $json("accessibility.limitations.list") as string[],
-        },
-        {
-            title: $_("accessibility.evaluation.title"),
-            text: $_("accessibility.evaluation.text"),
-        },
-        {
-            title: $_("accessibility.contact.title"),
-            text: $_("accessibility.contact.text"),
-        },
-    ];
+    $: markdown = toMarkdown($_("accessibility.content"));
 </script>
 
 <svelte:head>
@@ -80,27 +43,6 @@
 <HeroLayout>
     <Hero slot="hero" {heroContent} />
 
-    <div slot="content" class="flex flex-col gap-4">
-        {#each sections as section}
-            <section class="flex flex-col gap-4">
-                <h2 class="pt-4 text-3xl font-bold">
-                    {section.title}
-                </h2>
-                {#if section.text}
-                    <p>
-                        {section.text}
-                    </p>
-                {/if}
-                {#if section.list}
-                    <ul>
-                        {#each section.list as item}
-                            <li>
-                                {item}
-                            </li>
-                        {/each}
-                    </ul>
-                {/if}
-            </section>
-        {/each}
-    </div>
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    <main class="prose" slot="content">{@html markdown.content}</main>
 </HeroLayout>
