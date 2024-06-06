e9a2be2bcf50b369fe2af41bfa6309807cb3227a
Justus Perlwitz <hello@justus.pw>
2024-06-06 09:50:05 +0900

    BE: error_serializer -> error_schema

--- backend/projectify/settings/spectacular.py
+++ backend/projectify/settings/spectacular.py
@@ -32,7 +32,7 @@ class SpectacularSettings:
         "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
         "REDOC_DIST": "SIDECAR",
         "PREPROCESSING_HOOKS": (
-            "projectify.lib.error_serializer.preprocess_derive_error_schemas",
+            "projectify.lib.error_schema.preprocess_derive_error_schemas",
         ),
     }
 
