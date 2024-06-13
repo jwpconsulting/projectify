ff3aea54065aaf3dd648dc1a77b3347f1819a7a2
Justus Perlwitz <hello@justus.pw>
2024-06-13 11:45:23 +0900

    FE: Move dev warning header to root layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -16,9 +16,12 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script lang="ts">
+    import { _ } from "svelte-i18n";
     import "$lib/app.scss";
     import Footer from "$lib/figma/navigation/Footer.svelte";
     import faviconHtml from "favicon-html";
+
+    const isDevelopment = __MODE__ === "development";
 </script>
 
 <svelte:head>
@@ -30,6 +33,14 @@
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html faviconHtml}
 </svelte:head>
+
+{#if isDevelopment}
+    <p
+        class="absolute left-0 top-0 w-full bg-destructive/30 text-center font-bold"
+    >
+        {$_("navigation.header.development-preview")}
+    </p>
+{/if}
 
 <slot />
 <Footer />
