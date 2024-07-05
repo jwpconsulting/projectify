2881158783abdc9ba3db51bf1ba79221a416dff8
Justus Perlwitz <hello@justus.pw>
2024-07-05 13:31:01 +0900

    FE: Simplify flake file

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -15,14 +15,24 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 {
+  description = "Projectify frontend";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "Projectify frontend";
-      shell = ./shell.nix;
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        inherit self nixpkgs;
+        packages = {
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+          ];
+        };
+      });
 }
