af609d7f8eb68bd40261945f2822bbd25b113b1a
Justus Perlwitz <hello@justus.pw>
2024-07-11 10:30:34 +0900

    Revproxy: Configure new django static path

--- revproxy/flake.nix
+++ revproxy/flake.nix
@@ -12,28 +12,9 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         # https://ryantm.github.io/nixpkgs/builders/trivial-builders/#chap-trivial-builders
-        caddyfile = pkgs.writeText "Caddyfile" ''
-            :{$PORT:invalid} {
-              handle /admin/* {
-                reverse_proxy {$BACKEND_ADDRESS}
-              }
-              handle /backend/static/* {
-                reverse_proxy {$BACKEND_ADDRESS}
-              }
-              handle /ws/* {
-                reverse_proxy {$BACKEND_ADDRESS}
-              }
-              handle_path /api/* {
-                reverse_proxy {$BACKEND_ADDRESS}
-              }
-              handle /* {
-                reverse_proxy {$FRONTEND_ADDRESS}
-              }
-            }
-        '';
         caddyfileFormatted = pkgs.runCommand "Caddyfile" {} ''
           mkdir $out
-          ${pkgs.caddy}/bin/caddy fmt - <${caddyfile} > $out/Caddyfile
+          ${pkgs.caddy}/bin/caddy fmt - <${./Caddyfile} > $out/Caddyfile
         '';
         projectify-revproxy = pkgs.writeShellApplication {
           name = "projectify-revproxy";
