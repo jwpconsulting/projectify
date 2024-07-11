15345f6569dbf619e37092a99063e8b716a0601b
Justus Perlwitz <hello@justus.pw>
2024-07-11 09:59:34 +0900

    BE: Change static url to /backend/django/

--- backend/projectify/settings/development_nix.py
+++ backend/projectify/settings/development_nix.py
@@ -42,7 +42,6 @@ class DevelopmentNix(Development):
 
     # We need to inject the static root path during the nix build process
     STATIC_ROOT = Path(os.environ["STATIC_ROOT"])
-    STATIC_URL = "/backend/static/"
 
     # No need for debug or debug libs, for added realism
     DEBUG = False
