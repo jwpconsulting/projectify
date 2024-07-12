f69d1846d3df775a8fcd234482cd65b5079a3dd7
Justus Perlwitz <hello@justus.pw>
2024-07-12 09:02:45 +0900

    BE: Make python-dotenv a dev dependency
    Warn or throw error when starting up and trying to import python-dotenv
    in production environment, where there is no python-dotenv

--- backend/projectify/settings/development.py
+++ backend/projectify/settings/development.py
@@ -24,9 +24,12 @@ from collections.abc import (
 )
 from typing import Optional
 
-from dotenv import (
-    load_dotenv,
-)
+try:
+    from dotenv import load_dotenv
+except ImportError as e:
+    raise RuntimeError(
+        "dotenv was not found. Please check if dev dependencies have been installed"
+    ) from e
 
 from .base import Base
 from .spectacular import SpectacularSettings
