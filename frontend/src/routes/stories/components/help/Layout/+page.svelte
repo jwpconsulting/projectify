82ff23ebab0ef76a7c9ae076c86e2873d76ee75f
Justus Perlwitz <hello@justus.pw>
2024-10-05 16:40:03 +0900

    FE: Turn Layout.stories.ts into +page.svelte

---
+++ frontend/src/routes/stories/components/help/Layout/+page.svelte
@@ -0,0 +1,41 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<!--
+    Copyright (C) 2024 JWP Consulting GK
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+<script lang="ts">
+    import Layout from "$lib/components/help/Layout.svelte";
+    import type { SolutionsHeroContent } from "$lib/types/ui";
+
+    const heroContent: SolutionsHeroContent = {
+        title: "Sample Help Page",
+    };
+
+    const content = `
+# Sample Help Content
+
+This is a sample help document.
+
+## Section 1
+
+This is the content of section 1.
+
+## Section 2
+
+This is the content of section 2.
+`;
+</script>
+
+<Layout {heroContent} {content} />
