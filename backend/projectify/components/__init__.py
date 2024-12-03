56ed27e557b68a2144465f2dc9e481bfc83c2408
Justus Perlwitz <hello@justus.pw>
2024-12-03 17:39:33 +0900

    BE: Fix components file

--- backend/projectify/components/__init__.py
+++ backend/projectify/components/__init__.py
@@ -1,33 +1,8 @@
-"""Components."""
-
-from typing import Any
-
-from django_components import Component, register
-
+# SPDX-FileCopyrightText: 2024 JWP Consulting GK
+# SPDX-License-Identifier: AGPL-3.0-or-later
 """
-export type ButtonAction =
-    | { kind: "a"; href: string; onInteract?: () => void }
-    | { kind: "button"; form?: string; action: () => void; disabled?: boolean }
-    // For the case that a disabled button is disabled because no callback is
-    // present
-    | { kind: "button"; form?: string; action?: undefined; disabled: true }
-    | { kind: "submit"; form?: string; disabled?: boolean };
+Django Components.
 
-// For buttons/Button.svelte
-export type ButtonStyle =
-    | { kind: "primary" }
-    | { kind: "secondary" }
-    // TODO make this icon? instead Justus 2023-08-28
-    | {
-          kind: "tertiary";
-          icon?: { position: "right" | "left"; icon: IconSource };
-      };
-export type ButtonColor = "blue" | "red";
+See
+https://github.com/EmilStenstrom/django-components
 """
-
-
-@register("pbutton")
-class PButton(Component[Any, Any, Any, Any, Any, Any]):
-    """Projectify Button Component."""
-
-    template_name = "pbutton.html"
