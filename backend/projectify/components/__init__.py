a11fe19b08d3215c5725a0a5727bbb044f6729ba
Justus Perlwitz <hello@justus.pw>
2024-12-12 12:06:31 +0900

    BE: Remove django-components

--- backend/projectify/components/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2024 JWP Consulting GK
-# SPDX-License-Identifier: AGPL-3.0-or-later
-"""
-Django Components.
-
-See
-https://github.com/EmilStenstrom/django-components
-"""
