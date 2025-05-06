d0670a764ecbbba8324fd7b5140a277cae19af43
sakiadachi <sakiadachi.88@gmail.com>
2025-05-06 18:16:15 +0900

    BE: Add copyright and licencing comments

--- backend/projectify/storefront/urls.py
+++ backend/projectify/storefront/urls.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# SPDX-FileCopyrightText: 2025 JWP Consulting GK
 from django.urls import path
 
 from projectify.storefront.views import accessibility
