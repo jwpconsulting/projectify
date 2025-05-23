bf424b315ecd5a2f248ef164179dee871538d201
Justus Perlwitz <hello@justus.pw>
2025-05-23 18:29:11 +0900

    BE: User views WIP

--- backend/projectify/user/urls.py
+++ backend/projectify/user/urls.py
@@ -13,6 +13,11 @@ from projectify.user.views.auth import (
     PasswordResetConfirm,
     PasswordResetRequest,
     SignUp,
+    confirm_password_reset,
+    log_in,
+    log_out,
+    request_password_reset,
+    sign_up,
 )
 from projectify.user.views.user import (
     ChangePassword,
@@ -21,9 +26,38 @@ from projectify.user.views.user import (
     RequestEmailAddressUpdate,
     UserRead,
     UserUpdate,
+    email_address_confirm,
+    email_address_update,
+    password_change,
+    user_profile,
 )
 
 app_name = "user"
+
+user_patterns_django = (
+    path("profile/", user_profile, name="profile"),
+    path("profile/change-password", password_change, name="change-password"),
+    path(
+        "profile/update-email-address/",
+        email_address_update,
+        name="update-email-address",
+    ),
+    path(
+        "profile/update-email-address/confirm/<str:token>",
+        email_address_confirm,
+        name="confirm-email-address",
+    ),
+    # auth views
+    path("log-in", log_in, name="log-in"),
+    path(
+        "request-password-reset",
+        request_password_reset,
+        name="request-password-reset",
+    ),
+    path("sign-up", sign_up, name="sign-up"),
+    path("log-out", log_out, name="log-out"),
+    path("confirm-password-reset", confirm_password_reset, name="log-out"),
+)
 
 user_patterns = (
     # Create
@@ -104,6 +138,7 @@ auth_patterns = (
 )
 
 urlpatterns = (
+    path("", include((user_patterns_django, "users-django"))),
     path("user/", include((user_patterns, "users"))),
     path("user/", include((auth_patterns, "auth"))),
 )
