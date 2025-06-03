0d2a1d08c538265726d61426b4a24d2d7afecad8
Justus Perlwitz <hello@justus.pw>
2025-06-03 15:53:01 +0900

    BE: Add form error helper method

--- backend/projectify/user/views/auth.py
+++ backend/projectify/user/views/auth.py
@@ -23,6 +23,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_204_NO_CONTENT
 
 from projectify.lib.error_schema import DeriveSchema
+from projectify.lib.forms import populate_form_with_drf_errors
 from projectify.lib.schema import extend_schema
 from projectify.user.serializers import (
     AnonymousUserSerializer,
