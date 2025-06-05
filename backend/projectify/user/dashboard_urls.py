af68a58e4f8719dbfe7e80b661ce633550a586fa
Justus Perlwitz <hello@justus.pw>
2025-06-05 11:09:03 +0900

    BE: Implement email_confirm Django view
    Add test code so that you can manually test this view as well.

--- backend/projectify/user/dashboard_urls.py
+++ backend/projectify/user/dashboard_urls.py
@@ -6,15 +6,16 @@
 from django.urls import path
 
 from projectify.user.views.auth import (
-    confirm_password_reset,
+    email_confirm,
     log_in,
     log_out,
-    request_password_reset,
+    password_reset_confirm,
+    password_reset_request,
     sign_up,
 )
 from projectify.user.views.user import (
-    email_address_confirm,
     email_address_update,
+    email_address_update_confirm,
     password_change,
     user_profile,
 )
@@ -31,21 +32,26 @@ urlpatterns = (
     ),
     path(
         "profile/update-email-address/confirm/<str:token>",
-        email_address_confirm,
-        name="confirm-email-address",
+        email_address_update_confirm,
+        name="confirm-email-address-update",
     ),
     # auth views
     path("log-in", log_in, name="log-in"),
     path(
         "request-password-reset",
-        request_password_reset,
+        password_reset_request,
         name="request-password-reset",
     ),
     path("sign-up", sign_up, name="sign-up"),
+    path(
+        "confirm-email/<str:email>/<str:token>",
+        email_confirm,
+        name="confirm-email",
+    ),
     path("log-out", log_out, name="log-out"),
     path(
         "confirm-password-reset",
-        confirm_password_reset,
+        password_reset_confirm,
         name="confirm-password-reset",
     ),
 )
