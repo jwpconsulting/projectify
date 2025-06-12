2ea756c762e3549e8aab143512a4a234316b593c
Justus Perlwitz <hello@justus.pw>
2025-06-12 15:09:22 +0900

    BE: Add email address update views (WIP)

--- backend/projectify/user/testing_urls.py
+++ backend/projectify/user/testing_urls.py
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
-# SPDX-FileCopyrightText: 2024 JWP Consulting GK
+# SPDX-FileCopyrightText: 2025 JWP Consulting GK
 """Test URL patterns for email confirmation testing."""
 
 from django.urls import path
 
 from projectify.lib.settings import get_settings
-from projectify.user.views.view_tests import email_confirm_test
+from projectify.user.views.view_tests import (
+    email_confirm_test,
+    email_update_confirm_test,
+)
 
 settings = get_settings()
 assert settings.DEBUG_AUTH, "Can't import this if DEBUG_AUTH isn't set"
@@ -14,4 +17,5 @@ assert settings.DEBUG_AUTH, "Can't import this if DEBUG_AUTH isn't set"
 
 urlpatterns = [
     path("email-confirm-test/", email_confirm_test),
+    path("email-update-confirm-test/", email_update_confirm_test),
 ]
