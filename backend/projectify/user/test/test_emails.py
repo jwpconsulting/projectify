0fefa517af44c58600ca68282057a1e58c48b522
Justus Perlwitz <hello@justus.pw>
2025-06-13 14:08:24 +0900

    BE: Test user email confirmation

--- backend/projectify/user/test/test_emails.py
+++ backend/projectify/user/test/test_emails.py
@@ -6,12 +6,17 @@
 import re
 
 from django.core.mail import EmailMessage
+from django.test.client import Client
 
 import pytest
 
 from projectify.user.services.internal import Token, user_check_token
 
-from ..emails import UserEmailConfirmationEmail, UserPasswordResetEmail
+from ..emails import (
+    UserEmailAddressUpdateEmail,
+    UserEmailConfirmationEmail,
+    UserPasswordResetEmail,
+)
 from ..models import User
 
 
@@ -50,3 +55,30 @@ class TestUserPasswordResetEmail:
         assert match
         token = Token(match.group(1))
         assert user_check_token(token=token, user=user, kind="reset_password")
+
+
+@pytest.mark.django_db
+class TestUserEmailAddressUpdateEmail:
+    """Test UserEmailAddressUpdateEmail."""
+
+    def test_send(
+        self, user: User, user_client: Client, mailoutbox: list[EmailMessage]
+    ) -> None:
+        """Test send."""
+        new_email = "new-email@example.com"
+        user.unconfirmed_email = new_email
+        user.save()
+        UserEmailAddressUpdateEmail(receiver=user, obj=user).send()
+        assert len(mailoutbox) == 1, mailoutbox
+
+        m = mailoutbox[0]
+        assert m.to == [new_email]
+        assert new_email in m.body
+        match = re.search(
+            r"(/user/profile/update-email-address/confirm/.+)\n", m.body
+        )
+        assert match, m.body
+        response = user_client.get(match.group(1))
+        assert response.status_code == 200, response
+        user.refresh_from_db()
+        assert user.email == new_email
