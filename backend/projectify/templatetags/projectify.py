a5f00198ce827fd54977d2e24445cf5fcc0013f5
Justus Perlwitz <hello@justus.pw>
2025-06-26 15:10:35 +0900

    BE: Add anchor template tag

--- backend/projectify/templatetags/projectify.py
+++ backend/projectify/templatetags/projectify.py
@@ -6,6 +6,8 @@
 from typing import Optional
 
 from django import template
+from django.utils.html import format_html
+from django.utils.safestring import SafeText
 
 register = template.Library()
 
@@ -16,3 +18,17 @@ def percent(value: Optional[float]) -> Optional[str]:
     if not value:
         return None
     return f"{value:3.0%}"
+
+
+@register.simple_tag
+def anchor(href: str, label: str) -> SafeText:
+    """
+    Render a fully styled HTML anchor.
+
+    Expects label to be translated.
+    """
+    return format_html(
+        '<a href="{href}" class="text-primary underline hover:text-primary-hover active:text-primary-pressed text-base">{label}</a>',
+        href=href,
+        label=label,
+    )
