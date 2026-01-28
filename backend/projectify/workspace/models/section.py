46e1aca40daf80b74b68711904f40c4a8e9100ff
Justus Perlwitz <hello@justus.pw>
2026-01-28 18:29:37 +0900

    BE: Remove SectionQuerySet

--- backend/projectify/workspace/models/section.py
+++ backend/projectify/workspace/models/section.py
@@ -4,16 +4,15 @@
 """Section model."""
 
 import uuid
-from typing import TYPE_CHECKING, Callable, ClassVar, Self, cast
+from typing import TYPE_CHECKING, Callable
 
-from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from django.urls import reverse
 
 from projectify.lib.models import BaseModel, TitleDescriptionModel
 
 from .task import Task
-from .types import GetOrder, Pks, SetOrder
+from .types import GetOrder, SetOrder
 
 if TYPE_CHECKING:
     from django.db.models.manager import RelatedManager  # noqa: F401
@@ -21,29 +20,12 @@ if TYPE_CHECKING:
     from . import Project  # noqa: F401
 
 
-class SectionQuerySet(models.QuerySet["Section"]):
-    """QuerySet for Project."""
-
-    def filter_by_project_pks(self, keys: Pks) -> Self:
-        """Filter by projects."""
-        return self.filter(project__pk__in=keys)
-
-    def filter_for_user_and_uuid(
-        self, user: AbstractBaseUser, uuid: uuid.UUID
-    ) -> Self:
-        """Return a workspace for user and uuid."""
-        return self.filter(project__workspace__users=user, uuid=uuid)
-
 
 class Section(TitleDescriptionModel, BaseModel):
     """Section of a Project."""
 
     project = models.ForeignKey["Project"]("Project", on_delete=models.CASCADE)
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    objects: ClassVar[SectionQuerySet] = cast(  # type: ignore[assignment]
-        SectionQuerySet,
-        SectionQuerySet.as_manager(),
-    )
 
     if TYPE_CHECKING:
         # Related managers
