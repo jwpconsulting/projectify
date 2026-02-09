fe2dce11a5b945879e4786c5164ca94a93e03360
Justus Perlwitz <hello@justus.pw>
2026-02-09 18:47:20 +0900

    BE: Redirect from dashboard to project/workspace

--- backend/projectify/workspace/views/dashboard.py
+++ backend/projectify/workspace/views/dashboard.py
@@ -11,11 +11,33 @@ from projectify.lib.types import AuthenticatedHttpRequest
 from projectify.workspace.selectors.project import (
     project_find_by_workspace_uuid,
 )
+from projectify.workspace.selectors.team_member import (
+    team_member_last_project,
+    team_member_last_workspace,
+)
 from projectify.workspace.selectors.workspace import workspace_find_for_user
 
 
 def redirect_to_dashboard(request: AuthenticatedHttpRequest) -> HttpResponse:
     """Redirect to first available workspace or workspace list."""
+    # TODO it would be fun if I could refactor this into a single call
+    # that returns either a workspace or project
+    # but still, if we're going to a workspace, we should remember what
+    # project we looked at last
+    maybe_last_visited_workspace = team_member_last_workspace(
+        user=request.user
+    )
+    if maybe_last_visited_workspace:
+        maybe_project = team_member_last_project(
+            user=request.user, workspace=maybe_last_visited_workspace
+        )
+        if maybe_project:
+            return redirect(maybe_project.get_absolute_url())
+        return redirect(
+            "dashboard:workspaces:detail",
+            args=(maybe_last_visited_workspace.uuid,),
+        )
+
     maybe_workspace = workspace_find_for_user(who=request.user).first()
     if not maybe_workspace:
         return redirect(reverse("onboarding:welcome"))
@@ -24,12 +46,7 @@ def redirect_to_dashboard(request: AuthenticatedHttpRequest) -> HttpResponse:
         workspace_uuid=maybe_workspace.uuid, who=request.user, archived=False
     ).first()
     if maybe_project:
-        return redirect(
-            reverse(
-                "dashboard:projects:detail",
-                kwargs={"project_uuid": maybe_project.uuid},
-            )
-        )
+        return redirect(maybe_project.get_absolute_url())
 
     return redirect(
         reverse(
