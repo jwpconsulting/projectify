fa6513e9cdf45341c9a9de7a1ebbc969fc9a1bc5
Justus Perlwitz <hello@justus.pw>
2026-02-23 21:50:39 +0900

    FE: Remove routes

--- frontend/src/routes/(storefront)/ethicalads/+page.svelte
+++
@@ -1,7 +0,0 @@
-<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
-<!-- SPDX-FileCopyrightText: 2024 JWP Consulting GK -->
-<script lang="ts">
-    import Storefront from "../+page.svelte";
-</script>
-
-<Storefront />
