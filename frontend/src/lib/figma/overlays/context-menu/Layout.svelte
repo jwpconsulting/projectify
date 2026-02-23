a56d0a118925e9f81cefa8e4b247578972c9d0d8
Justus Perlwitz <hello@justus.pw>
2026-02-23 21:03:37 +0900

    FE: Remove figma components

--- frontend/src/lib/figma/overlays/context-menu/Layout.svelte
+++
@@ -1,7 +0,0 @@
-<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
-<!-- SPDX-FileCopyrightText: 2023 JWP Consulting GK -->
-<div
-    class="flex min-w-max max-w-xs flex-col rounded-lg border border-border bg-foreground py-2 shadow-context-menu"
->
-    <slot />
-</div>
--- frontend/src/lib/figma/screens/task/Layout.svelte
+++
@@ -1,9 +0,0 @@
-<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
-<!-- SPDX-FileCopyrightText: 2023 JWP Consulting GK -->
-<div class="flex h-full flex-col p-4 pt-0">
-    <slot name="top-bar" />
-    <div class="flex flex-col gap-8">
-        <slot name="tab-bar-mobile" />
-        <slot name="content" />
-    </div>
-</div>
