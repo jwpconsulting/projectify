f1e3207a63793cceb3558a86758f8e219f9d41f3
Justus Perlwitz <hello@justus.pw>
2026-02-24 10:05:15 +0900

    BE: Remove auth DRF views

--- backend/projectify/user/urls.py
+++ backend/projectify/user/urls.py
@@ -5,15 +5,6 @@
 
 from django.urls import include, path
 
-from projectify.user.views.auth import (
-    ConfirmEmail,
-    LogIn,
-    LogOut,
-    PasswordPolicyRead,
-    PasswordResetConfirm,
-    PasswordResetRequest,
-    SignUp,
-)
 from projectify.user.views.user import (
     ChangePassword,
     ConfirmEmailAddressUpdate,
@@ -64,47 +55,4 @@ user_patterns = (
     ),
 )
 
-auth_patterns = (
-    # Auth
-    path(
-        "log-out",
-        LogOut.as_view(),
-        name="log-out",
-    ),
-    # The following urls do not require being authenticated
-    path(
-        "sign-up",
-        SignUp.as_view(),
-        name="sign-up",
-    ),
-    path(
-        "confirm-email",
-        ConfirmEmail.as_view(),
-        name="confirm-email",
-    ),
-    path(
-        "log-in",
-        LogIn.as_view(),
-        name="log-in",
-    ),
-    path(
-        "request-password-reset",
-        PasswordResetRequest.as_view(),
-        name="request-password-reset",
-    ),
-    path(
-        "confirm-password-reset",
-        PasswordResetConfirm.as_view(),
-        name="confirm-password-reset",
-    ),
-    path(
-        "password-policy",
-        PasswordPolicyRead.as_view(),
-        name="password-policy",
-    ),
-)
-
-urlpatterns = (
-    path("user/", include((user_patterns, "users"))),
-    path("user/", include((auth_patterns, "auth"))),
-)
+urlpatterns = (path("user/", include((user_patterns, "users"))),)
