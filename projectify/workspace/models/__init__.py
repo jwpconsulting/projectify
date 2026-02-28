53e1ca2060fa993feb4fa2fe06adf91f1c5ce757
Justus Perlwitz <hello@justus.pw>
2026-02-28 23:45:20 +0900

    Simplify project model

--- projectify/workspace/models/__init__.py
+++ projectify/workspace/models/__init__.py
@@ -19,7 +19,6 @@ from projectify.user.models import UserInvite
 
 from .const import TeamMemberRoles
 from .label import Label
-from .project import Project
 from .types import GetOrder, SetOrder
 from .workspace import Workspace
 
@@ -31,6 +30,49 @@ if TYPE_CHECKING:
 
 
 logger = logging.getLogger(__name__)
+
+
+class Project(TitleDescriptionModel, BaseModel):
+    """Project."""
+
+    workspace = models.ForeignKey["Workspace"](
+        Workspace,
+        on_delete=models.PROTECT,
+    )
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    archived = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text=_("Archival timestamp of this workspace board."),
+    )
+    due_date = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text=_("Due date for this workspace board"),
+    )
+
+    if TYPE_CHECKING:
+        # Related managers
+        section_set: RelatedManager["Section"]
+
+        # For ordering
+        get_section_order: GetOrder
+        set_section_order: SetOrder
+
+    def __str__(self) -> str:
+        """Return title."""
+        return self.title
+
+    def get_absolute_url(self) -> str:
+        """Return the absolute URL for this project."""
+        return reverse(
+            "dashboard:projects:detail", kwargs={"project_uuid": self.uuid}
+        )
+
+    class Meta:
+        """Order by created, descending."""
+
+        ordering = ("-created",)
 
 
 class Section(TitleDescriptionModel, BaseModel):
