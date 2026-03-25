5eca69cc8a2456cb93d671e4b004b612f113f0e1
Justus Perlwitz <hello@justus.pw>
2026-03-25 12:17:41 +0900

    Import static only in developmen

--- projectify/urls.py
+++ projectify/urls.py
@@ -15,7 +15,6 @@ This module also contains a 500 exception handler.
 from collections.abc import Sequence
 from typing import Union
 
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import URLPattern, URLResolver, include, path, reverse_lazy
@@ -110,6 +109,8 @@ if settings.PREMAIL_PREVIEW:
     )
 
 if settings.SERVE_MEDIA:
+    from django.conf.urls.static import static
+
     urlpatterns = (
         *urlpatterns,
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
