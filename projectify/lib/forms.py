5aa573a85140a8ed2a8884f51283a4200e8084a7
Justus Perlwitz <hello@justus.pw>
2026-03-27 21:55:26 +0900

    Remove DRF stubs

--- projectify/lib/forms.py
+++ projectify/lib/forms.py
@@ -1,83 +1,26 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
-# SPDX-FileCopyrightText: 2025 JWP Consulting GK
+# SPDX-FileCopyrightText: 2025-2026 JWP Consulting GK
 
 """Form utilities."""
 
 import logging
-from typing import Union
 
-from django.core.exceptions import ValidationError as DjangoValidationError
+from django.core.exceptions import ValidationError
 from django.forms import BaseForm
-
-from rest_framework.exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
 
 
-def populate_form_with_django_errors(
-    form: BaseForm, error: DjangoValidationError
+def populate_form_with_drf_errors(
+    form: BaseForm, error: ValidationError
 ) -> None:
-    """Populate a django form with errors from a Django ValidationError."""
-    if error.error_list:
+    """Populate a Django form with errors from a Django ValidationError."""
+    if hasattr(error, "error_list") and error.error_list:
         for error in error.error_list:
             form.add_error(field=None, error=error)
-        return
-    match error.error_dict:
-        case None:
-            return
-        case error_dict:
-            pass
-    for k, errors in error_dict.items():
-        for error in errors:
-            form.add_error(k, error)
-
-
-def populate_form_with_drf_errors(
-    form: BaseForm, error: Union[ValidationError, DjangoValidationError]
-) -> None:
-    """Populate a Django form with errors from a DRF ValidationError."""
-    match error:
-        case ValidationError():
-            pass
-        case DjangoValidationError():
-            return populate_form_with_django_errors(form, error)
-    match error.detail:
-        case dict():
-            pass
-        case _:
-            logger.warning(
-                "Can't populate form %s with errors of type %s",
-                repr(form),
-                type(error.detail),
-            )
-            return
-    for field, error_message in error.detail.items():
-        # Add a non-field error to '__all__' by setting the field to None
-        if field not in form.fields:
-            field = None
-        match error_message:
-            case str():
-                form.add_error(field, error_message)
-            case list() as error_list:
-                for e in error_list:
-                    match e:
-                        case str():
-                            form.add_error(field, e)
-                        case _:
-                            logger.warning(
-                                "When adding errors from a list to field %s "
-                                "in form %s, found type %s "
-                                "that I don't know how to handle",
-                                field,
-                                repr(form),
-                                type(e),
-                            )
-            case _:
-                logger.warning(
-                    "When adding errors to field %s in form %s, found type %s "
-                    "that I don't know how to handle",
-                    field,
-                    repr(form),
-                    type(error_message),
-                )
+    if hasattr(error, "error_dict") and error.error_dict:
+        for k, errors in error.error_dict.items():
+            for error in errors:
+                form.add_error(k, error)
+    return
