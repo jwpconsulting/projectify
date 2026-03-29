fca866f03909052e59457f04213483c139bc1aa2
Justus Perlwitz <hello@justus.pw>
2026-03-29 09:49:40 +0900

    Run collectstatic in projectify-demo

--- projectify/demo.py
+++ projectify/demo.py
@@ -13,10 +13,10 @@ from pathlib import Path
 from typing import Any, Callable, Optional, Union
 
 # https://gunicorn.org/custom/
-import gunicorn.app.base
+from gunicorn.app.base import BaseApplication  # type: ignore
 
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
+class StandaloneApplication(BaseApplication):  # type: ignore
     """Standalone WSGI app based on gunicorn example."""
 
     def __init__(
@@ -29,7 +29,7 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         self.application = app
         super().__init__()
 
-    def load_config(self):
+    def load_config(self) -> None:
         """Load configuration."""
         config = {
             key: value
@@ -39,40 +39,50 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
-    def load(self):
+    def load(self) -> Any:
         """Return application."""
         return self.application
+
+
+def _main(tmpdir: str) -> None:
+    """Take tmpdir, run Projectify demo."""
+    db_path = Path(tmpdir) / "demo.db"
+    static_path = Path(tmpdir) / "static"
+
+    print(f"Creating temporary database at: {db_path}")
+    print(f"Creating temporary static files directory at: {static_path}")
+
+    env = os.environ
+    env["DATABASE_URL"] = f"sqlite:///{db_path}"
+    env["STATIC_ROOT"] = str(static_path)
+    env["DJANGO_SETTINGS_MODULE"] = "projectify.settings.demo"
+    env["DJANGO_CONFIGURATION"] = "Demo"
+
+    from configurations.management import (  # type: ignore
+        execute_from_command_line,
+    )
+
+    execute_from_command_line([".", "seeddb"])
+    execute_from_command_line([".", "collectstatic", "--noinput"])
+
+    print("Starting Projectify demo server")
+    print("Address: http://localhost:8100")
+    print("Admin login:")
+    print("Username: admin@localhost")
+    print("Password: password")
+    print("=" * 60)
+
+    from projectify.wsgi import application
+
+    StandaloneApplication(
+        application, {"bind": "127.0.0.1:8100", "workers": 1}
+    ).run()
 
 
 def main() -> None:
     """Run Projectify demo with temporary database."""
     with tempfile.TemporaryDirectory() as tmpdir:
-        db_path = Path(tmpdir) / "demo.db"
-
-        print(f"Creating temporary database at: {db_path}")
-
-        env = os.environ
-        env["DATABASE_URL"] = f"sqlite:///{db_path}"
-        env["DJANGO_SETTINGS_MODULE"] = "projectify.settings.demo"
-        env["DJANGO_CONFIGURATION"] = "Demo"
-
-        from configurations.management import execute_from_command_line
-
-        execute_from_command_line([".", "seeddb"])
-
-        print("Starting Projectify demo server")
-        print("Address: http://localhost:8100")
-        print("Admin login:")
-        print("Username: admin@localhost")
-        print("Password: password")
-        print("=" * 60)
-
-        from projectify.wsgi import application
-        options = {
-            "bind": "%s:%s" % ("127.0.0.1", "8100"),
-            "workers": 1,
-        }
-        StandaloneApplication(application, options).run()
+        _main(tmpdir)
 
 
 if __name__ == "__main__":
