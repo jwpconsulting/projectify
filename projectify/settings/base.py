a293049160e3887b543e7adab9cd88c4555f88f6
Justus Perlwitz <hello@justus.pw>
2026-04-05 13:01:40 +0900

    Use BASE_DIR in static root

--- projectify/settings/base.py
+++ projectify/settings/base.py
@@ -306,7 +306,7 @@ class Base(Configuration):  # type:ignore
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
     STATIC_URL = "/static/django/"
-    STATIC_ROOT = Path(BASE_DIR, "staticfiles")
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
