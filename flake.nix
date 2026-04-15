3679bee47fad8dd54f01b7091ba5ca36ca906139
Justus Perlwitz <hello@justus.pw>
2026-04-15 10:15:58 +0900

    Add htmx-js build to flake
    Fix htmx version to 2.0.8
    Add safe-nonce extension

--- flake.nix
+++ flake.nix
@@ -25,6 +25,54 @@
       {
         packages = rec {
           # This is where projectify-bundle once lived
+          # Copy with:
+          htmx-js = pkgs.stdenv.mkDerivation rec {
+            pname = "htmx-js";
+            version = src.rev;
+            src = pkgs.fetchFromGitHub {
+              owner = "bigskysoftware";
+              repo = "htmx";
+              rev = "v2.0.8";
+              sha256 = "sha256-hHBX3UkxRJTULzGsunUFQBYYrAKqmVdAHwj9LoCjTlg=";
+            };
+            installPhase = ''
+              mkdir $out
+              cp src/htmx.js $out/htmx.js
+            '';
+          };
+          htmx-safe-nonce = pkgs.stdenv.mkDerivation rec {
+            pname = "htmx-safe-nonce";
+            version = src.rev;
+            src = pkgs.fetchFromGitHub {
+              owner = "MichaelWest22";
+              repo = "htmx-extensions";
+              rev = "main";
+              sha256 = "sha256-Ae8f/L6i9YuVQUHWnrAnBITZa9STzm3lr9L0c6DY8GY=";
+            };
+            installPhase = ''
+              mkdir $out
+              cp src/safe-nonce/safe-nonce.js $out/safe-nonce.js
+            '';
+          };
+
+          # cp $(nix build .#htmx-js --print-out-paths --no-link)/htmx.js projectify/static/htmx.js
+          htmx-bundle = pkgs.stdenv.mkDerivation {
+            pname = "htmx-bundle";
+            version = htmx-js.version;
+            dontUnpack = true;
+            buildInputs = [ htmx-js htmx-safe-nonce ];
+            installPhase = ''
+              mkdir $out
+              echo "\
+              /*! SPDX-License-Identifier: 0BSD
+                  SPDX-FileCopyrightText: NONE */" > $out/htmx.js
+              cat ${htmx-js}/htmx.js  >> $out/htmx.js
+              echo "\
+              /*! SPDX-License-Identifier: 0BSD
+                  SPDX-FileCopyrightText: 2023, Alexander Petros */" >> $out/htmx.js
+              cat ${htmx-safe-nonce}/safe-nonce.js >> $out/htmx.js
+            '';
+          };
         };
         devShell = pkgs.mkShell {
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ postgres ];
--- projectify/static/htmx.js
+++ projectify/static/htmx.js
@@ -1510,7 +1510,7 @@ var htmx = (function() {
       oobElement.parentNode.removeChild(oobElement)
     } else {
       oobElement.parentNode.removeChild(oobElement)
-      triggerErrorEvent(getDocument().body, 'htmx:oobErrorNoTarget', { content: oobElement, target: selector })
+      triggerErrorEvent(getDocument().body, 'htmx:oobErrorNoTarget', { content: oobElement })
     }
     return oobValue
   }
@@ -3113,7 +3113,7 @@ var htmx = (function() {
       htmx.logger(elt, eventName, detail)
     }
     if (detail.error) {
-      logError(detail.error + (detail.target ? ', ' + detail.target : ''))
+      logError(detail.error)
       triggerEvent(elt, 'htmx:error', { errorInfo: detail })
     }
     let eventResult = elt.dispatchEvent(event)
@@ -5344,3 +5344,43 @@ var htmx = (function() {
  * @property {(xhr: XMLHttpRequest, parameters: FormData, elt: Node) => *|string|null} encodeParameters
  * @property {() => string[]|null} getSelectors
  */
+/*! SPDX-License-Identifier: 0BSD
+    SPDX-FileCopyrightText: 2023, Alexander Petros */
+htmx.defineExtension('safe-nonce', {
+  transformResponse: function(text, xhr, elt) {
+    htmx.config.refreshOnHistoryMiss = true // disable ajax fetching on history miss because it doesn't handle nonce replacment
+    let replaceRegex = new RegExp(`<script(\\s[^>]*>|>).*?<\\/script(\\s[^>]*>|>)`, 'gis') // remove all script tags regex
+    let nonce = xhr.getResponseHeader('HX-Nonce')
+    if (!nonce) {
+      const csp = xhr.getResponseHeader('content-security-policy')
+      if (csp) {
+        const cspMatch = csp.match(/(default|script)-src[^;]*'nonce-([^']*)'/i)
+        if (cspMatch) {
+          nonce = cspMatch[2]
+        }
+      }
+    }
+    if (window.location.hostname) {
+      const responseURL = new URL(xhr.responseURL)
+      if (responseURL.hostname !== window.location.hostname) {
+        nonce = '' // ignore nonce header if request is not some domain 
+      }
+    }
+    if (nonce) { // if nonce is valid then change regex to remove all scripts without this nonce
+      replaceRegex = new RegExp(`<script(\\s(?!nonce="${nonce.replace(/[\\\[\]\/^*.+?$(){}'#:!=|]/g, '\\$&')}")[^>]*>|>).*?<\\/script(\\s[^>]*>|>)`, 'gis')
+    }
+    return text.replace(replaceRegex, '').replace(/ignore:safe-nonce/g, '') // remove script tags and strip ignore extension
+  },
+  onEvent: function(name, evt) {
+    if (name === 'htmx:load') {
+      Array.from(evt.detail.elt.querySelectorAll('script')).forEach((script) => {
+        if (script.nonce !== htmx.config.inlineScriptNonce) {
+          script.remove() // remove all scripts with invalid nonce from page loaded content so it can't get saved in history where inlineScriptNonce can enable bad scripts
+        }
+      })
+      Array.from(evt.detail.elt.querySelectorAll('[hx-ext*="ignore:safe-nonce"], [data-hx-ext*="ignore:safe-nonce"]')).forEach((elt) => {
+        elt.remove() // remove content that tries to disable safe-nonce extension
+      })
+    }
+  }
+})
