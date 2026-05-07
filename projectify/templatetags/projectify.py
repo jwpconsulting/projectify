9f0abea2b354652c68da50f52b98a943be2be628
Justus Perlwitz <hello@justus.pw>
2026-05-07 09:39:10 +0900

    Add form attribute to action_button

--- projectify/templatetags/projectify.py
+++ projectify/templatetags/projectify.py
@@ -157,6 +157,7 @@ def action_button(
     style: Literal["secondary", "destructive"] = "secondary",
     value: Optional[str] = None,
     name: Optional[str] = None,
+    form: Optional[str] = None,
     grow: bool = True,
     disabled: bool = False,
     justify_left: bool = False,
@@ -172,6 +173,7 @@ def action_button(
     return format_html(
         '<button type="{type}" '
         'class="{width_class} shrink-0 {color_classes}{disabled_class} flex min-w-0 flex-row {justify} gap-2 rounded-lg px-4 py-2 font-bold"'
+        "{form}"
         "{disabled_attr}{value}{name}>"
         "{icon}"
         '<span class="truncate">{text}</span>'
@@ -182,6 +184,7 @@ def action_button(
         disabled_class=" opacity-20" if disabled else "",
         disabled_attr=" disabled" if disabled else "",
         justify="justify-left" if justify_left else "justify-center",
+        form=format_html('form="{form}"', form=form) if form else "",
         # TODO
         # icon=icon(icon, style, 6) if icon else "",
         icon=format_html(
