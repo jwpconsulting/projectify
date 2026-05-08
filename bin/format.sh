498c392ec01e7f3da4fb0c5ba9a588a92e2678be
Justus Perlwitz <hello@justus.pw>
2026-05-08 18:16:20 +0900

    Bin: Improve format.sh
    Keep going after encountering errors and exit with 1 when
    any errors were encountered

--- bin/format.sh
+++ bin/format.sh
@@ -6,6 +6,24 @@
 # This script prefixes commands with `uv run` so that you can just run
 # `bin/format.sh`
 set -e
-uv run ruff format .
-uv run ruff check --fix .
-uv run djlint --reformat .
+OK=1
+
+if ! uv run ruff format .; then
+    echo "Ruff format encountered a problem. Continuing."
+    OK=0
+fi
+
+if ! uv run ruff check --fix .; then
+    echo "Ruff check encountered a problem. Continuing."
+    OK=0
+fi
+
+if ! uv run djlint --reformat .; then
+    echo "djlint --reformat encountered a problem. Continuing."
+    OK=0
+fi
+
+if [[ OK -ne 1 ]]; then
+    echo "Couldn't format files correctly."
+    exit 1
+fi
