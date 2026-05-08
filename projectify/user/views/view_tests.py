5bd20889bcb06a248e12657f315b6a2c88fba5a5
Justus Perlwitz <hello@justus.pw>
2026-05-08 19:00:39 +0900

    Fix docstring

--- projectify/user/views/view_tests.py
+++ projectify/user/views/view_tests.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
 # SPDX-FileCopyrightText: 2025-2026 JWP Consulting GK
-"""Test views for email confirmation testing."""
+"""Test views for user authentication and authorization testing."""
 
 import secrets
 
