669d04fae52de1be9f211d39f9d7b2bd72a4c882
Justus Perlwitz <hello@justus.pw>
2026-07-02 21:00:19 +0900

    Workspace: Refactor attachment quota calc

--- projectify/workspace/selectors/quota.py
+++ projectify/workspace/selectors/quota.py
@@ -45,14 +45,14 @@ trial_conditions: Limitations = {
     "Attachment": 0,
 }
 
+
 # Full workspace conditions are somewhat like this:
 # {
 #     "Task": None,
 #     "Project": None,
 #     "TeamMemberAndInvite": workspace.customer.seats,
+#     "Attachment": 100 * 1024 * 1024
 # }
-
-
 def get_workspace_quota_for_resource(
     resource: Resource, workspace: Workspace
 ) -> Limitation:
@@ -68,37 +68,14 @@ def get_workspace_quota_for_resource(
             return trial_conditions[resource]
         case "full":
             pass
-    if resource == "TeamMemberAndInvite":
-        customer = workspace.customer
-        return customer.seats
-    return None
-
-
-def get_workspace_resource_count(
-    resource: Resource, workspace: Workspace
-) -> int:
-    """Return resource count for a specific resource."""
     match resource:
-        case "Task":
-            return Task.objects.filter(project__workspace=workspace).count()
-        case "Project":
-            return workspace.project_set.count()
         case "TeamMemberAndInvite":
-            user_count = workspace.users.count()
-            invite_count = workspace.teammemberinvite_set.filter(
-                redeemed=False
-            ).count()
-            return user_count + invite_count
+            customer = workspace.customer
+            return customer.seats
         case "Attachment":
-            match workspace.attachment_set.aggregate(
-                total_size=Sum("size", default=0)
-            ):
-                case {"total_size": int() as result}:
-                    return result
-                case other:
-                    raise RuntimeError(
-                        f"Encountered unexpected result {other}"
-                    )
+            return 100 * 1024 * 1024
+        case _:
+            return None
 
 
 def workspace_quota_for(*, resource: Resource, workspace: Workspace) -> Quota:
@@ -107,7 +84,26 @@ def workspace_quota_for(*, resource: Resource, workspace: Workspace) -> Quota:
     # Short circuit for no limit
     if limit is None:
         return Quota(current=None, limit=None, can_create_more=True)
-    current = get_workspace_resource_count(resource, workspace)
+    match resource:
+        case "Task":
+            current = Task.objects.filter(project__workspace=workspace).count()
+        case "Project":
+            current = workspace.project_set.count()
+        case "TeamMemberAndInvite":
+            user_count = workspace.users.count()
+            invite_count = workspace.teammemberinvite_set.filter(
+                redeemed=False
+            ).count()
+            current = user_count + invite_count
+        case "Attachment":
+            aggregate = workspace.attachment_set.aggregate(
+                total_size=Sum("size", default=0)
+            )
+            match aggregate:
+                case {"total_size": int() as result}:
+                    current = result
+                case other:
+                    raise RuntimeError(f"Unexpected result {other}")
     return Quota(current=current, limit=limit, can_create_more=current < limit)
 
 
